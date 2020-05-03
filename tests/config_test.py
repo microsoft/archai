1d42b62d4f7a3e6ced2d47728019ca121c26ea1d
Shital Shah <shitals@microsoft.com>
2020-05-03 04:41:51 -0700

    additional test for config resolution and override

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,6 +1,6 @@
 from archai.common.config import Config
 
-def test_param_override():
+def test_param_override1():
     conf = Config('confs/algos/darts.yaml;confs/datasets/cifar10.yaml')
     assert not conf['nas']['eval']['trainer']['apex']['enabled']
     assert not conf['nas']['eval']['loader']['apex']['enabled']
@@ -10,4 +10,15 @@ def test_param_override():
     assert conf['nas']['eval']['trainer']['apex']['enabled']
     assert conf['nas']['eval']['loader']['apex']['enabled']
 
-test_param_override()
+def test_param_override2():
+    conf = Config('confs/algos/darts.yaml;confs/datasets/imagenet.yaml')
+    assert conf['nas']['eval']['trainer']['apex']['distributed_enabled']
+    assert conf['nas']['eval']['loader']['apex']['distributed_enabled']
+
+    conf = Config('confs/algos/darts.yaml;confs/datasets/imagenet.yaml',
+           param_args=["--nas.eval.trainer.apex.enabled", "True", "--nas.eval.trainer.apex.distributed_enabled", "False"])
+    assert not conf['nas']['eval']['trainer']['apex']['distributed_enabled']
+    assert not conf['nas']['eval']['loader']['apex']['distributed_enabled']
+
+test_param_override1()
+test_param_override2()
