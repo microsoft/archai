207c26165372879fd75872fde0a95f470ad56687
Debadeepta Dey <dedey@microsoft.com>
2020-06-11 21:13:27 -0700

    Made gumbelsoftmax implementation general.

--- archai/algos/gumbelsoftmax/gs_cell_builder.py
+++ archai/algos/gumbelsoftmax/gs_cell_builder.py
@@ -1,5 +1,6 @@
 from overrides import overrides
 
+from archai.common.common import get_conf
 from archai.nas.cell_builder import CellBuilder
 from archai.nas.operations import Op, MultiOp
 from archai.nas.model_desc import ModelDesc, CellDesc, CellType, OpDesc, EdgeDesc
@@ -12,14 +13,18 @@ class GsCellBuilder(CellBuilder):
                        lambda op_desc, arch_params, affine:
                            GsOp(op_desc, arch_params, affine))
 
+
     @overrides
     def build(self, model_desc:ModelDesc, search_iter:int)->None:
         # if this is not the first iteration, we add new node to each cell
         if search_iter > 0:
             self.add_node(model_desc)
 
+        conf = get_conf()
+        self._gs_num_sample = conf['nas']['search']['gs']['num_sample']
+
         for cell_desc in model_desc.cell_descs():
-            self._build_cell(cell_desc, model_desc.params['gs_num_sample'])
+            self._build_cell(cell_desc, self._gs_num_sample)
 
     def _build_cell(self, cell_desc:CellDesc, gs_num_sample:int)->None:
         reduction = (cell_desc.cell_type==CellType.Reduction)
@@ -31,7 +36,7 @@ class GsCellBuilder(CellBuilder):
                                     params={
                                         'conv': cell_desc.conv_params,
                                         'stride': 2 if reduction and j < 2 else 1,
-                                        'gs_num_sample': gs_num_sample,
+                                        'gs_num_sample': self._gs_num_sample,
                                     }, in_len=1, trainables=None, children=None)
                 edge = EdgeDesc(op_desc, input_ids=[j])
                 node.edges.append(edge)
