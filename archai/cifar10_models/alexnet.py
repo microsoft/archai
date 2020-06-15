ba6fd76ca452f119f3de44812efaaabb8b8c0f47
shatadru99 <shatadru1999@gmail.com>
2020-06-15 17:16:21 +0530

    Update alexnet.py

--- archai/cifar10_models/alexnet.py
+++ archai/cifar10_models/alexnet.py
@@ -13,7 +13,7 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=(5,5), padding=1),
+            nn.Conv2d(96, 256, kernel_size=(5,5), padding=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
