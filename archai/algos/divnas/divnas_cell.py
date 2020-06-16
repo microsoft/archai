d8743a260bbbb8dd25c8c274ddf9bf0c83250601
Debadeepta Dey <dedey@microsoft.com>
2020-06-16 10:33:37 -0700

    Updated divnas.

--- archai/algos/divnas/divnas_cell.py
+++ archai/algos/divnas/divnas_cell.py
@@ -16,6 +16,8 @@ from overrides import overrides, EnforceOverrides
 import archai.algos.divnas.analyse_activations as aa
 
 from archai.nas.cell import Cell
+from archai.nas.operations import Zero
+from archai.nas.operations import Op
 
 
 class Divnas_Cell():
@@ -28,22 +30,35 @@ class Divnas_Cell():
         self._edgeoptype = None
         self._sigma = None
         self._counter = 0
-        self.node_covs:Dict[int, np.array] = {}        
+        self.node_covs:Dict[int, np.array] = {}
+        self.node_num_to_node_op_to_cov_ind:Dict[int, Dict[Op, int]] = {}        
         
     def collect_activations(self, edgeoptype, sigma:float)->None:
         self._collect_activations = True
         self._edgeoptype = edgeoptype
         self._sigma = sigma
 
+        # collect bookkeeping info
+        for i, node in enumerate(self._cell.dag):
+            node_op_to_cov_ind:Dict[Op, int] = {}
+            counter = 0
+            for edge in node:
+                for op, alpha in edge._op.ops():
+                    if isinstance(op, Zero):
+                        continue
+                    node_op_to_cov_ind[op] = counter
+                    counter += 1                        
+            self.node_num_to_node_op_to_cov_ind[i] = node_op_to_cov_ind
+
+
         # go through all edges in the DAG and if they are of edgeoptype
         # type then set them to collect activations
-        for i, node in enumerate(self._cell.dag):
-            
+        for i, node in enumerate(self._cell.dag):            
             # initialize the covariance matrix for this node
             num_ops = 0
             for edge in node:
                 if hasattr(edge._op, 'PRIMITIVES') and type(edge._op) == self._edgeoptype:
-                    num_ops += edge._op.num_valid_div_ops
+                    num_ops += edge._op.num_primitive_ops - 1
                     edge._op.collect_activations = True
                    
             self.node_covs[id(node)] = np.zeros((num_ops, num_ops))
@@ -53,7 +68,7 @@ class Divnas_Cell():
         assert self._collect_activations
 
         for _, node in enumerate(self._cell.dag):
-            # TODO: conver to explicit ordering
+            # TODO: convert to explicit ordering
             all_activs = []
             for j, edge in enumerate(node):
                 if type(edge._op) == self._edgeoptype:
