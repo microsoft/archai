a042dcba7245259050cb5970e4bd1c8e4b794d29
Shital Shah <shitals@microsoft.com>
2020-07-08 07:34:56 -0700

    add yaml_log setting

--- archai/common/ordereddict_logger.py
+++ archai/common/ordereddict_logger.py
@@ -24,14 +24,15 @@ class OrderedDictLogger:
     """The purpose of the structured logging is to store logs as key value pair. However, when you have loop and sub routine calls, what you need is hierarchical dictionaries where the value for a key could be a dictionary. The idea is that you set one of the nodes in tree as current node and start logging your values. You can then use pushd to create and go to child node and popd to come back to parent. To implement this mechanism we use two main variables: _stack allows us to push each node on stack when pushd is called. The node is OrderedDictionary. As a convinience, we let specify child path in pushd in which case child hierarchy is created and current node will be set to the last node in specified path. When popd is called, we go back to original parent instead of parent of current node. To implement this we use _paths variable which stores subpath when each pushd call was made.
     """
     def __init__(self, filepath:Optional[str], logger:Optional[logging.Logger],
-                 save_delay:Optional[float]=30.0) -> None:
+                 save_delay:Optional[float]=30.0, yaml_log=True) -> None:
         super().__init__()
-        self.reset(filepath, logger, save_delay)
+        self.reset(filepath, logger, save_delay, yaml_log)
 
     def reset(self, filepath:Optional[str], logger:Optional[logging.Logger],
                  save_delay:Optional[float]=30.0,
-                 load_existing_file=False, backup_existing_file=True) -> None:
+                 load_existing_file=False, backup_existing_file=True, yaml_log=True) -> None:
         self._logger = logger
+        self._yaml_log = yaml_log
         # stack stores dict for each path
         # path stores each path created via pushd
         self._paths = [['']]
@@ -42,7 +43,7 @@ class OrderedDictLogger:
 
         # backup file if already exist
         root_od = OrderedDict()
-        if filepath and os.path.exists(filepath):
+        if self._yaml_log and filepath and os.path.exists(filepath):
             if load_existing_file:
                 root_od = yaml.load(self._filepath, Loader=yaml.Loader)
             if backup_existing_file:
@@ -96,7 +97,7 @@ class OrderedDictLogger:
             with open(filepath, 'w') as f:
                 yaml.dump(self._root(), f)
 
-    def load(self, filepath:str)->None:
+    def load(self, filepath:str)->None:                
         with open(filepath, 'r') as f:
             od = yaml.load(f, Loader=yaml.Loader)
             self._stack = [od]
@@ -116,6 +117,9 @@ class OrderedDictLogger:
 
     def _update_key(self, key:Any, val:Any, exists_ok=True,
                     node:Optional[OrderedDict]=None, path:List[str]=[]):
+        if not self._yaml_log:
+            return
+        
         if not exists_ok and key in self._cur():
             raise KeyError(f'Key "{key}" already exists in log at path {self.path()}')
 
@@ -127,6 +131,9 @@ class OrderedDictLogger:
         node[str(key)] = val
 
     def _ensure_paths(self)->None:
+        if not self._yaml_log:
+            return
+                
         if self._stack[-1] is not None:
             return
         last_od = None
@@ -143,6 +150,9 @@ class OrderedDictLogger:
             last_od = od
 
     def pushd(self, *keys:Any)->'OrderedDictLogger':
+        if not self._yaml_log:
+            return self
+                
         """Creates new path as specified by the sequence of the keys"""
         self._paths.append([str(k) for k in keys])
         self._stack.append(None) # delay create
@@ -150,12 +160,18 @@ class OrderedDictLogger:
         return self # this allows calling __enter__
 
     def popd(self):
+        if not self._yaml_log:
+            return
+                
         if len(self._stack)==1:
             raise RuntimeError('There is no child logger, popd() call is invalid')
         self._stack.pop()
         self._paths.pop()
 
     def path(self)->str:
+        if not self._yaml_log:
+            return '/'
+        
         # flatten array of array
         return '/'.join(itertools.chain.from_iterable(self._paths[1:]))
 
