c3947d6f8657d79895314e0c3b5f9b489804f7fc
Ubuntu <dedey@raytest.0soa4rctbgdexcx4cqxzmursxe.xx.internal.cloudapp.net>
2020-07-17 03:00:36 +0000

    Evaluation of gallery of models saved by petridish distributed search nominally works.

--- archai/nas/evaluate.py
+++ archai/nas/evaluate.py
@@ -25,7 +25,7 @@ def eval_arch(conf_eval:Config, cell_builder:Optional[CellBuilder]):
     # region conf vars
     conf_loader       = conf_eval['loader']
     model_filename    = conf_eval['model_filename']
-    metric_filename    = conf_eval['metric_filename'] p
+    metric_filename    = conf_eval['metric_filename']
     conf_checkpoint = conf_eval['checkpoint']
     resume = conf_eval['resume']
     conf_train = conf_eval['trainer']
@@ -73,15 +73,14 @@ def eval_archs(conf_eval:Config, cell_builder:Optional[CellBuilder]):
         cell_builder.register_ops()
 
     # get list of model descs in the gallery folder
-    files = [f for f in os.listdir(final_desc_folderpath) if os.path.isfile(os.path.join(final_desc_folderpath, f))]
+    files = [os.path.join(final_desc_folderpath, f) for f in os.listdir(final_desc_folderpath) if os.path.isfile(os.path.join(final_desc_folderpath, f))]
 
     # TODO: parallelize model training
     for model_desc_filename in files:
-        final_desc_filename = model_desc_filename.split('.')[0] + '_final.yaml'
         full_desc_filename = model_desc_filename.split('.')[0] + '_full.yaml'
         metric_filename = model_desc_filename.split('.')[0] + '_metrics.yaml'
         model_filename = model_desc_filename.split('.')[0] + '_model.pt'
-        model = create_model(conf_eval, final_desc_filename=final_desc_filename, full_desc_filename=full_desc_filename)
+        model = create_model(conf_eval, final_desc_filename=model_desc_filename, full_desc_filename=full_desc_filename)
 
         # get data
         train_dl, _, test_dl = data.get_data(conf_loader)
@@ -95,6 +94,10 @@ def eval_archs(conf_eval:Config, cell_builder:Optional[CellBuilder]):
         # save model
         if model_filename:
             model_filename = utils.full_path(model_filename)
+            ml_utils.save_model(model, model_filename)
+            logger.info({'model_save_path': model_filename})
+
+
 
     logger.popd()
 
