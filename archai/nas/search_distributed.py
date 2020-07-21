480786326b7c8eeee646e57db425f39ed6597dc0
Ubuntu <dedey@raytest.0soa4rctbgdexcx4cqxzmursxe.xx.internal.cloudapp.net>
2020-07-21 02:57:00 +0000

    A certain logging hack is no longer needed.

--- archai/nas/search_distributed.py
+++ archai/nas/search_distributed.py
@@ -162,12 +162,6 @@ def train_desc(model_desc_wrapped, conf_train: Config, finalizers: Finalizers, t
     drop_path_prob = conf_trainer['drop_path_prob']
     # endregion
 
-    # REVIEW: ss: Is below still happening?
-    # TODO: logger was throwing errors that key already exists
-    # current hack is to add a random string to create subfolder
-    # need more principled workaround to this
-    #res = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
-    #                                              for i in range(7))
     logger.pushd(trainer_title)
 
     model_desc = model_desc_wrapped.model_desc
@@ -324,7 +318,7 @@ class SearchDistributed:
 
     def should_terminate_search(self)->bool:
         ''' Looks at the parent pool and decides whether to terminate search '''
-        # NOTE: Placeholder for now
+        # TODO: Placeholder for now
         if len(self._parent_models) > 16:
             return True
         else:
