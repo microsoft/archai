acfd3c104e6fc28bb08b2cf6d40f821bc3c9d5cb
Shital Shah <shitals@microsoft.com>
2020-08-23 21:47:59 -0700

    nas_utils cleanup, aux tower separation from build_cell,

--- archai/nas/nas_utils.py
+++ archai/nas/nas_utils.py
@@ -14,16 +14,6 @@ from .model import Model
 from ..common.common import logger
 from ..common.checkpoint import CheckPoint
 
-def build_cell(model_desc, model_desc_builder:Optional[ModelDescBuilder], search_iter:int)->None:
-    if model_desc_builder:
-        model_desc_builder.register_ops()
-        model_desc_builder.build(model_desc, search_iter)
-
-def create_macro_desc(conf_model_desc: Config,
-                      template_model_desc:Optional[ModelDesc])->ModelDesc:
-    builder = ModelDescBuilder(conf_model_desc, template=template_model_desc)
-    model_desc = builder.build_model_desc()
-    return model_desc
 
 def checkpoint_empty(checkpoint:Optional[CheckPoint])->bool:
     return checkpoint is None or checkpoint.is_empty()
@@ -40,18 +30,5 @@ def create_checkpoint(conf_checkpoint:Config, resume:bool)->Optional[CheckPoint]
                  'checkpoint_path': None  if checkpoint is None else checkpoint.filepath})
     return checkpoint
 
-def model_from_conf(full_desc_filename:str, conf_model_desc: Config,
-        affine:bool, droppath:bool, template_model_desc:ModelDesc)->Model:
-    """Creates model given desc config and template"""
-    # create model
-    model_desc = create_macro_desc(conf_model_desc, template_model_desc)
-    # save model that we would eval for reference
-    model_desc.save(full_desc_filename)
-
-    return model_from_desc(model_desc, droppath=droppath, affine=affine)
-
-def model_from_desc(model_desc, droppath:bool, affine:bool)->Model:
-    model = Model(model_desc, droppath=droppath, affine=affine)
-    return model
 
 
