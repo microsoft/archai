602a633d4a91c1af882b8b0d5fd53668311e4648
Shital Shah <shitals@microsoft.com>
2020-09-08 03:07:30 -0700

    conda default -y in install

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 set -o xtrace
 
 # ray.init() fails if below is not done
-conda install -c conda-forge pickle5
+conda install -y -c conda-forge pickle5
+conda install -y -c anaconda pydot graphviz
 
 bash tools/apex_install.sh
 pip install -e .
