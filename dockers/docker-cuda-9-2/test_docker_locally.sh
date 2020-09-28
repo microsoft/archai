daa2c0ee51fb62f3757c6426e89ee537a70e76d6
Ubuntu <dedey@raytest.0soa4rctbgdexcx4cqxzmursxe.xx.internal.cloudapp.net>
2020-09-28 20:18:57 +0000

    Deleted dockerfile for cuda9.2.

--- dockers/docker-cuda-9-2/test_docker_locally.sh
+++
@@ -1,3 +0,0 @@
-# This command maps /home/dedey to /root/dedey inside the docker and drops one into 
-# a terminal inside the docker. Then one can just run the training command.
-docker run --runtime=nvidia -it --rm -v /home/dedey:/root/dedey debadeepta/petridishpytorchcuda92:latest 
