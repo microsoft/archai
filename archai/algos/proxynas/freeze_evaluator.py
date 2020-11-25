e4579a2dbf9318db7d1efa46102972d1abbdf42f
Debadeepta Dey <dedey@microsoft.com>
2020-11-25 10:32:33 -0800

    Train and freeze all but last layer implemented. To be tested.

---
+++ archai/algos/proxynas/freeze_evaluator.py
@@ -0,0 +1,45 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from archai.nas.evaluater import Evaluater
+from typing import Optional, Tuple
+import importlib
+import sys
+import string
+import os
+
+import torch
+from torch import nn
+from torch.utils.data.dataloader import DataLoader
+
+from overrides import overrides, EnforceOverrides
+
+from archai.common.trainer import Trainer
+from archai.common.config import Config
+from archai.common.common import logger
+from archai.datasets import data
+from archai.nas.model_desc import ModelDesc
+from archai.nas.model_desc_builder import ModelDescBuilder
+from archai.nas import nas_utils
+from archai.common import ml_utils, utils
+from archai.common.metrics import EpochMetrics, Metrics
+from archai.nas.model import Model
+from archai.common.checkpoint import CheckPoint
+
+from .freeze_trainer import FreezeTrainer
+
+
+class FreezeEvaluator(Evaluater, EnforceOverrides):
+    @overrides
+    def train_model(self,  conf_train:Config, model:nn.Module,
+                    checkpoint:Optional[CheckPoint])->Metrics:
+        conf_loader = conf_train['loader']
+        conf_train = conf_train['trainer']
+
+        # get data
+        train_dl, test_dl = self.get_data(conf_loader)
+
+        trainer = FreezeTrainer(conf_train, model, checkpoint)
+        train_metrics = trainer.fit(train_dl, test_dl)
+        return train_metrics
+            
