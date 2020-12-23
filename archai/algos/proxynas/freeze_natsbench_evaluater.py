409b778e89c100c3bde5b9372af62c0e3deaf13c
Debadeepta Dey <dedey@microsoft.com>
2020-12-22 21:26:17 -0800

    Added natsbench for rapid evaluation of architecture ranking proxies.

--- archai/algos/proxynas/freeze_natsbench_evaluater.py
+++ archai/algos/proxynas/freeze_natsbench_evaluater.py
@@ -28,8 +28,10 @@ from archai.common.checkpoint import CheckPoint
 from archai.nas.evaluater import Evaluater
 from archai.algos.proxynas.freeze_trainer import FreezeTrainer
 
+from nats_bench import create
+from archai.algos.natsbench.lib.models import get_cell_based_tiny_net
 
-class ManualFreezeEvaluater(Evaluater):
+class FreezeNatsbenchEvaluater(Evaluater):
     @overrides
     def create_model(self, conf_eval:Config, model_desc_builder:ModelDescBuilder,
                       final_desc_filename=None, full_desc_filename=None)->nn.Module:
@@ -39,30 +41,32 @@ class ManualFreezeEvaluater(Evaluater):
         # if explicitly passed in then don't get from conf
         if not final_desc_filename:
             final_desc_filename = conf_eval['final_desc_filename']
-        model_factory_spec = conf_eval['model_factory_spec']
+        arch_index = conf_eval['natsbench']['arch_index']
+        natsbench_location = conf_eval['natsbench']['natsbench_location']
         # endregion
 
-        assert model_factory_spec
+        assert arch_index
+        assert natsbench_location
 
-        return self._model_from_factory(model_factory_spec, dataset_name)
+        return self._model_from_natsbench(arch_index, dataset_name, natsbench_location)
 
-    def _model_from_factory(self, model_factory_spec:str, dataset_name:str)->Model:
-        splitted = model_factory_spec.rsplit('.', 1)
-        function_name = splitted[-1]
+    def _model_from_natsbench(self, arch_index:int, dataset_name:str, natsbench_location:str)->Model:
 
-        if len(splitted) > 1:
-            module_name = splitted[0]
-        else:
-            module_name = self._default_module_name(dataset_name, function_name)
+        # create natsbench api
+        api = create(natsbench_location, 'tss', fast_mode=True, verbose=True)
 
-        module = importlib.import_module(module_name) if module_name else sys.modules[__name__]
-        function = getattr(module, function_name)
-        model = function()
+        if arch_index > 15625 or arch_index < 0:
+            logger.warn(f'architecture id {arch_index} is invalid ')
+        
+        if dataset_name not in {'cifar10', 'cifar100', 'ImageNet16-120'}:
+            logger.warn(f'dataset {dataset_name} is not part of natsbench')
+            raise NotImplementedError()
 
-        logger.info({'model_factory':True,
-                    'module_name': module_name,
-                    'function_name': function_name,
-                    'params': ml_utils.param_size(model)})
+        config = api.get_net_config(arch_index, dataset_name)
+        # network is a nn.Module subclass. the last few modules have names
+        # lastact, lastact.0, lastact.1, global_pooling, classifier 
+        # which we can freeze train as usual
+        model = get_cell_based_tiny_net(config)
 
         return model
 
