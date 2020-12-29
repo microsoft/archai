6eac18583273458ffc3a387ed71744b35613bbca
Debadeepta Dey <dedey@microsoft.com>
2020-12-29 14:57:17 -0800

    Found good parameters for freeze training.

--- scripts/reports/analysis_aggregate.py
+++ scripts/reports/analysis_aggregate.py
@@ -22,8 +22,8 @@ import pandas as pd
 
 from archai.common import utils
 from archai.common.ordereddict_logger import OrderedDictLogger
-import scripts.reports
-from scripts.reports.analysis_utils import epoch_nodes, fix_yaml, remove_seed_part, group_multi_runs, collect_epoch_nodes, EpochStats, FoldStats, stat2str, get_epoch_stats, get_summary_text, get_details_text, plot_epochs, write_report
+
+from analysis_utils import epoch_nodes, fix_yaml, remove_seed_part, group_multi_runs, collect_epoch_nodes, EpochStats, FoldStats, stat2str, get_epoch_stats, get_summary_text, get_details_text, plot_epochs, write_report
 
 import re
 
