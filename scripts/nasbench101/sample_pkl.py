b911b469933f75f711dc99ea2c5ab7d306490454
Shital Shah <shitals@microsoft.com>
2021-01-16 14:24:13 -0800

    add cell info debug, resample pkl

--- scripts/nasbench101/sample_pkl.py
+++ scripts/nasbench101/sample_pkl.py
@@ -8,7 +8,13 @@ def main():
 
     with open(in_dataset_file, 'rb') as f:
       records = pickle.load(f)
-    sampled_hashes = set(records[i][0] for i in [0, 4, 40, 400, 4000, 40000, 400000, 401277, len(records)-1])
+
+    sampled_indices = set()
+    adj_samples = 1000
+    for i in [0, 4000, 40000, len(records)-1-adj_samples+1]:
+      sampled_indices = sampled_indices.union([i+k for k in range(adj_samples)])
+
+    sampled_hashes = set(records[i][0] for i in sorted(list(sampled_indices)))
     sampled = [r for r in records if r[0] in sampled_hashes]
     with open(out_dataset_file, 'wb') as f:
       pickle.dump(sampled, f)
