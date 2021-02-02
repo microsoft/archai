5835e0d6083daaf387bffe17d7b5264bf3b29f09
Debadeepta Dey <dedey@microsoft.com>
2021-02-01 21:17:19 -0800

    Creating scripts  for aggregate analysis of experiments.

--- scripts/reports/proxynas_plots/cross_exp_plots.py
+++ scripts/reports/proxynas_plots/cross_exp_plots.py
@@ -1,55 +1,75 @@
+import os
+import yaml 
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 def main():
+
+    exp_folder = 'C:\\Users\\dedey\\archai_experiment_reports'
     
-    # plot spe vs. important hyperparams
-    legends = ['conditionalbatch_256_freezebatch_2048_freezeepochs_5_freezethresh_0.6',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_10_freezethresh_0.6',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_5_freezethresh_0.5',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_10_freezethresh_0.5',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_5_freezethresh_0.4',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_10_freezethresh_0.4',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_5_freezethresh_0.3',\
-               'conditionalbatch_256_freezebatch_2048_freezeepochs_10_freezethresh_0.3']
+    exp_list = ['ft_fb2048_ftlr1.5_fte5_ct256_ftt0.6', \
+                'ft_fb2048_ftlr1.5_fte10_ct256_ftt0.6', \
+                'ft_fb2048_ftlr1.5_fte5_ct256_ftt0.5', \
+                'ft_fb2048_ftlr1.5_fte10_ct256_ftt0.5', \
+                'ft_fb2048_ftlr1.5_fte5_ct256_ftt0.4', \
+                'ft_fb2048_ftlr1.5_fte10_ct256_ftt0.4', \
+                'ft_fb2048_ftlr1.5_fte5_ct256_ftt0.3', \
+                'ft_fb2048_ftlr1.5_fte10_ct256_ftt0.3', \
+                'ft_fb1024_ftlr1.5_fte5_ct256_ftt0.6', \
+                'ft_fb1024_ftlr1.5_fte10_ct256_ftt0.6', \
+                'ft_fb512_ftlr1.5_fte5_ct256_ftt0.6', \
+                'ft_fb512_ftlr1.5_fte10_ct256_ftt0.6', \
+                'ft_fb256_ftlr1.5_fte5_ct256_ftt0.6', \
+                'ft_fb256_ftlr1.5_fte10_ct256_ftt0.6', \
+                'ft_fb1024_ftlr0.1_fte5_ct256_ftt0.6', \
+                'ft_fb1024_ftlr0.1_fte10_ct256_ftt0.6', \
+                'ft_fb512_ftlr0.1_fte5_ct256_ftt0.6', \
+                'ft_fb512_ftlr0.1_fte10_ct256_ftt0.6', \
+                'ft_fb256_ftlr0.1_fte5_ct256_ftt0.6', \
+                'ft_fb256_ftlr0.1_fte10_ct256_ftt0.6']
 
-    spes = [0.81, 0.85, 0.71, 0.75, 0.52, 0.57, 0.43, 0.51]
+    # parse raw data from all processed experiments
+    data = {}
+    for exp in exp_list:
+        exp_full_path = os.path.join(exp_folder, exp)
+        with open(os.path.join(exp_full_path, 'raw_data.yaml')) as f:
+            data[exp] = yaml.load(f, Loader=yaml.Loader)
 
-    total_durations = [238.21, 276.71, 158.92, 204.53, 99.32, 123.42, 61.99, 103.48]
-    total_std = [294.70, 290.37, 223.42, 208.84, 102.00, 81.14, 43.50, 45.41]
+    # plot spearman correlation vs. top percent of architectures
+    plt.clf()
+    legend_labels = []
 
-    conditional_durations = [197.05, 194.98, 111.83, 110.01, 52.41, 41.93, 20.87, 21.06]
-    conditional_std = [294.86, 290.68, 223.68, 209.26, 102.05, 81.43, 43.42, 45.12]
+    for key in data.keys():
+        plt.plot(data[key]['top_percents'], data[key]['spe_freeze'], marker='*', mfc='red', ms=10)
+        legend_labels.append(key + '_freezetrain')
+        plt.plot(data[key]['top_percents'], data[key]['spe_naswot'], marker='.', mfc='blue', ms=10)
+        legend_labels.append(key + '_naswot')
 
-    partial_durations = [41.16, 81.72, 47.08, 94.51, 46.91, 81.48, 41.11, 82.41]
-    partial_std = [1.92, 3.2, 1.92, 4.05, 1.85, 3.74, 1.52, 3.10]
-
-    reg_spes = [0.78, 0.9, 0.94]
-    reg_total_durations = [110.98, 223.71, 379.28]
-
-    reg_legends = ['regular_batch_1024_epochs_10', 'regular_batch_1024_epochs_20', 'regular_batch_1024_epochs_30']
-
-
-    plt.scatter(total_durations[0], spes[0])
-    plt.scatter(total_durations[1], spes[1])
-    plt.scatter(total_durations[2], spes[2])
-    plt.scatter(total_durations[3], spes[3])
-    plt.scatter(total_durations[4], spes[4])
-    plt.scatter(total_durations[5], spes[5])
-    plt.scatter(total_durations[6], spes[6])
-    plt.scatter(total_durations[7], spes[7])
-    plt.scatter(reg_total_durations[0], reg_spes[0])
-    plt.scatter(reg_total_durations[1], reg_spes[1])
-    plt.scatter(reg_total_durations[2], reg_spes[2])
-    plt.legend(legends + reg_legends)
-    plt.xlabel('Avg. Wall Clock (s)')
+    plt.ylim((-1.0, 1.0))
+    plt.xlabel('Top percent of architectures')
     plt.ylabel('Spearman Correlation')
+    plt.legend(labels=legend_labels)
     plt.grid()
     plt.show()
+    savename = os.path.join(exp_folder, f'aggregate_spe.png')
+    plt.savefig(savename, dpi=plt.gcf().dpi, bbox_inches='tight')
 
+    # plot timing information vs. top percent of architectures
+    plt.clf()
+    time_legend_labels = []
 
-
+    for key in data.keys():
+        plt.errorbar(data[key]['top_percents'], data[key]['freeze_times_avg'], yerr=np.array(data[key]['freeze_times_std'])/2, marker='*', mfc='red', ms=5)    
+        time_legend_labels.append(key + '_freezetrain')
+        
+    plt.xlabel('Top percent of architectures')
+    plt.ylabel('Avg. Duration (s)')
+    plt.legend(labels=time_legend_labels)
+    plt.grid()
+    plt.show()
+    savename = os.path.join(exp_folder, f'aggregate_duration.png')
+    plt.savefig(savename, dpi=plt.gcf().dpi, bbox_inches='tight')
 
 
 
