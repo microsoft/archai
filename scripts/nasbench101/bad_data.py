6b50633c7a5d2861f3d3352e0e3df93ec8a670f7
Shital Shah <shitals@microsoft.com>
2021-02-08 10:04:18 -0800

    dump count o fbadly trained archs

--- scripts/nasbench101/bad_data.py
+++ scripts/nasbench101/bad_data.py
@@ -13,7 +13,8 @@ def main():
     vars = [statistics.variance(statistics.mean(nsds.get_test_acc(i, epochs=e)) for e in Nasbench101Dataset.VALID_EPOCHS) \
             for i in range(len(nsds))]
 
-    print(vars[:100])
+    bad_archs = list((i,v) for i,v in enumerate(vars) if v < 0.01)
+    print(bad_archs)
 
 
 if __name__ == '__main__':
