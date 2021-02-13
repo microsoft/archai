2e02c013abc64dc4b52233630afb635076662113
Debadeepta Dey <dedey@microsoft.com>
2021-02-12 19:25:16 -0800

    Added imagenet16 provider classes.

---
+++ archai/datasets/providers/imagenet16_provider.py
@@ -0,0 +1,61 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from typing import List, Tuple, Union, Optional
+import os
+
+from overrides import overrides, EnforceOverrides
+from torch.utils.data.dataset import Dataset
+
+import torchvision
+from torchvision.transforms import transforms
+
+from archai.datasets.dataset_provider import DatasetProvider, register_dataset_provider, TrainTestDatasets
+from archai.datasets.providers.imagenet16 import ImageNet16
+from archai.common.config import Config
+from archai.common import utils
+
+
+class ImageNet16120Provider(DatasetProvider):
+    def __init__(self, conf_dataset:Config):
+        super().__init__(conf_dataset)
+        self._dataroot = utils.full_path(conf_dataset['dataroot'])
+
+    @overrides
+    def get_datasets(self, load_train:bool, load_test:bool,
+                     transform_train, transform_test)->TrainTestDatasets:
+        trainset, testset = None, None
+
+        if load_train:
+            trainpath = os.path.join(self._dataroot, 'imagenet16')
+            trainset = ImageNet16(trainpath, True, None, 120)
+        if load_test:
+            testpath = os.path.join(self._dataroot, 'imagenet16')
+            testset = ImageNet16(testpath, False, None, 120)
+
+        return trainset, testset
+
+    @overrides
+    def get_transforms(self)->tuple:
+        # MEAN, STD from Xuanyi Dong [Github D-X-Y]        
+        MEAN = [x / 255 for x in [122.68, 116.66, 104.01]]
+        STD  = [x / 255 for x in [63.22,  61.26 , 65.09]]
+
+        # transformations match that in
+        
+        train_transf = [
+            transforms.RandomHorizontalFlip(), 
+            transforms.RandomCrop(16, padding=2)            
+        ]
+
+        normalize = [
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)
+        ]
+
+        train_transform = transforms.Compose(train_transf + normalize)
+        test_transform = transforms.Compose(normalize)
+
+        return train_transform, test_transform
+
+register_dataset_provider('imagenet16120', ImageNet16120Provider)
