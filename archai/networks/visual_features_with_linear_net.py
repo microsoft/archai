da8e80b56ab7fa8ff8ec0910fc9041e0cd88c419
Debadeepta Dey <dedey@microsoft.com>
2021-02-16 19:22:43 -0800

    Linear network with hog features working now.

---
+++ archai/networks/visual_features_with_linear_net.py
@@ -0,0 +1,42 @@
+import torch
+from torch.functional import Tensor
+import torch.nn as nn
+from skimage import feature
+import matplotlib.pyplot as plt
+
+
+
+class VisualFeaturesWithLinearNet(nn.Module):
+    def __init__(self, feature_len:int, n_classes:int):
+        super(VisualFeaturesWithLinearNet, self).__init__()
+        self.feature_len = feature_len
+
+        self.fc = nn.Linear(feature_len, n_classes)        
+
+    def _compute_features(self, x:Tensor)->Tensor:
+        # compute image features on each image
+        num_images = x.shape[0]
+        hog_feat_storage = []
+        for i in range(num_images):
+
+            img = x[i, :, :, :]
+            img = img.permute(1, 2, 0)
+            img = img.cpu().numpy()
+            # if self.counter > 0:
+            #     plt.imshow(img)
+            #     plt.show()
+
+            hog_feat = feature.hog(img)
+            #hog_feat = torch.from_numpy(hog_feat).reshape(self.feature_len, 1)
+            hog_feat = torch.from_numpy(hog_feat)
+            assert self.feature_len == hog_feat.shape[0]
+            hog_feat_storage.append(hog_feat)
+
+        all_hog_feats = torch.stack(hog_feat_storage)
+        all_hog_feats.requires_grad = False
+        all_hog_feats = all_hog_feats.cuda()
+        return all_hog_feats    
+
+    def forward(self, x):
+        feats = self._compute_features(x)    
+        return self.fc(feats.float())
---
+++ scripts/misc/train_visual_features_linearnet.py
@@ -0,0 +1,35 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import torch
+from archai.networks.visual_features_with_linear_net import VisualFeaturesWithLinearNet
+from archai.common.trainer import Trainer
+from archai.common.config import Config
+from archai.common.common import common_init
+from archai.datasets import data
+
+def train_test(conf_eval:Config):
+    conf_loader = conf_eval['loader']
+    conf_trainer = conf_eval['trainer']
+
+    # create model
+    Net = VisualFeaturesWithLinearNet
+    feature_len = 324
+    n_classes = 10
+    model = Net(feature_len, n_classes).to(torch.device('cuda',  0))
+
+    # get data
+    data_loaders = data.get_data(conf_loader)
+
+    # train!
+    trainer = Trainer(conf_trainer, model)
+    trainer.fit(data_loaders)
+
+
+if __name__ == '__main__':
+    conf = common_init(config_filepath='confs/algos/visual_features_linearnet.yaml')
+    conf_eval = conf['nas']['eval']
+
+    train_test(conf_eval)
+
+
