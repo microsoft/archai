6ed7b139f48d6a192943473f1fea00fa73cdf2b2
Shital Shah <shitals@microsoft.com>
2021-03-01 06:14:40 -0800

    add hf perplexity and datasets

--- archai/nlp/gpt2_trainer_atg.py
+++ archai/nlp/gpt2_trainer_atg.py
@@ -6,6 +6,7 @@ from pytorch_lightning import callbacks
 
 import torch
 from torch import nn
+from torch.utils.data import DataLoader, Dataset
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, AutoConfig
 from tokenizers import ByteLevelBPETokenizer
@@ -181,6 +182,34 @@ def generate(model, tokenizer, prompt:str,
 
 
     return gen_texts
+
+def evaluate(net:nn.Module, data_loader:DataLoader, apex:ApexUtils, device):
+    net.eval()
+
+    with torch.no_grad():
+        pred_counts = []
+        losses:List[torch.Tensor] = []
+        prev_seg_state = None
+
+        for inputs, labels in data_loader:
+            inputs, labels = inputs.to(device, non_blocking=True), labels.to(device, non_blocking=True)
+
+            preds = net.forward(inputs, labels, prev_seg_state=prev_seg_state)
+            prev_seg_state = preds['state']
+            losses.append(preds['loss'])
+            pred_counts.append(labels.size(0))
+        pred_counts = np.array(pred_counts)
+        pred_counts_normed = pred_counts / pred_counts.sum()
+        loss = sum(x * w for x, w in zip(losses, pred_counts_normed))
+
+    if apex.is_dist():
+        gathered_losses = [torch.zeros_like(loss) for _ in range(apex.world_size)]
+        torch.distributed.all_gather(gathered_losses, loss)
+        loss = sum(gathered_losses) / len(gathered_losses)
+
+    loss = loss.item()
+    perplexity = np.nan if loss > 5 else np.e ** loss
+    return dict(loss=loss, perplexity=perplexity)
 
 def main():
     parser = argparse.ArgumentParser(description='GPT2 trainer')
