6dad505dde1e038187b1d1d1a6beb75a6308e1e9
Debadeepta Dey <dedey@microsoft.com>
2021-03-11 20:26:36 -0800

    Starter code for generating synthetic dataset.

---
+++ archai/datasets/synthetic_gen/simple_nn.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+
+class SimpleNN(nn.Module):
+    def __init__(self, num_classes:int=10):
+        super(SimpleNN, self).__init__()
+
+        self.net = nn.Sequential(
+                                nn.Linear(32*32*3, 2048),
+                                nn.ReLU(),
+                                nn.Linear(2048, 2048),
+                                nn.ReLU(),
+                                nn.Linear(2048, num_classes)
+                            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:        
+        return self.net(x)
