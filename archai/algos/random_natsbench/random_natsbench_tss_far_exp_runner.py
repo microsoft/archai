965bacf1f1635686d1ee881f3a577f482b15d13f
Debadeepta Dey <dedey@microsoft.com>
2021-04-13 14:53:13 -0700

    Working on random search using FastArchRank.

--- archai/algos/random_natsbench/random_natsbench_tss_far_exp_runner.py
+++ archai/algos/random_natsbench/random_natsbench_tss_far_exp_runner.py
@@ -1,6 +1,6 @@
 
 
-import overrides
+from overrides import overrides
 from typing import Optional, Type, Tuple
 
 from archai.nas.exp_runner import ExperimentRunner
@@ -14,9 +14,20 @@ from archai.nas.searcher import Searcher, SearchResult
 from archai.nas.finalizers import Finalizers
 from archai.nas.random_finalizers import RandomFinalizers
 from archai.nas.model_desc_builder import ModelDescBuilder
+from archai.algos.random_natsbench.random_natsbench_tss_far_searcher import RandomNatsbenchTssFarSearcher
 
 
-class RandomNatsbenchTssExpRunner(ExperimentRunner):
+class RandomNatsbenchTssFarExpRunner(ExperimentRunner):
+    ''' Runs random search using FastArchRank on Natsbench space '''
+
+    @overrides
+    def model_desc_builder(self)->Optional[ModelDescBuilder]:
+        return None
+
+    @overrides
+    def trainer_class(self)->TArchTrainer:
+        return None # no search trainer
+
 
     @overrides
     def run_search(self, conf_search:Config)->SearchResult:
@@ -30,11 +41,11 @@ class RandomNatsbenchTssExpRunner(ExperimentRunner):
 
     @overrides
     def searcher(self)->Searcher:
-        return RandomNatsbenchSearcher()
+        return RandomNatsbenchTssFarSearcher()
 
     @overrides
     def evaluater(self)->Evaluater:
-        return RandomNatsbenchEvaluater()
+        return None
 
     
 
