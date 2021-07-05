d5d02886b74f05093d75e26cf5f4564c92bf4c1e
Debadeepta Dey <dedey@microsoft.com>
2021-07-05 08:13:48 -0700

    Darts space evaluater needed its own evaluater.

--- archai/algos/proxynas/darts_space_evaluater.py
+++ archai/algos/proxynas/darts_space_evaluater.py
@@ -32,14 +32,14 @@ from archai.algos.proxynas.conditional_trainer import ConditionalTrainer
 from archai.algos.proxynas.constant_darts_space_sampler import ConstantDartsSpaceSampler
 from archai.algos.random.random_model_desc_builder import RandomModelDescBuilder
 
-class FreezeDartsSpaceEvaluater(Evaluater):
+class DartsSpaceEvaluater(Evaluater):
     @overrides
     def create_model(self, conf_eval:Config, model_desc_builder:RandomModelDescBuilder,
                       final_desc_filename=None, full_desc_filename=None)->nn.Module:
         
-        assert model_desc_builder is not None, 'FreezeDartsSpaceEvaluater requires model_desc_builder'
-        assert final_desc_filename is None, 'FreezeDartsSpaceEvaluater creates its own model desc based on arch index'
-        assert type(model_desc_builder) == RandomModelDescBuilder, 'FreezeDartsSpaceEvaluater requires RandomModelDescBuilder'
+        assert model_desc_builder is not None, 'DartsSpaceEvaluater requires model_desc_builder'
+        assert final_desc_filename is None, 'DartsSpaceEvaluater creates its own model desc based on arch index'
+        assert type(model_desc_builder) == RandomModelDescBuilder, 'DartsSpaceEvaluater requires RandomModelDescBuilder'
 
         # region conf vars
         # if not explicitly passed in then get from conf
@@ -75,38 +75,3 @@ class FreezeDartsSpaceEvaluater(Evaluater):
                     'n_nodes': conf_model_desc['cell']['n_nodes']})
 
         return model
-
-    @overrides
-    def train_model(self, conf_train:Config, model:nn.Module,
-                    checkpoint:Optional[CheckPoint])->Metrics:
-        conf_loader = conf_train['loader']
-        conf_train_cond = conf_train['trainer']
-        conf_train_freeze = conf_train['freeze_trainer']
-
-        # NOTE: we don't pass checkpoint to the trainers
-        # as it creates complications and we don't need it
-        # as these trainers are quite fast
-        checkpoint = None
-
-        logger.pushd('conditional_training')
-        data_loaders = self.get_data(conf_loader)
-        # first regular train until certain accuracy is achieved
-        cond_trainer = ConditionalTrainer(conf_train_cond, model, checkpoint, None)
-        cond_trainer_metrics = cond_trainer.fit(data_loaders)
-        logger.popd()
-
-        # get data with new batch size for freeze training
-        # NOTE: important to create copy and modify as otherwise get_data will return
-        # a cached data loader by hashing the id of conf_loader
-        conf_loader_freeze = deepcopy(conf_loader)
-        conf_loader_freeze['train_batch'] = conf_loader['freeze_loader']['train_batch']
-
-        logger.pushd('freeze_training')
-        data_loaders = self.get_data(conf_loader_freeze)
-        # now just finetune the last few layers
-        checkpoint = None
-        trainer = FreezeTrainer(conf_train_freeze, model, checkpoint)
-        freeze_train_metrics = trainer.fit(data_loaders)
-        logger.popd()
-
-        return freeze_train_metrics
