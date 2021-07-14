f332903deeb1e372a65b67f7265890676e7d6b09
Debadeepta Dey <dedey@microsoft.com>
2021-07-13 20:40:26 -0700

    Added an utility function to convert ModelDesc to Nasbench301 genotype!

--- archai/nas/model_desc_builder.py
+++ archai/nas/model_desc_builder.py
@@ -138,7 +138,7 @@ class ModelDescBuilder(EnforceOverrides):
 
         if self.template is None:
             node_count = self.get_node_count(cell_index)
-            in_shape = stem_shapes[0] # input shape to noded is same as cell stem
+            in_shape = stem_shapes[0] # input shape to nodes is same as cell stem
             out_shape = stem_shapes[0] # we ask nodes to keep the output shape same
             node_shapes, nodes = self.build_nodes(stem_shapes, conf_cell,
                                                   cell_index, cell_type, node_count, in_shape, out_shape)
