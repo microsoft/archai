998066e575bd6a5e3888f926c63172813bbf1598
Gustavo Rosa <gth.rosa@uol.com.br>
2021-09-01 16:17:16 -0300

    fix(setup): Removes unused import and bumps the version to one after latest release.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-import setuptools, platform
+import setuptools
 
-with open("README.md", "r", encoding='utf_8') as fh:
+with open('README.md', 'r', encoding='utf_8') as fh:
     long_description = fh.read()
 
-install_requires=[
+install_requires = [
     'pystopwatch2 @ git+https://github.com/ildoonet/pystopwatch2.git',
-    'hyperopt', #  @ git+https://github.com/hyperopt/hyperopt.git
+    'hyperopt',  # @ git+https://github.com/hyperopt/hyperopt.git
     'tensorwatch>=0.9.1', 'tensorboard',
     'pretrainedmodels', 'tqdm', 'sklearn', 'matplotlib', 'psutil',
     'requests', 'seaborn', 'h5py', 'rarfile',
@@ -17,20 +17,20 @@ install_requires=[
 ]
 
 setuptools.setup(
-    name="archai",
-    version="0.4.2",
-    author="Shital Shah, Debadeepta Dey,",
-    author_email="shitals@microsoft.com, dedey@microsoft.com",
-    description="Research platform for Neural Architecture Search",
+    name='archai',
+    version='0.5.2',
+    author='Shital Shah, Debadeepta Dey,',
+    author_email='shitals@microsoft.com, dedey@microsoft.com',
+    description='Research platform for Neural Architecture Search',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sytelus/archai",
+    long_description_content_type='text/markdown',
+    url='https://github.com/sytelus/archai',
     packages=setuptools.find_packages(),
-	license='MIT',
+        license='MIT',
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ),
     include_package_data=True,
     install_requires=install_requires
