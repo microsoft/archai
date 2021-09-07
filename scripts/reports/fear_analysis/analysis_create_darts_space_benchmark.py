cc1f417171824b672a4c96e9dcb42c8d233fbe2d
Debadeepta Dey <dedey@microsoft.com>
2021-09-06 21:26:22 -0700

    Added jobs for DARTS FEAR experiments.

--- scripts/reports/fear_analysis/analysis_create_darts_space_benchmark.py
+++ scripts/reports/fear_analysis/analysis_create_darts_space_benchmark.py
@@ -70,7 +70,7 @@ def main():
     #     a = parse_a_job(job_dir)
 
     # parallel parsing of yaml logs
-    num_workers = 9
+    num_workers = 8
     with Pool(num_workers) as p:
         a = p.map(parse_a_job, job_dirs)
 
