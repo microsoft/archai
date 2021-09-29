461e0779028c9b4353eab23a46ec4f915a3974c5
Shital Shah <shitals@microsoft.com>
2021-09-29 14:48:07 -0700

    rewrite bbpe vocab to load/save from new json file

--- archai/nlp/tokenizer_utils/bbpe_vocab.py
+++ archai/nlp/tokenizer_utils/bbpe_vocab.py
@@ -8,10 +8,10 @@ import json
 from overrides import overrides
 
 from tokenizers import ByteLevelBPETokenizer
+from transformers import PreTrainedTokenizerFast
 
 from archai.nlp.nvidia_transformer_xl import nvidia_utils as nv_utils
 from archai.nlp.tokenizer_utils.vocab_base import VocabBase
-from archai.nlp.tokenizer_utils.tokenizer_files import TokenizerFiles
 from archai.nlp.tokenizer_utils.token_config import TokenConfig
 from archai.common import utils, common
 from archai.nlp.tokenizer_utils.special_token_enum import SpecialTokenEnum
@@ -24,12 +24,12 @@ class BbpeVocab(VocabBase):
         self._config = TokenConfig(bos_token=bos_token, eos_token=eos_token,
                                    unk_token=unk_token, pad_token=None,
                                    add_prefix_space=add_prefix_space, add_prefix_new_line=add_prefix_new_line)
-        self._files = TokenizerFiles.from_path(save_path)
-        self._tokenizer:Optional[ByteLevelBPETokenizer] = None # will load existing or create new
-        self.save_path = utils.full_path(save_path, create=True) if save_path else save_path
+        self._tokenizer:Optional[PreTrainedTokenizerFast] = None # will load existing or create new
+        self._tokenizer_filepath = os.path.join(utils.full_path(save_path, create=True), 'bbpe_tokenizer.json') if save_path else save_path
         self.vocab_size = vocab_size
         self.sorted_vocab = sorted_vocab
         self.min_frequency = min_frequency
+
         self.bos_id = []
         self.eos_id = []
 
@@ -41,53 +41,83 @@ class BbpeVocab(VocabBase):
     def train(self, filepaths: List[str]) -> None:
         with nv_utils.distributed.sync_workers() as rank:
             if rank == 0:
-                logging.info(f'Training BBPE Vocab for size {self.vocab_size} at "{self.save_path}" ...')
-                _train_tokenizer(filepaths, self._config, vocab_size=self.vocab_size, save_dir=self.save_path,
+                logging.info(f'Training BBPE Vocab for size {self.vocab_size} at "{self._tokenizer_filepath}" ...')
+                _train_tokenizer(filepaths, self._config, vocab_size=self.vocab_size, save_filepath=self._tokenizer_filepath,
                     min_frequency=self.min_frequency if self.min_frequency is not None else 2) # 2 is Huggingface default
 
                 if self.sorted_vocab:
                     self.load()
-                    counter = _count_token_freq(filepaths, self._tokenizer, self._config, self.bos_id, self.eos_id)
-                    _save_sort_tokens(counter, self._tokenizer, self._config, self._files)
+                    self._rewrite_json_sorted(filepaths)
         self.load()
 
     @overrides
     def special_token_id(self, sp:SpecialTokenEnum)->Optional[int]:
-        return self._tokenizer.token_to_id(self._config.special_token_name(sp))
+        return self.token_to_id(self._config.special_token_name(sp))
 
     @overrides
     def token_to_id(self, t:str)->int:
-        return self._tokenizer.token_to_id(t)
+        return self._tokenizer.convert_tokens_to_ids(t)
 
     @overrides
     def id_to_token(self, id:int)->str:
-        return self._tokenizer.id_to_token(id)
+        return self._tokenizer.convert_ids_to_tokens(id)
 
     @overrides
     def load(self)->None:
-        self._tokenizer = _create_tokenizer(self._files, self._config)
+        self._tokenizer = _load_tokenizer(self._tokenizer_filepath, self._config)
         self._finalize_tokenizer()
 
-        self.bos_id = [] if not self._config.bos_token else [self._tokenizer.token_to_id(self._config.bos_token)]
-        self.eos_id = [] if not self._config.eos_token else [self._tokenizer.token_to_id(self._config.eos_token)]
+        self.bos_id = [] if not self._config.bos_token else [self.token_to_id(self._config.bos_token)]
+        self.eos_id = [] if not self._config.eos_token else [self.token_to_id(self._config.eos_token)]
 
-        logging.info(f'tokenizer len: {self._tokenizer.get_vocab_size()}')
-        logging.info(f'merges_file: {self._files.merges_file}')
-        logging.info(f'vocab_file: {self._files.vocab_file}')
+        logging.info(f'tokenizer len: {len(self._tokenizer)}')
+        logging.info(f'tokenizer filepath: {self._tokenizer_filepath}')
+
+    def _rewrite_json_sorted(self, filepaths:List[str]):
+
+        tokens_counter = _count_token_freq(filepaths, self._tokenizer, self._config,
+                                    self.bos_id, self.eos_id)
+
+        logging.info('Saving sorted vocab file...')
+        tokens_counter.update(list(range(len(self._tokenizer)))) # add 1 to each value, to ensure that all of them > 0
+        min_sort_id = 256 + len(self._config.get_special_tokens())
+        sorted_ids = list(range(min_sort_id)) + \
+                            [int(token_id) for token_id, _ in tokens_counter.most_common() if int(token_id) >= min_sort_id]
+
+        t_map = [(new, old) for new, old in enumerate(sorted_ids)]
+        t_map.sort(key=lambda t:t[1])
+        orig2sorted_ids = [t[0] for t in t_map]
+
+        with open(self._tokenizer_filepath, encoding="utf-8") as f:
+            tok_json = json.load(f)
+        vocab_orig = tok_json['model']['vocab']
+
+        assert len(vocab_orig) == len(orig2sorted_ids)
+        v_map = OrderedDict([(vocab, orig2sorted_ids[idx]) for vocab, idx in vocab_orig.items()])
+
+        # save unsorted file
+        utils.copy_file(self._tokenizer_filepath, self._tokenizer_filepath + '.unsorted.json')
+
+        tok_json['model']['vocab'] = v_map
+        with open(self._tokenizer_filepath, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(tok_json, ensure_ascii=False, indent=2))
 
     def _finalize_tokenizer(self):
         # TODO: EOT is supposed to be added at the end of the file but currently its not done
         # self.EOT = self.tokenizer.bos_token_id # .encoder['<|endoftext|>']
+
+        # TODO: measure impact of padding and remove additional complexity
         if self.pad_vocab_size:
-            vocab_size = self._tokenizer.get_vocab_size()
+            vocab_size = len(self._tokenizer)
             self.padded_vocab_size = (vocab_size + self.pad - 1) // self.pad * self.pad
-            for i in range(0, padded_vocab_size - vocab_size):
+            for i in range(0, self.padded_vocab_size - vocab_size):
                 token = f'madeupword{i:09d}'
                 self._tokenizer.add_tokens([token])
 
     @overrides
     def is_trained(self)->bool:
-        return TokenizerFiles.files_exists(self.save_path)
+        # if any files exist in the directory
+        return os.path.isfile(self._tokenizer_filepath)
 
     @overrides
     def encode_line(self, line)->List[int]:
@@ -100,7 +130,7 @@ class BbpeVocab(VocabBase):
 
     @overrides
     def __len__(self):
-        return self._tokenizer.get_vocab_size()
+        return len(self._tokenizer)
 
 def _encode_line(line:str, token_config:TokenConfig, tokenizer:ByteLevelBPETokenizer,
                 bos_id:List[int], eos_id:List[int])->List[int]:
@@ -111,13 +141,13 @@ def _encode_line(line:str, token_config:TokenConfig, tokenizer:ByteLevelBPEToken
         line = ' ' + line
     if token_config.add_prefix_new_line:
         line = '\n' + line
-    return bos_id + tokenizer.encode(line).ids + eos_id
+    return bos_id + tokenizer.encode(line) + eos_id
 
-def _count_token_freq(filepaths:List[str], tokenizer:ByteLevelBPETokenizer, token_config:TokenConfig,
+def _count_token_freq(filepaths:List[str], tokenizer:PreTrainedTokenizerFast, token_config:TokenConfig,
                      bos_id:List[int], eos_id:List[int])->Counter:
     logging.info('Counting token frequencies...')
     tokens_counter = Counter()
-    tokens_counter.update(list(range(tokenizer.get_vocab_size()))) # add each token
+    tokens_counter.update(list(range(len(tokenizer)))) # add each token
 
     for filepath in filepaths:
         with open(filepath, 'r', encoding='utf-8') as f:
@@ -131,45 +161,16 @@ def _count_token_freq(filepaths:List[str], tokenizer:ByteLevelBPETokenizer, toke
 
     return tokens_counter
 
-def _save_sort_tokens(tokens_counter:Counter, tokenizer:ByteLevelBPETokenizer,
-                    token_config:TokenConfig, tokenizer_files:TokenizerFiles):
-    logging.info('Saving sorted vocab file...')
-    tokens_counter.update(list(range(tokenizer.get_vocab_size()))) # add 1 to each value, to ensure that all of them > 0
-    min_sort_id = 256 + len(token_config.get_special_tokens())
-    sorted_ids = list(range(min_sort_id)) + \
-                        [int(token_id) for token_id, _ in tokens_counter.most_common() if int(token_id) >= min_sort_id]
-
-    t_map = [(new, old) for new, old in enumerate(sorted_ids)]
-    t_map.sort(key=lambda t:t[1])
-    orig2sorted_ids = [t[0] for t in t_map]
-
-    with open(tokenizer_files.vocab_file, encoding="utf-8") as f:
-        vocab_orig = json.load(f)
-
-    assert len(vocab_orig) == len(orig2sorted_ids)
-    v_map = OrderedDict([(vocab, orig2sorted_ids[idx]) for vocab, idx in vocab_orig.items()])
-
-    utils.copy_file(tokenizer_files.vocab_file, tokenizer_files.vocab_file + '.unsorted.json')
-
-    with open(tokenizer_files.vocab_file, 'w', encoding="utf-8") as f:
-        f.write(json.dumps(v_map, ensure_ascii=False))
-
 def _train_tokenizer(filepaths:List[str], token_config: TokenConfig,
-                    vocab_size: int, save_dir: str, save_prefix='tokenizer',
-                    dropout: float = None, min_frequency: int = 2, show_progress=False,
-                    added_tokens: List[str] = []) -> TokenizerFiles:
+                    vocab_size: int, save_filepath: str,
+                    min_frequency:int, dropout: float = None, # min_frequency default is 2
+                    added_tokens: List[str] = []) -> None:
     logging.info('Training tokenizer...')
-    # check if we already have tokenizer cached filed
-    tokenizer_out_files = TokenizerFiles.from_path(save_dir=save_dir)
-    # if utils.is_debugging() and os.path.exists(tokenizer_out_files.vocab_file) \
-    #         and os.path.exists(tokenizer_out_files.merges_file):
-    #     logging.info(f'Found BBPE tokenizer cached files at "{save_dir}", reusing them.')
-    #     return tokenizer_out_files
 
     special_tokens = token_config.get_special_tokens()
 
     # TODO: measure impact of dropout
-    tokenizer = ByteLevelBPETokenizer(dropout=dropout, add_prefix_space=token_config.add_prefix_space)ens)
+    tokenizer = ByteLevelBPETokenizer(dropout=dropout, add_prefix_space=token_config.add_prefix_space)
     tokenizer.train(files=filepaths, vocab_size=vocab_size, min_frequency=min_frequency,
         special_tokens=special_tokens)
 
@@ -178,13 +179,15 @@ def _train_tokenizer(filepaths:List[str], token_config: TokenConfig,
         tokenizer.add_tokens(added_tokens)
 
     # generates save_prefix-vocab.json and save_prefix-merges.txt
-    tokenizer.save_model(save_dir, save_prefix)
+    tokenizer.save(save_filepath, pretty=True)
 
-    return tokenizer_out_files
-
-def _create_tokenizer(tokenizer_files:TokenizerFiles, token_config: TokenConfig, max_length=1024)->ByteLevelBPETokenizer:
-    tokenizer = ByteLevelBPETokenizer.from_file(tokenizer_files.vocab_file, tokenizer_files.merges_file)
+def _load_tokenizer(save_filepath:str, token_config: TokenConfig)->PreTrainedTokenizerFast:
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=save_filepath)
 
     # TODO: below shouldn't be required: https://github.com/huggingface/transformers/issues/664
     #tokenizer.padding_side = "left"
+
+    # TODO: check is loaded tokenizer has same settings as in vocab constructor
+
     return tokenizer
+
