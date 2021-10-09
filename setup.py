5a0f96499f660363f82144b4299248f9556963e7
Shital Shah <shitals@microsoft.com>
2021-10-09 00:26:21 -0700

    merge from nlp branch

--- setup.py
+++ setup.py
@@ -1,37 +1,41 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-import setuptools
+import setuptools, platform
 
-with open('README.md', 'r', encoding='utf_8') as fh:
+with open("README.md", "r", encoding='utf_8') as fh:
     long_description = fh.read()
 
-install_requires = [
+install_requires=[
     'pystopwatch2 @ git+https://github.com/ildoonet/pystopwatch2.git',
-    'hyperopt',
+    'hyperopt', #  @ git+https://github.com/hyperopt/hyperopt.git
     'tensorwatch>=0.9.1', 'tensorboard',
     'pretrainedmodels', 'tqdm', 'sklearn', 'matplotlib', 'psutil',
     'requests', 'seaborn', 'h5py', 'rarfile',
-    'gorilla', 'pyyaml', 'overrides<4.0.0', 'runstats', 'psutil', 'statopt',
-    'pyunpack', 'patool', 'ray>=1.0.0', 'Send2Trash'
+    'gorilla', 'pyyaml', 'overrides', 'runstats', 'psutil', 'statopt',
+    'pyunpack', 'patool', 'ray>=1.0.0', 'Send2Trash',
+    'transformers', 'pytorch_lightning', 'tokenizers', 'datasets',
+    'ftfy', # needed for text scoring, fixes text for you
+    # nvidia transformer-xl
+    'dllogger @ git+https://github.com/NVIDIA/dllogger.git',
+    'pytorch-transformers', 'sacremoses', 'pynvml'
 ]
 
 setuptools.setup(
-    name='archai',
-    version='0.5.2',
-    author='Shital Shah, Debadeepta Dey,',
-    author_email='shitals@microsoft.com, dedey@microsoft.com',
-    description='Research platform for Neural Architecture Search',
+    name="archai",
+    version="0.5.0",
+    author="Shital Shah, Debadeepta Dey,",
+    author_email="shitals@microsoft.com, dedey@microsoft.com",
+    description="Research platform for Neural Architecture Search",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/microsoft/archai',
+    long_description_content_type="text/markdown",
+    url="https://github.com/sytelus/archai",
     packages=setuptools.find_packages(),
-        license='MIT',
+	license='MIT',
     classifiers=(
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ),
     include_package_data=True,
     install_requires=install_requires
