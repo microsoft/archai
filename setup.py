3b58284e95cb8e1a9281738f8aa655d416c82847
Shital Shah <shitals@microsoft.com>
2021-10-09 00:26:21 -0700

    merge from nlp branch

--- setup.py
+++ setup.py
@@ -1,45 +1,42 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from setuptools import find_packages, setup
+import setuptools, platform
 
 with open("README.md", "r", encoding='utf_8') as fh:
     long_description = fh.read()
 
 install_requires=[
-    'pystopwatch2',
-    'hyperopt',
+    'pystopwatch2 @ git+https://github.com/ildoonet/pystopwatch2.git',
+    'hyperopt', #  @ git+https://github.com/hyperopt/hyperopt.git
     'tensorwatch>=0.9.1', 'tensorboard',
     'pretrainedmodels', 'tqdm', 'sklearn', 'matplotlib', 'psutil',
     'requests', 'seaborn', 'h5py', 'rarfile',
     'gorilla', 'pyyaml', 'overrides', 'runstats', 'psutil', 'statopt',
     'pyunpack', 'patool', 'ray>=1.0.0', 'Send2Trash',
-    'transformers>=4.16.2', 'pytorch_lightning', 'tokenizers>=0.10.3', 'datasets',
-    'onnx==1.10.2', 'onnxruntime', 'coloredlogs', 'sympy',
+    'transformers', 'pytorch_lightning', 'tokenizers', 'datasets',
     'ftfy', # needed for text scoring, fixes text for you
     # nvidia transformer-xl
     'dllogger @ git+https://github.com/NVIDIA/dllogger.git',
-    'pytorch-transformers', 'sacremoses', 'pynvml',
-    'plotly', 'kaleido'
+    'pytorch-transformers', 'sacremoses', 'pynvml'
 ]
 
-setup(
+setuptools.setup(
     name="archai",
-    version="0.6.0",
-    author="Shital Shah, Debadeepta Dey",
+    version="0.5.0",
+    author="Shital Shah, Debadeepta Dey,",
     author_email="shitals@microsoft.com, dedey@microsoft.com",
     description="Research platform for Neural Architecture Search",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/microsoft/archai",
+    url="https://github.com/sytelus/archai",
     packages=setuptools.find_packages(),
 	license='MIT',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Science/Research'
-    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
