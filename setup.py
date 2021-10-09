1b2d55c6ee61f4693e0e05341b6692af65d7f0d8
Shital Shah <shitals@microsoft.com>
2021-10-09 00:29:56 -0700

    sync setup.py from master

--- setup.py
+++ setup.py
@@ -23,20 +23,21 @@ install_requires=[
 
 setuptools.setup(
     name="archai",
-    version="0.5.0",
-    author="Shital Shah, Debadeepta Dey,",
+    version="0.6.0",
+    author="Shital Shah, Debadeepta Dey",
     author_email="shitals@microsoft.com, dedey@microsoft.com",
     description="Research platform for Neural Architecture Search",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sytelus/archai",
+    url="https://github.com/microsoft/archai",
     packages=setuptools.find_packages(),
 	license='MIT',
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research'
+    ],
     include_package_data=True,
     install_requires=install_requires
 )
