79c8a29aecc772a4aae0ef55431aea477ec314ac
Debadeepta Dey <debadeepta@users.noreply.github.com>
2021-11-01 11:38:27 -0700

    Forced overrides version to be 3.1.0.
    Later versions of overrides has stricter signature checking which fails with archai core class heirarchy.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires=[
     'tensorwatch>=0.9.1', 'tensorboard',
     'pretrainedmodels', 'tqdm', 'sklearn', 'matplotlib', 'psutil',
     'requests', 'seaborn', 'h5py', 'rarfile',
-    'gorilla', 'pyyaml', 'overrides', 'runstats', 'psutil', 'statopt',
+    'gorilla', 'pyyaml', 'overrides==3.1.0', 'runstats', 'psutil', 'statopt',
     'pyunpack', 'patool', 'ray>=1.0.0', 'Send2Trash',
     'transformers', 'pytorch_lightning', 'tokenizers', 'datasets',
     'ftfy', # needed for text scoring, fixes text for you
