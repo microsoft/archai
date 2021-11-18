e61b3dcfda01face7d32359befac6a46e7336c07
Caio Mendes <caioctmendes@gmail.com>
2021-11-18 21:59:37 +0000

    Added license and changed to startswith

--- archai/nlp/nvidia_transformer_xl/nvidia_utils/cyclic_cosine_scheduler.py
+++ archai/nlp/nvidia_transformer_xl/nvidia_utils/cyclic_cosine_scheduler.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2019 abhuse
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from collections.abc import Iterable
 from math import log, cos, pi, floor
 
@@ -5,6 +27,13 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CyclicCosineDecayLR(_LRScheduler):
+    """ Implements Cyclic Cosine annealing.
+
+        This learning rate scheduler is useful when doing QAT provinding
+        a ~0.3 ppl boost over the traditional cosine annealing scheduler.
+
+        Code and additional documentation: https://github.com/abhuse/cyclic-cosine-decay
+    """
     def __init__(self,
                  optimizer,
                  init_decay_epochs,
