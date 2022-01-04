ae1ac1f65816bb954eb7f28979dcd4bbba5cc752
Gustavo de Rosa <gth.rosa@uol.com.br>
2022-01-04 22:31:10 +0000

    chore(nlp): Adds remaining files which were moved or had paths changed.

--- scripts/nlp/test_score.py
+++ scripts/nlp/test_score.py
@@ -7,8 +7,8 @@ import torch
 
 from archai.nlp.scoring import score
 from archai.nlp.models.mem_transformer import MemTransformerLM
-from archai.nlp.models.data_utils import get_lm_corpus
-from archai.nlp.models.nvidia_utils import exp_utils
+from archai.nlp.datasets.distributed_utils.data_utils import get_lm_corpus
+from archai.nlp.datasets import exp_utils
 from archai.common import utils, common
 from archai.nlp.tokenizer_utils.vocab_base import VocabBase
 
