9b6eac434eb7f5b382c82f692b0ee88763190dc8
Gustavo de Rosa <gth.rosa@uol.com.br>
2022-01-27 20:53:21 +0000

    chore(models): Adds default search configuration to models' config.

--- archai/nlp/models/mem_transformer/config_mem_transformer.py
+++ archai/nlp/models/mem_transformer/config_mem_transformer.py
@@ -69,6 +69,7 @@ class MemTransformerLMConfig(Config):
             'use_cache': False
         }
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     @property
     def search(self) -> Dict[str, Any]:
@@ -80,3 +81,18 @@ class MemTransformerLMConfig(Config):
         }
 =======
 >>>>>>> b64935ed (chore(nlp): Adds configuration classes to every available model.)
+=======
+
+    @property
+    def search(self) -> Dict[str, Any]:
+        """Defines the default configuration used when searching with the class.
+
+        """
+
+        return {
+            'n_layer': [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            'd_model': [128, 256, 512, 768, 1024],
+            'd_inner': list(range(512, 2049, 50))+list(range(2048, 3072, 200)),
+            'n_head': [2, 4, 8]
+        }
+>>>>>>> 3a5c71c5 (chore(models): Adds default search configuration to models' config.)
