fix(hf_gpt2): Fixes embedding parameters calculation.

archai/nlp/models/hf_gpt2/model_hf_gpt2.py
@@ -148,7 +148,7 @@ class HfGPT2(ArchaiModel):
 
         params = {}
 
-        params['embedding'] = self.get_params_from_layer(['nn.Embedding'])
+        params['embedding'] = self.get_params_from_layer(['Embedding'])
         params['attention'] = self.get_params_from_layer(['GPT2Attention'])
         params['ff'] = self.get_params_from_layer(['GPT2MLP'])
 