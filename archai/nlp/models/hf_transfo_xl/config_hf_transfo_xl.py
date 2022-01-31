58d269f3b1974e37be857e80b80ab70c5d5b3e6c
Gustavo de Rosa <gth.rosa@uol.com.br>
2022-01-31 13:20:53 +0000

    chore(models): Starts to rework `models` package to comply with latest merges.

---
+++ archai/nlp/models/hf_transfo_xl/config_hf_transfo_xl.py
@@ -0,0 +1,63 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Hugginface's Transformer-XL configuration.
+"""
+
+from typing import Any, Dict
+
+from archai.nlp.models.config_base import Config
+
+
+class HfTransfoXLConfig(Config):
+    """Provides a configuration for HfTransfoXL.
+
+    """
+
+    def __init__(self, **kwargs) -> None:
+        """Initializes the configuration.
+
+        """
+
+        super().__init__(**kwargs)
+
+    @property
+    def default(self) -> Dict[str, Any]:
+        """Defines the default configuration used by the class.
+
+        """
+
+        return {
+            'd_head': None,
+            'd_embed': 512,
+            'dropout': 0.1,
+            'dropatt': 0.0,
+            'n_token': 267736,
+            'div_val': 4,
+            'pre_lnorm': False,
+            'cutoffs': [19997, 39997, 199997],
+            'mem_len': 192,
+            'same_length': False,
+            'attn_type': 0,
+            'clamp_len': -1,
+            'sample_softmax': -1,
+            'adaptive': True,
+            'weight_init_type': 'normal',
+            'weight_init_range': 0.01,
+            'weight_init_std': 0.02,
+            'proj_init_std': 0.01,
+            'tie_weight': True
+        }
+
+    @property
+    def search(self) -> Dict[str, Any]:
+        """Defines the default configuration used when searching with the class.
+
+        """
+
+        return {
+            'n_layer': [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            'd_model': [128, 256, 512, 768, 1024],
+            'd_inner': list(range(512, 2049, 50)) + list(range(2048, 3072, 200)),
+            'n_head': [2, 4, 8]
+        }
---
+++ archai/nlp/models/model_dict.py
@@ -0,0 +1,46 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Dictionary that allows the implementation and usage of Transformer-based models.
+"""
+
+from archai.nlp.models.hf_gpt2.config_hf_gpt2 import HfGPT2Config
+from archai.nlp.models.hf_gpt2.model_hf_gpt2 import HfGPT2, HfGPT2Flex
+from archai.nlp.models.hf_gpt2.onnx_hf_gpt2 import HfGPT2OnnxConfig, HfGPT2OnnxModel
+
+from archai.nlp.models.mem_transformer.config_mem_transformer import MemTransformerLMConfig
+from archai.nlp.models.mem_transformer.model_mem_transformer import MemTransformerLM
+from archai.nlp.models.mem_transformer.onnx_mem_transformer import MemTransformerLMOnnxConfig, MemTransformerLMOnnxModel
+
+from archai.nlp.models.hf_transfo_xl.config_hf_transfo_xl import HfTransfoXLConfig
+from archai.nlp.models.hf_transfo_xl.model_hf_transfo_xl import HfTransfoXL
+from archai.nlp.models.hf_transfo_xl.onnx_hf_transfo_xl import HfTransfoXLOnnxConfig, HfTransfoXLOnnxModel 
+
+
+# Available models and their configurations
+MODELS = {
+    'hf_gpt2': HfGPT2,
+    'hf_gpt2_flex': HfGPT2Flex,
+    'hf_transfo_xl': HfTransfoXL,
+    'mem_transformer': MemTransformerLM
+}
+MODELS_CONFIGS = {
+    'hf_gpt2': HfGPT2Config,
+    'hf_gpt2_flex': HfGPT2OnnxConfig,
+    'hf_transfo_xl': HfTransfoXLConfig,
+    'mem_transformer': MemTransformerLMConfig
+}
+
+# Available ONNX-based models and their configurations
+ONNX_MODELS = {
+    'hf_gpt2': HfGPT2OnnxModel,
+    'hf_gpt2_flex': HfGPT2FlexOnnxModel,
+    'hf_transfo_xl': HfTransfoXLOnnxModel,
+    'mem_transformer': MemTransformerLMOnnxModel
+}
+ONNX_MODELS_CONFIGS = {
+    'hf_gpt2': HfGPT2OnnxConfig,
+    'hf_gpt2_flex': HfGPT2FlexOnnxConfig,
+    'hf_transfo_xl': HfTransfoXLOnnxConfig,
+    'mem_transformer': MemTransformerLMOnnxConfig
+}
