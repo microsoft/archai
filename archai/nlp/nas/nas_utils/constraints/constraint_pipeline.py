10cff13bac6e8c601168c68cb9238a378d40c73b
Gustavo de Rosa <gth.rosa@uol.com.br>
2022-02-08 15:36:07 +0000

    chore(nas): Changes how constraints are handled to enable future ONNX-based constraints.

---
+++ archai/nlp/nas/nas_utils/constraints/constraint_pipeline.py
@@ -0,0 +1,99 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Constraints pipelines used throughout the search procedure.
+"""
+
+from typing import Tuple, Optional
+
+import torch
+
+from archai.nlp.nas.nas_utils.constraints.torch_constraints import (measure_torch_inference_latency,
+                                                                    measure_torch_parameters,
+                                                                    measure_torch_peak_memory)
+
+
+# Latency upper bound on different device targets
+# Any model with more latency than this will be removed from consideration during search
+DEVICE_LATENCY_CONSTRAINT = {
+    'XeonE5-2690': 10.0,
+    'corei7': 10.0,
+    'corei5': 10.0,
+    'D3_V2': 10.0,
+}
+
+
+class ConstraintPipeline:
+    """
+    """
+
+    def __init__(self,
+                 use_quantization: bool,
+                 use_median: bool,
+                 seq_len: int,
+                 n_threads: int,
+                 n_trials: int,
+                 device: str) -> None:
+        """
+        """
+
+        self.use_quantization = use_quantization
+        self.use_median = use_median
+
+        self.seq_len = seq_len
+        self.n_threads = n_threads
+        self.n_trials = n_trials
+
+        self.device = device
+
+    def __call__():
+        """
+        """
+
+        raise NotImplementedError
+
+
+class TorchConstraintPipeline(ConstraintPipeline):
+    """
+    """
+
+    def __init__(self,
+                 use_quantization: Optional[bool] = False,
+                 use_median: Optional[bool] = False,
+                 seq_len: Optional[int] = 192,
+                 n_threads: Optional[int] = 1,
+                 n_trials: Optional[int] = 10,
+                 device: Optional[str] = 'cpu') -> None:
+        """
+        """
+
+        super().__init__(use_quantization, use_median, seq_len,
+                         n_threads, n_trials, device)
+
+    def __call__(self, model: torch.nn.Module) -> Tuple[int, int, float, float]:
+        """
+        """
+
+        return [
+            # Number of decoder (non-embedding) parameters
+            measure_torch_parameters(model, ['non_embedding']),
+
+            # Number of total parameters
+            measure_torch_parameters(model, ['total']),
+
+            # Inference latency
+            measure_torch_inference_latency(model,
+                                            self.use_quantization,
+                                            self.use_median,
+                                            self.seq_len,
+                                            self.n_threads,
+                                            self.n_trials,
+                                            self.device),
+
+            # Peak memory usage
+            measure_torch_peak_memory(model,
+                                      self.use_quantization,
+                                      self.seq_len,
+                                      self.n_threads,
+                                      self.device)
+        ]
