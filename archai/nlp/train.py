94c72294110d500e3938b8957400a7bb85c348c4
Debadeepta Dey <dedey@microsoft.com>
2022-02-11 19:05:27 -0800

    Commented out summaries.csv saving at the end of training as it sometimes causes errors.

--- archai/nlp/train.py
+++ archai/nlp/train.py
@@ -1243,9 +1243,10 @@ def main():
     utils.save_as_yaml(summary, os.path.join(args.work_dir, 'summary.yaml'))
     utils.save_as_yaml(model_config, os.path.join(args.work_dir, 'model_config.yaml'))
 
-    exp_results_dir = utils.full_path(os.path.join(args.work_dir, 'textpred', 'experiment_results'), create=True)
-    summary_csv_filepath = os.path.join(exp_results_dir, 'summaries.tsv')
-    utils.append_csv_file(summary_csv_filepath, list(summary.items()))
+    # NOTE: this causes random errors on cluster. commenting out for now.    
+    # exp_results_dir = utils.full_path(os.path.join(args.work_dir, 'textpred', 'experiment_results'), create=True)
+    # summary_csv_filepath = os.path.join(exp_results_dir, 'summaries.tsv')
+    # utils.append_csv_file(summary_csv_filepath, list(summary.items()))
 
     logging.info(f'Output dir: {args.work_dir}')
     dllogger.log(step=tuple(), data=summary)
