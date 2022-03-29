9355bca7091e69968d3ec3a178637d02f9c125cf
Gustavo Rosa <gth.rosa@uol.com.br>
2022-03-29 13:54:43 +0000

    chore(nlp): Improves ONNX configuration classes.

--- archai/nlp/models/hf_gpt2/onnx_hf_gpt2.py
+++ archai/nlp/models/hf_gpt2/onnx_hf_gpt2.py
@@ -4,16 +4,14 @@
 """Huggingface's Open AI GPT-2 for ONNX.
 """
 
-from collections import OrderedDict
-from typing import Any, Dict, Mapping
+from typing import Any, Dict
 
-import torch
 from onnxruntime.transformers.onnx_model_gpt2 import Gpt2OnnxModel as HfGPT2OnnxModel
 
-from archai.nlp.models.config_base import OnnxConfig
+from archai.nlp.models.config_base import OnnxConfigWithPast
 
 
-class HfGPT2OnnxConfig(OnnxConfig):
+class HfGPT2OnnxConfig(OnnxConfigWithPast):
     """Huggingface's Open AI GPT-2 ONNX-based configuration.
 
     """
@@ -27,38 +25,4 @@ class HfGPT2OnnxConfig(OnnxConfig):
 
         """
 
-        model_config['past_key_values'] = 2
-        model_config['model_type'] = 'gpt2'
-
-        super().__init__(model_config)
-
-    @property
-    def mockups(self) -> Mapping[str, torch.Tensor]:
-        input_ids = torch.randint(0, self.config.n_token, (self.batch_size, self.seq_len))
-
-        # Shape of past states
-        # [past_key_values, batch_size, n_head, past_seq_len, d_head]
-        past_key_values =  tuple([torch.zeros(self.config.past_key_values, self.batch_size, self.config.n_head, self.seq_len, self.config.d_head) for _ in range(self.config.n_layer)])
-
-        return OrderedDict({'input_ids': input_ids, 'past_key_values': past_key_values})
-
-    @property
-    def inputs(self) -> Mapping[str, Mapping[int, str]]:
-        input_ids = [('input_ids', {0: 'batch_size', 1: 'seq_len'})]
-
-        # Shape of past states
-        # [past_key_values, batch_size, n_head, past_seq_len, d_head]
-        past_key_values = [(f'past_{i}', {1: 'batch_size', 3: 'past_seq_len'}) for i in range(self.config.n_layer)]
-
-        return OrderedDict(input_ids + past_key_values)
-
-    @property
-    def outputs(self) -> Mapping[str, Mapping[int, str]]:
-        probs = [('probs', {0: 'batch_size'})]
-
-        # Shape of present states (past states when outputting)
-        # [past_key_values, batch_size, n_head, total_seq_len, d_head]
-        # Note that total_seq_len is seq_len + past_seq_len
-        present_key_values = [(f'present_{i}', {1: 'batch_size', 3: 'total_seq_len'}) for i in range(self.config.n_layer)]
-
-        return OrderedDict(probs + present_key_values)
+        super().__init__(model_config, model_type='gpt2', past_key_values=2)
--- archai/nlp/models/mem_transformer/onnx_mem_transformer.py
+++ archai/nlp/models/mem_transformer/onnx_mem_transformer.py
@@ -4,10 +4,8 @@
 """NVIDIA's Memory Transformer for ONNX.
 """
 
-from collections import OrderedDict
-from typing import Any, Dict, List, Mapping, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
-import torch
 from onnx import (GraphProto, ModelProto, NodeProto, TensorProto,
                   ValueInfoProto, helper)
 from onnxruntime.transformers.fusion_attention import (AttentionMask,
@@ -21,10 +19,10 @@ from onnxruntime.transformers.fusion_utils import FusionUtils
 from onnxruntime.transformers.onnx_model import OnnxModel
 
 from archai.nlp.compression.onnx.onnx_utils.fusion_options import FusionOptions
-from archai.nlp.models.config_base import OnnxConfig
+from archai.nlp.models.config_base import OnnxConfigWithPast
 
 
-class MemTransformerLMOnnxConfig(OnnxConfig):
+class MemTransformerLMOnnxConfig(OnnxConfigWithPast):
     """NVIDIA's Memory Transformer ONNX-based configuration.
 
     """
@@ -38,50 +36,17 @@ class MemTransformerLMOnnxConfig(OnnxConfig):
 
         """
 
-        model_config['d_head'] = model_config['d_model'] // model_config['n_head']
-        model_config['model_type'] = 'transfo-xl'
-
         # Checks the type of attention to define the `past_key_values`
         if model_config['attn_type'] == 0:
             # `k`, `v` and relative embeddings
-            model_config['past_key_values'] = 3
+            past_key_values = 3
         else:
             # `k` and `v`
-            model_config['past_key_values'] = 2
+            past_key_values = 2
 
-        super().__init__(model_config)
-
-    @property
-    def mockups(self) -> Mapping[str, torch.Tensor]:
-        input_ids = torch.randint(0, self.config.n_token, (self.batch_size, self.seq_len))
-
-        # Shape of past states
-        # [past_key_values, batch_size, n_head, past_seq_len, d_head]
-        past_key_values =  tuple([torch.zeros(self.config.past_key_values, self.batch_size, self.config.n_head, self.seq_len, self.config.d_head) for _ in range(self.config.n_layer)])
-
-        return OrderedDict({'input_ids': input_ids, 'past_key_values': past_key_values})
-
-    @property
-    def inputs(self) -> Mapping[str, Mapping[int, str]]:
-        input_ids = [('input_ids', {0: 'batch_size', 1: 'seq_len'})]
-
-        # Shape of past states
-        # [past_key_values, batch_size, n_head, past_seq_len, d_head]
-        past_key_values = [(f'past_{i}', {1: 'batch_size', 3: 'past_seq_len'}) for i in range(self.config.n_layer)]
-
-        return OrderedDict(input_ids + past_key_values)
-
-    @property
-    def outputs(self) -> Mapping[str, Mapping[int, str]]:
-        probs = [('probs', {0: 'batch_size'})]
-
-        # Shape of present states (past states when outputting)
-        # [past_key_values, batch_size, n_head, total_seq_len, d_head]
-        # Note that total_seq_len is seq_len + past_seq_len
-        present_key_values = [(f'present_{i}', {1: 'batch_size', 3: 'total_seq_len'}) for i in range(self.config.n_layer)]
-
-        return OrderedDict(probs + present_key_values)
-
+        super().__init__(model_config,
+                         model_type='transfo-xl',
+                         past_key_values=past_key_values)
 
     
 class MemTransformerLMOnnxModel(OnnxModel):
