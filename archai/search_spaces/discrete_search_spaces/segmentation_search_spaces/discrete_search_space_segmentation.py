e00f90f1ec9181eda4817e3022be04324c5c5987
Debadeepta Dey <dedey@microsoft.com>
2022-04-21 17:00:57 -0700

    The pipeline runs through nominally! But lots of work to be done still.

--- archai/search_spaces/discrete_search_spaces/segmentation_search_spaces/discrete_search_space_segmentation.py
+++ archai/search_spaces/discrete_search_spaces/segmentation_search_spaces/discrete_search_space_segmentation.py
@@ -4,6 +4,8 @@ from overrides.overrides import overrides
 import copy
 import uuid
 
+import torch
+
 from archai.nas.arch_meta import ArchWithMetaData
 from archai.nas.discrete_search_space import DiscreteSearchSpace
 
@@ -50,7 +52,7 @@ class DiscreteSearchSpaceSegmentation(DiscreteSearchSpace):
         # and change its operator at random
         # and its input sources
         # WARNING: this can result in some nodes left hanging
-        chosen_node_idx = random.randint(1, len(graph))
+        chosen_node_idx = random.randint(1, len(graph)-1)
         node = graph[chosen_node_idx]
         node['op'] = random.choice(self.operations)
         # choose up to k inputs from previous nodes
@@ -61,7 +63,7 @@ class DiscreteSearchSpaceSegmentation(DiscreteSearchSpace):
 
         # now go through every node in the graph (except output node)
         # and make sure it is being used as input in some node after it
-        for i, node in enumerate(graph):
+        for i, node in enumerate(graph[:-1]):
             this_name = node['name']
             orphan = True
             # test whether not orphan
@@ -70,17 +72,22 @@ class DiscreteSearchSpaceSegmentation(DiscreteSearchSpace):
                     orphan = False
             if orphan:
                 # choose a forward node to connect it with
-                chosen_forward_idx = random.randint(i+1, len(graph))
+                chosen_forward_idx = random.randint(i+1, len(graph)-1)
                 graph[chosen_forward_idx]['inputs'].append(this_name)
 
         # compile the model
         model = SegmentationNasModel.from_config(graph, channels_per_scale)
+        # TODO: these should come from config or elsewhere 
+        # such that they are not hardcoded in here
+        out_shape = model.validate_forward(torch.randn(1, 3, 256, 256)).shape
+        assert out_shape == torch.Size([1, 19, 256, 256])
         extradata = {
                         'datasetname': self.datasetname,
                         'graph': graph,
                         'channels_per_scale': channels_per_scale,
                         'archid': uuid.uuid4(), #TODO: need to replace with a string of the graph 
                     }
+
         arch_meta = ArchWithMetaData(model, extradata)
         return [arch_meta]
 
