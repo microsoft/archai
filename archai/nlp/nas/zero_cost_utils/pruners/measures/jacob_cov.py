fix(zero_cost_utils): Attempts to fix zero-cost calculation.

archai/nlp/nas/zero_cost_utils/pruners/measures/jacob_cov.py
@@ -172,134 +172,7 @@ def forward_after_embedding_gpt2(self, word_emb, target, mems=None):
     return self.fc_to_1class(outputs.logits), None
 
 
-def forward_after_embedding_flex(self, word_emb, target, mems=None):
-    if mems is None:
-        mems = self.init_mems()
-
-    qlen, bsz = word_emb.size(0), word_emb.size(1)
-
-    mlen = mems[0].size(0) if mems is not None else 0
-    klen = mlen + qlen
-    if self.same_length:
-        all_ones = word_emb.new_ones(qlen, klen)
-        mask_len = klen - self.mem_len - 1
-        if mask_len > 0:
-            mask_shift_len = qlen - mask_len
-        else:
-            mask_shift_len = qlen
-        dec_attn_mask = (
-            torch.triu(all_ones, 1 + mlen) + torch.tril(all_ones, -mask_shift_len)
-        ).bool()
-    else:
-        dec_attn_mask = torch.triu(
-            word_emb.new_ones(qlen, klen), diagonal=1 + mlen
-        ).bool()
-
-    hids = []
-    # default
-    if self.attn_type == 0:
-        pos_seq = torch.arange(
-            klen - 1, -1, -1.0, device=word_emb.device, dtype=word_emb.dtype
-        )
-        if self.clamp_len > 0:
-            pos_seq.clamp_(max=self.clamp_len)
-        pos_emb = self.pos_emb(pos_seq)
-
-        core_out = self.drop(word_emb)
-        pos_emb = self.drop(pos_emb)
-
-        for i, layer in enumerate(self.layers):
-            hids.append(core_out.detach())
-            mems_i = None if mems is None else mems[i]
-
-            # core_out = layer(core_out, pos_emb, self.r_w_bias[i], self.r_r_bias[i], dec_attn_mask=dec_attn_mask, mems=mems_i)
-            core_out = layer(
-                core_out,
-                pos_emb,
-                getattr(self, f"r_w_bias_{i}"),
-                getattr(self, f"r_r_bias_{i}"),
-                dec_attn_mask=dec_attn_mask,
-                mems=mems_i,
-            )
-    # learnable
-    elif self.attn_type == 1:
-        core_out = self.drop(word_emb)
-        for i, layer in enumerate(self.layers):
-            hids.append(core_out.detach())
-            if self.clamp_len > 0:
-                r_emb = getattr(self, f"r_emb_{i}")[-self.clamp_len :]
-                r_bias = getattr(self, f"r_bias_{i}")[-self.clamp_len :]
-                # r_emb = self.r_emb[i][-self.clamp_len:]
-                # r_bias = self.r_bias[i][-self.clamp_len:]
-            else:
-                r_emb, r_bias = getattr(self, f"r_emb_{i}"), getattr(
-                    self, f"r_bias_{i}"
-                )
-                # r_emb, r_bias = self.r_emb[i], self.r_bias[i]
-
-            mems_i = None if mems is None else mems[i]
-            # core_out = layer(core_out, r_emb, self.r_w_bias[i], r_bias, dec_attn_mask=dec_attn_mask, mems=mems_i)
-            core_out = layer(
-                core_out,
-                r_emb,
-                getattr(self, f"r_w_bias_{i}"),
-                r_bias,
-                dec_attn_mask=dec_attn_mask,
-                mems=mems_i,
-            )
-    # absolute
-    elif self.attn_type == 2:
-        pos_seq = torch.arange(
-            klen - 1, -1, -1.0, device=word_emb.device, dtype=word_emb.dtype
-        )
-        if self.clamp_len > 0:
-            pos_seq.clamp_(max=self.clamp_len)
-        pos_emb = self.pos_emb(pos_seq)
-
-        core_out = self.drop(word_emb + pos_emb[-qlen:])
-
-        for i, layer in enumerate(self.layers):
-            hids.append(core_out.detach())
-            mems_i = None if mems is None else mems[i]
-            if mems_i is not None and len(mems_i) and i == 0:
-                mems_i += pos_emb[:mlen]
-            core_out = layer(core_out, dec_attn_mask=dec_attn_mask, mems=mems_i)
-
-    elif self.attn_type == 3:
-        core_out = self.drop(word_emb)
-
-        for i, layer in enumerate(self.layers):
-            hids.append(core_out.detach())
-            mems_i = None if mems is None else mems[i]
-            if mems_i is not None and len(mems_i) and mlen > 0:
-                cur_emb = self.r_emb[i][:-qlen]
-                cur_size = cur_emb.size(0)
-                if cur_size < mlen:
-                    cur_emb_pad = cur_emb[0:1].expand(mlen - cur_size, -1, -1)
-                    cur_emb = torch.cat([cur_emb_pad, cur_emb], 0)
-                else:
-                    cur_emb = cur_emb[-mlen:]
-                mems_i += cur_emb.view(mlen, 1, -1)
-            core_out += self.r_emb[i][-qlen:].view(qlen, 1, -1)
-
-            core_out = layer(core_out, dec_attn_mask=dec_attn_mask, mems=mems_i)
-
-    core_out = self.drop(core_out)
-    # core_out = self.fc_to_1class(core_out)
-
-    new_mems = self._update_mems(hids, mems, qlen, mlen)
-
-    tgt_len = target.size(0)
-    pred_hid = core_out[-tgt_len:]
-
-    out = self.crit(pred_hid.view(-1, pred_hid.size(-1)))
-    # out = out.view(tgt_len, -1)
-    out = self.fc_to_1class(out)
-
-    return out, new_mems
-
-
-def forward_crit(self, hidden, target=None, keep_order=False):
+def forward_crit(self, hidden, target=None, keep_order=False, output_loss=True, output_prediction_scores=False):
     """
     hidden :: [len*bsz x d_proj]
     """
@@ -310,7 +183,7 @@ def forward_crit(self, hidden, target=None, keep_order=False):
             self.out_layers_biases[0],
             self.get_out_proj(0),
         )
-        return logit
+        return None, logit
     else:
         # construct weights and biases
         weights, biases, projs = [], [], []
@@ -344,7 +217,7 @@ def forward_crit(self, hidden, target=None, keep_order=False):
             )  # self.tail[i](input)
             out[:, start_idx:stop_idx] = cluster_output
 
-        return out
+        return None, out
 
 
 def modify_net(net):
@@ -366,7 +239,7 @@ def modify_net(net):
     # net.forward = types.MethodType(forward_after_embedding_flex, net)
     else:
         raise NotImplementedError
-    # net.crit.forward = types.MethodType(forward_crit, net.crit)
+    net.model.lm_head.forward = types.MethodType(forward_crit, net.model.lm_head)
     net.fc_to_1class = torch.nn.Linear(net.n_token, 1, bias=False)
     return net
 

archai/nlp/nas/zero_cost_utils/pruners/measures/jacob_cov_relu.py
@@ -70,7 +70,7 @@ def forward_jacob_cov(
     return self.fc_to_1class(outputs.logits)
 
 
-def forward_crit(self, hidden, target=None, keep_order=False):
+def forward_crit(self, hidden, target=None, keep_order=False, output_loss=True, output_prediction_scores=False):
     """
     hidden :: [len*bsz x d_proj]
     """
@@ -81,7 +81,7 @@ def forward_crit(self, hidden, target=None, keep_order=False):
             self.out_layers_biases[0],
             self.get_out_proj(0),
         )
-        return logit
+        return None, logit
     else:
         # construct weights and biases
         weights, biases, projs = [], [], []
@@ -115,7 +115,7 @@ def forward_crit(self, hidden, target=None, keep_order=False):
             )  # self.tail[i](input)
             out[:, start_idx:stop_idx] = cluster_output
 
-        return out
+        return None, out
 
 
 def modify_net(net, jacob_size):
@@ -130,7 +130,7 @@ def modify_net(net, jacob_size):
     #         del l
 
     net.forward = types.MethodType(forward_jacob_cov, net)
-    # net.crit.forward = types.MethodType(forward_crit, net.crit)
+    net.model.lm_head.forward = types.MethodType(forward_crit, net.model.lm_head)
     net.fc_to_1class = torch.nn.Linear(net.n_token, 1, bias=False)
 
     net.K = np.zeros((jacob_size, jacob_size))

archai/nlp/nas/zero_cost_utils/synflow_utils.py
@@ -58,7 +58,7 @@ def compute_synflow_per_weight(net, inputs, targets):
     outputs = net(inputs, targets, mems=None)
 
     # TODO: should this be loss or logits?
-    torch.sum(outputs.loss).backward()
+    torch.sum(outputs.logits).backward()
 
     # select the gradients that we want to use for search/prune
     def synflow(layer):