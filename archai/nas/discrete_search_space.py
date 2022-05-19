c021837a0516e9da2a7c5cf06c0edfdf76f0c1bf
piero2c <pierokauffmann@gmail.com>
2022-05-18 17:07:21 -0700

    feat(discrete search): `EncodableDiscreteSearchSpace` class

--- archai/nas/discrete_search_space.py
+++ archai/nas/discrete_search_space.py
@@ -2,10 +2,11 @@
 # Licensed under the MIT license.
 
 from abc import ABCMeta, abstractmethod
-from typing import List
+from typing import List, Union
 from overrides.enforce import EnforceOverrides
 
-import torch.nn as nn
+import torch
+import torch_geometric
 from archai.nas.arch_meta import ArchWithMetaData
 
 
@@ -20,3 +21,16 @@ class DiscreteSearchSpace(EnforceOverrides, metaclass=ABCMeta):
         '''Return the neighbors (some definition of neighborhood) of an architecture'''
         pass
 
+class EncodableDiscreteSearchSpace(DiscreteSearchSpace):
+    @abstractmethod
+    def get_arch_repr(self, arch:ArchWithMetaData)->Union[torch.Tensor, torch_geometric.data.Data]:
+        """Gets a representation (either a torch.Tensor or a torch_geometric.data.Data graph)
+        of an architecture from the search space.
+
+        Args:
+            arch (ArchWithMetaData): An architecture from the search space.
+
+        Returns:
+            Union[torch.Tensor, Data]: A representation of the architecture.
+        """        
+        pass
