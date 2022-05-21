5feaa613ca580817ff32ed4e391839ce70d70003
piero2c <pierokauffmann@gmail.com>
2022-05-20 17:58:55 -0700

    Removes torch_geometric to avoid env issues

--- archai/nas/discrete_search_space.py
+++ archai/nas/discrete_search_space.py
@@ -6,7 +6,6 @@ from typing import List, Union
 from overrides.enforce import EnforceOverrides
 
 import torch
-import torch_geometric
 from archai.nas.arch_meta import ArchWithMetaData
 
 
@@ -19,18 +18,4 @@ class DiscreteSearchSpace(EnforceOverrides, metaclass=ABCMeta):
     @abstractmethod
     def get_neighbors(self, arch:ArchWithMetaData)->List[ArchWithMetaData]:
         '''Return the neighbors (some definition of neighborhood) of an architecture'''
-        pass
-
-class EncodableDiscreteSearchSpace(DiscreteSearchSpace):
-    @abstractmethod
-    def get_arch_repr(self, arch:ArchWithMetaData)->Union[torch.Tensor, torch_geometric.data.Data]:
-        """Gets a representation (either a torch.Tensor or a torch_geometric.data.Data graph)
-        of an architecture from the search space.
-
-        Args:
-            arch (ArchWithMetaData): An architecture from the search space.
-
-        Returns:
-            Union[torch.Tensor, Data]: A representation of the architecture.
-        """        
         pass
