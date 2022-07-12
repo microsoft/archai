255f8d047cb85c9376e0403696e1301f7b6401f8
Debadeepta Dey <dedey@microsoft.com>
2022-07-12 14:30:34 -0700

    Removed file no longer needed.

--- scripts/misc/hf_featurize.py
+++ scripts/misc/hf_featurize.py
@@ -2,6 +2,8 @@
 Script to play with featurizing a large dataset like The Pile, with the 
 motivation to use downstream data summarization techniques on it.
 """
+from base64 import encode
+from math import trunc
 import torch
 
 from datasets import load_dataset
@@ -15,26 +17,30 @@ def main():
     checkpoint = "facebook/opt-1.3b" # "facebook/opt-350m" # "gpt2"
     model = AutoModel.from_pretrained(checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-    # can we speed this up like we can do the dataset map way?
-    # yes: see pipeline batching https://huggingface.co/docs/transformers/main_classes/pipelines
     embedder = pipeline("feature-extraction", model=model, tokenizer=tokenizer)
-    sample_text = 'Good morning! How are you?'
-    features = torch.tensor(embedder(sample_text))
-
-    # load wikitext-103 validation 
+    
+    # load wikitext-103 validation and extract features on it 
+    # using pipeline batching pipeline batching \
+    # https://huggingface.co/docs/transformers/main_classes/pipelines
     dataset = load_dataset("wikitext", 'wikitext-103-v1', split='validation')
-    for idx, out in enumerate(embedder(KeyDataset(dataset, "text"), 
-                        batch_size=100, 
-                        truncation=None, 
-                        num_workers=20)):
-        print(f'idx: {idx}, shape: {torch.tensor(out).shape}')
+    # for idx, out in enumerate(embedder(KeyDataset(dataset, "text"), 
+    #                     batch_size=100, 
+    #                     truncation=None, 
+    #                     num_workers=20)):
+    #     print(f'idx: {idx}, shape: {torch.tensor(out).shape}')
     
     # this outputs the same as features
-    # I know how to parallelize this via dataset.map
-    encoded_input = tokenizer(sample_text, return_tensors='pt')
-    output = model(**encoded_input)
-    features = output['last_hidden_state']
+    # and adds it to the dataset as an additional column
+    # but it makes the dataset massive
+    def featurize(examples):
+        encoded_input = tokenizer(examples["text"], 
+                        return_tensors='pt', 
+                        padding=True,
+                        truncation=True)
+        output = model(**encoded_input)
+        return {"embedding" : output['last_hidden_state']}
+
+    dataset.map(featurize, batched=True, batch_size=1000)
 
     print('done')
 
