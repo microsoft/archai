964df26a220e6b79b87f3e842cc590dac1f131ad
Debadeepta Dey <dedey@microsoft.com>
2022-07-12 13:47:50 -0700

    Pipeline parallelization is working for feature extraction.

--- scripts/misc/hf_featurize.py
+++ scripts/misc/hf_featurize.py
@@ -2,20 +2,36 @@
 Script to play with featurizing a large dataset like The Pile, with the 
 motivation to use downstream data summarization techniques on it.
 """
+import torch
 
 from datasets import load_dataset
+from transformers.pipelines.pt_utils import KeyDataset
 
-from transformers import pipeline, AutoTokenizer, AutoModel
+from transformers import pipeline, AutoTokenizer, AutoModel, AutoModelForCausalLM
+
 
 def main():
-    checkpoint = "gpt2"
-    model = AutoModel.from_pretrained(checkpoint, max_length=512)
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length=512, padding=True, truncation=True)
-    embedder = pipeline("feature-extraction", model=model, tokenizer=tokenizer, model_max_length=512)
+
+    checkpoint = "facebook/opt-1.3b" # "facebook/opt-350m" # "gpt2"
+    model = AutoModel.from_pretrained(checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+    # can we speed this up like we can do the dataset map way?
+    # yes: see pipeline batching https://huggingface.co/docs/transformers/main_classes/pipelines
+    embedder = pipeline("feature-extraction", model=model, tokenizer=tokenizer)
     sample_text = 'Good morning! How are you?'
-    features = embedder(sample_text)
+    features = torch.tensor(embedder(sample_text))
+
+    # load wikitext-103 validation 
+    dataset = load_dataset("wikitext", 'wikitext-103-v1', split='validation')
+    for idx, out in enumerate(embedder(KeyDataset(dataset, "text"), 
+                        batch_size=100, 
+                        truncation=None, 
+                        num_workers=20)):
+        print(f'idx: {idx}, shape: {torch.tensor(out).shape}')
     
-    # this outputs the same as features but in a nicer torch tensor format
+    # this outputs the same as features
+    # I know how to parallelize this via dataset.map
     encoded_input = tokenizer(sample_text, return_tensors='pt')
     output = model(**encoded_input)
     features = output['last_hidden_state']
