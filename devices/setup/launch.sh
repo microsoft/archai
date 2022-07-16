0a9a557bdfc158319821f596222a49f5a5d0946c
Chris Lovett <clovett@microsoft.com>
2022-07-15 17:15:32 -0700

    fix: qualcomm screen session setup

--- devices/setup/launch.sh
+++ devices/setup/launch.sh
@@ -5,16 +5,15 @@ next=1
 
 DeviceRunner () {
     folder="~/snpe/experiment$next"
-    echo "Setting up screen session for device $1 in $folder ..."
     mkdir -p $folder
-    cd $folder
-    screen -dmS "$1" "$command" "--device $1 --no_quantization"
+    echo cd $folder
+    echo screen -dmS $1 $command --device $1 --no_quantization
     next=$((next+1))
 }
+
+echo "# run the following lines to setup the screen sessions..."
 
 for usb in $(adb devices | awk '$2 ~ /device/ {print $1}')
 do
     DeviceRunner $usb
 done
-
-screen -ls
