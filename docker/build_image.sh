fcc6e49bc18acda07bb5b38adadadd88eb9620a1
Gustavo Rosa <gth.rosa@uol.com.br>
2022-08-19 18:58:26 -0300

    chore(root): Bumps version for PyPI release.

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -3,4 +3,4 @@
 # Licensed under the MIT license.
 
 # Builds a docker image with standard arguments
-docker build . --file Dockerfile --tag nvidia22.07-archai0.6.6:latest
+docker build . --file Dockerfile --tag nvidia22.07-archai0.6.7:latest
--- docker/run_container.sh
+++ docker/run_container.sh
@@ -5,10 +5,10 @@
 # Runs an interactive bash within the container
 docker run --rm \
     --gpus all \
-    --name nvidia22.07-archai0.6.6 \
+    --name nvidia22.07-archai0.6.7 \
     --shm-size=10g \
     --ipc=host \
     --ulimit memlock=-1 \
     --ulimit stack=67108864 \
     -e NCCL_P2P_LEVEL=NVL \
-    -it nvidia22.07-archai0.6.6:latest
+    -it nvidia22.07-archai0.6.7:latest
