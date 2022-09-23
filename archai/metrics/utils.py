067679515d4a99a0d41ae4ed5794ea8601eaa1ab
piero2c <pierokauffmann@gmail.com>
2022-09-23 13:29:30 -0700

    feat(discrete_search): `get_pareto_frontier` util

--- archai/metrics/utils.py
+++ archai/metrics/utils.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Dict
 
 import numpy as np
 import warnings
@@ -66,3 +66,75 @@ def evaluate_models(models: List[ArchWithMetaData],
         for obj_idx in range(len(objectives))
     ], dtype=np.float64).T
 
+
+def get_pareto_frontier(models: List[ArchWithMetaData], 
+                        evaluation_results: np.ndarray,
+                        objectives: List[Union[BaseMetric, BaseAsyncMetric]]) -> Dict:    
+    # Inverts maximization objectives 
+    for obj_idx, obj in enumerate(objectives):
+        if obj.higher_is_better:
+            evaluation_results[:, obj_idx] = -evaluation_results[:, obj_idx]
+
+    pareto_points = np.array(
+        _find_pareto_frontier_points(evaluation_results, is_decreasing=True)
+    )
+
+    return {
+        'models': [models[idx] for idx in pareto_points],
+        'evaluation_results': evaluation_results[pareto_points, :],
+        'indices': pareto_points
+    }
+
+
+def _find_pareto_frontier_points(all_points: np.ndarray, is_decreasing: bool = True) -> List[int]:
+    """Takes in a list of n-dimensional points, one per row, returns the list of row indices
+        which are Pareto-frontier points.
+        
+    Assumes that lower values on every dimension are better.
+
+    Args:
+        all_points: N-dimensional points.
+        is_decreasing: Whether Pareto-frontier decreases or not.
+
+    Returns:
+        List of Pareto-frontier indexes.
+    """
+
+    # For each point see if there exists  any other point which dominates it on all dimensions
+    # If that is true, then it is not a pareto point and vice-versa
+
+    # Inputs should alwyas be a two-dimensional array
+    assert len(all_points.shape) == 2
+
+    pareto_inds = []
+
+    dim = all_points.shape[1]
+
+    # Gets the indices of unique points
+    _, unique_indices = np.unique(all_points, axis=0, return_index=True)
+
+    for i in unique_indices:
+        this_point = all_points[i,:]
+        is_pareto = True
+
+        for j in unique_indices:
+            if j == i:
+                continue
+
+            other_point = all_points[j,:]
+
+            if is_decreasing:
+                diff = this_point - other_point
+            else:
+                diff = other_point - this_point
+
+            if sum(diff >= 0) == dim:
+                # Other point is smaller/larger on all dimensions
+                # so we have found at least one dominating point
+                is_pareto = False
+                break
+                
+        if is_pareto:
+            pareto_inds.append(i)
+
+    return pareto_inds
