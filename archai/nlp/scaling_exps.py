24be4ec9e71df5390033ff09eeb8f23c18b1dabd
mojanjp <mojavahe@ucsd.edu>
2022-09-29 16:05:28 -0700

    generating scaling exps configs for GPT2

--- archai/nlp/scaling_exps.py
+++ archai/nlp/scaling_exps.py
@@ -13,6 +13,7 @@ import pickle
 
 from archai.nlp.nas.nas_utils.constraints.constraint_pipeline import TorchConstraintPipeline
 from archai.nlp.models.model_loader import load_config, load_model_from_config, load_model_formula
+from archai.nlp.models.hf_gpt2.config_hf_gpt2 import HfGPT2FlexConfig
 
 def solve_quadratic(a, b, c):
     discriminant = b**2 - 4 * a * c
@@ -34,43 +35,69 @@ def solve_quadratic(a, b, c):
 
 
 if __name__ == '__main__':
-    model_type = 'mem_transformer'
-    model_config = load_config(model_type)
-    model_config.vocab_size = 267735
-    
-    pipeline = TorchConstraintPipeline()
-    orig_config = copy.deepcopy(model_config).to_dict()
-    orig_config.update({'n_layer': 3, 'd_model':960, 'n_head':8, 'd_inner':1920})
-    model = load_model_from_config(model_type, orig_config)
-    orig_dec_params = load_model_formula(model_type)(orig_config)['non_embedding']
-    print('original number of parameters:', orig_dec_params)
+    model_type = 'hf_gpt2_flex' #'mem_transformer'
+    model_config = {
+        'n_token': 267735 if model_type=='mem_transformer' else 50257,
+        'n_layer': 6,
+        'n_head': 8,
+        'd_model': 1024,
+        'd_embed': 1024,
+        'd_inner': 2048,
+        'div_val': 4,
+        'tgt_len': 192
+    }
+    model = load_model_from_config(model_type, model_config)
+    assert model.model.transformer.h[0].mlp.c_proj.weight.size(1) == model_config['d_model']
+    orig_dec_params = model.get_params()['non_embedding'] #load_model_formula(model_type)(orig_config)['non_embedding']
+    params_formula = load_model_formula(model_type)(model_config)['non_embedding']
+    print('original parameter count:', orig_dec_params, params_formula)
 
     ## formula is:
-    ###### n_layer' * (5*d_model**2 + 7*d_model + 2*d_model*d_inner + d_inner)
+    ###### trans_xl: n_layer * (5*d_model**2 + 7*d_model + 2*d_model*d_inner + d_inner)
+    ###### gpt2: n_layer * (4*d_model**2 + 11*d_model + 2*d_model*d_inner + d_inner)
 
+    # load model and measure parameters
+    '''
+    model_config = HfGPT2FlexConfig(n_layer=6, d_model=1024, hidden_size=1024, n_head=8, d_inner=2048)
+    model_config.vocab_size = 267735 if model_type=='mem_transformer' else 50257
+    pipeline = TorchConstraintPipeline()
+    orig_config = copy.deepcopy(model_config).to_dict()
+    model = load_model_from_config(model_type, orig_config)
+    orig_dec_params = model.get_params()['non_embedding'] #load_model_formula(model_type)(orig_config)['non_embedding']
+    params_formula = load_model_formula(model_type)(orig_config)['non_embedding']
+    '''
+
+    orig_config = copy.deepcopy(model_config)
     new_configs = []
-    for n_layer in [orig_config['n_layer']] + [15, 30, 40]:#, 100, 255, 540]:
+    for n_layer in [orig_config['n_layer']] + [51, 72, 96]:#[7, 13, 17, 25, 29, 34, 41]:
         curr_config = copy.deepcopy(orig_config)
         curr_config['n_layer'] = n_layer
 
-        new_factor = orig_dec_params/(9*n_layer)
-        new_d_model = solve_quadratic(a=1, b=1, c=-new_factor)
+        # mem_Transformer
+        # new_factor = orig_dec_params/(9*n_layer)
+        # new_d_model = solve_quadratic(a=1, b=1, c=-new_factor)
+        
+        # GPT2
+        new_factor = orig_dec_params/(n_layer)
+        new_d_model = solve_quadratic(a=8, b=13, c=-new_factor)
 
         if new_d_model is not None:
             curr_config['d_model'] = math.floor(new_d_model)
             curr_config['d_model'] += curr_config['n_head'] - (curr_config['d_model'] % curr_config['n_head'])
             assert curr_config['d_model'] % curr_config['n_head'] == 0
+
+            curr_config['d_embed'] = curr_config['d_model']
             curr_config['d_inner'] = 2 * curr_config['d_model']
 
             ratio = curr_config['d_model']/curr_config['n_layer']
             if ratio < 0.1:
                 break
             
-            # model = load_model_from_config(model_type, curr_config)
-            # proxy, total_params, latency, memory = pipeline(model, curr_config)
-            new_dec_params = load_model_formula(model_type)(curr_config)['non_embedding']
+            model = load_model_from_config(model_type, curr_config)
+            new_dec_params = model.get_params()['non_embedding']
+            # new_dec_params = load_model_formula(model_type)(curr_config)['non_embedding']
             
-            print('new d_model={}, parameter count:{}'.format(curr_config['d_model'], new_dec_params))
+            print('new n_layer={}, new d_model={}, parameter count:{}'.format(curr_config['n_layer'], curr_config['d_model'], new_dec_params))
             print('ratio:', ratio)
             new_configs.append(curr_config)
 
