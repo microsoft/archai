6ec365d1b455a2c9d49f629728808f8e3642d370
piero2c <pierokauffmann@gmail.com>
2022-09-30 14:31:12 -0700

    feat(discrete_search): faster impl. of non-dominated sorting

--- archai/metrics/utils.py
+++ archai/metrics/utils.py
@@ -1,8 +1,6 @@
-from typing import Union, List, Dict
+from typing import Union, List, Dict, Optional
 
 import numpy as np
-import warnings
-from dataclasses import dataclass
 from tqdm import tqdm
 
 from archai.nas.arch_meta import ArchWithMetaData
@@ -137,7 +135,7 @@ def get_pareto_frontier(models: List[ArchWithMetaData],
     results_array = np.vstack(list(inverted_results.values())).T
 
     pareto_points = np.array(
-        _find_pareto_frontier_points(results_array, is_decreasing=True)
+        _find_pareto_frontier_points(results_array)
     )
 
     return {
@@ -150,7 +148,36 @@ def get_pareto_frontier(models: List[ArchWithMetaData],
     }
 
 
-def _find_pareto_frontier_points(all_points: np.ndarray, is_decreasing: bool = True) -> List[int]:
+def get_non_dominated_sorting(models: List[ArchWithMetaData],
+                              evaluation_results: Dict[str, np.ndarray],
+                              objectives: Dict[str, Union[BaseMetric, BaseAsyncMetric]]) -> List[Dict]:
+    assert len(objectives) == len(evaluation_results)
+    assert all(len(r) == len(models) for r in evaluation_results.values())
+
+    # Inverts maximization objectives 
+    inverted_results = {
+        obj_name: (-obj_results if objectives[obj_name].higher_is_better else obj_results)
+        for obj_name, obj_results in evaluation_results.items()
+    }
+
+    # Converts results to an array of shape (len(models), len(objectives))
+    results_array = np.vstack(list(inverted_results.values())).T
+
+    frontiers = np.array(
+        _find_non_dominated_sorting(results_array)
+    )
+
+    return [{
+        'models': [models[idx] for idx in frontier],
+        'evaluation_results': {
+            obj_name: obj_results[frontier]
+            for obj_name, obj_results in evaluation_results.items()
+        },
+        'indices': frontier
+    } for frontier in frontiers]
+
+
+def _find_pareto_frontier_points(all_points: np.ndarray) -> List[int]:
     """Takes in a list of n-dimensional points, one per row, returns the list of row indices
         which are Pareto-frontier points.
         
@@ -158,7 +185,6 @@ def _find_pareto_frontier_points(all_points: np.ndarray, is_decreasing: bool = T
 
     Args:
         all_points: N-dimensional points.
-        is_decreasing: Whether Pareto-frontier decreases or not.
 
     Returns:
         List of Pareto-frontier indexes.
@@ -186,11 +212,7 @@ def _find_pareto_frontier_points(all_points: np.ndarray, is_decreasing: bool = T
                 continue
 
             other_point = all_points[j,:]
-
-            if is_decreasing:
-                diff = this_point - other_point
-            else:
-                diff = other_point - this_point
+            diff = this_point - other_point
 
             if sum(diff >= 0) == dim:
                 # Other point is smaller/larger on all dimensions
@@ -204,27 +226,87 @@ def _find_pareto_frontier_points(all_points: np.ndarray, is_decreasing: bool = T
     return pareto_inds
 
 
-def _find_non_dominated_sorting(all_points: np.ndarray, is_decreasing: bool = True) -> List[List[int]]:
+def _find_non_dominated_sorting(all_points: np.ndarray) -> List[List[int]]:
     """Finds non-dominated sorting frontiers from a matrix (#points, #objectives).
 
     Args:
         all_points (np.ndarray): N-dimensional points.
-        is_decreasing (bool, optional): Whether pareto-frontier decreases or not. Defaults to True.
 
     Returns:
         List[List[int]]: List of frontier indices
-    """    
-    all_indices = np.arange(all_points.shape[0])
-    available_indices = np.ones_like(all_indices).astype(bool)
-    frontiers = []
+    
+    References:
+        Implementation adapted from:
+            https://github.com/anyoptimization/pymoo/blob/main/pymoo/util/nds/efficient_non_dominated_sort.py
+    
+        Algorithm:
+            X. Zhang, Y. Tian, R. Cheng, and Y. Jin,
+            An efficient approach to nondominated sorting for evolutionary multiobjective optimization,
+            IEEE Transactions on Evolutionary Computation, 2015, 19(2): 201-213.
+    """
 
-    while available_indices.sum() > 0:
-        frontier_indices = _find_pareto_frontier_points(
-            all_points[available_indices], is_decreasing=is_decreasing
-        )
-        frontier_indices = all_indices[available_indices][frontier_indices]
+    lex_sorting = np.lexsort(all_points.T[::-1])
+    all_points = all_points.copy()[lex_sorting]
 
-        frontiers.append(frontier_indices.tolist())
-        available_indices[frontier_indices] = 0
+    fronts = []
 
-    return frontiers
+    for idx in range(all_points.shape[0]):
+        front_rank = _find_front_rank(all_points, idx, fronts)        
+        
+        if front_rank >= len(fronts):
+            fronts.append([])
+
+        fronts[front_rank].append(idx)
+
+    ret = []
+    for front in fronts:
+        ret.append(lex_sorting[front])
+
+    return ret
+
+
+def _find_front_rank(all_points: np.ndarray, idx: int, fronts: List[List[int]]) -> int:
+    """Finds the front rank for all_points[idx] given `fronts`.
+
+    Args:
+        all_points (np.ndarray): N-dimensional points.
+        idx (int): Point index.
+        fronts (List[List[int]]): Current NDS fronts
+
+    Returns:
+        int: Front rank for `all_points[idx]`
+    
+    Reference:
+        Adapted from https://github.com/anyoptimization/pymoo/blob/main/pymoo/util/nds/efficient_non_dominated_sort.py
+    """
+    
+    def dominates(x, y):
+        for i in range(len(x)):
+            if y[i] < x[i]:
+                return False
+
+        return True
+
+    num_found_fronts = len(fronts)
+    rank = 0
+    current = all_points[idx]
+
+    while True:
+        if num_found_fronts == 0:
+            return 0
+
+        fk_indices = fronts[rank]
+        solutions = all_points[fk_indices[::-1]]
+        non_dominated = True
+
+        for s in solutions:
+            if dominates(s, current):
+                non_dominated = False
+                break
+
+        if non_dominated:
+            return rank
+        else:
+            rank += 1
+            if rank >= num_found_fronts:
+                return num_found_fronts
