d655dc4b4ab1773349992fa1994dee192a122d25
piero2c <pierokauffmann@gmail.com>
2022-10-03 14:59:55 -0700

    feat(discrete_search): updates DnnEnsemble surrogate

--- archai/nas/surrogate/predictive_dnn_ensemble.py
+++ archai/nas/surrogate/predictive_dnn_ensemble.py
@@ -1,22 +1,23 @@
 from typing import Tuple
 from overrides.overrides import overrides
 
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
 import numpy as np
 
 from archai.common.common import logger
-from archai.common.config import Config
-from archai.common.trainer import Trainer
-from archai.nas.arch_meta import ArchWithMetaData
 from archai.nas.surrogate.predictive_function import MeanVar, PredictiveFunction
 
-class PredictiveDNNEnsemble(PredictiveFunction):
 
     def __init__(self, num_features: int, num_layers: int = 5, width: int = 64,
                  num_ensemble_members: int = 5, sigmoid: bool = False):
         self.num_ensemble_members = num_ensemble_members
+        self.num_layers = num_layers
+        self.width = width
+        self.lr = lr
+        self.num_tr_steps = num_tr_steps
 
         # TODO: should have an architecture featurizer
         # object here and the featurizer should tell 
@@ -34,39 +35,44 @@ class PredictiveDNNEnsemble(PredictiveFunction):
                                           for _ in range(self.num_ensemble_members)]
 
         self.is_fit = False
+        self.device = 'cuda'
 
     def to_cuda(self):
         for m in self.ensemble:
             m.cuda()
+        self.device = 'cuda'
 
     def to_cpu(self):
         for m in self.ensemble:
             m.cpu()
+        self.device = 'cpu'
+    
+    @overrides
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        assert len(X.shape) == 2
+        _, num_features = X.shape
 
-        
-    def fit(self, x:torch.Tensor, y:torch.Tensor, conf_train:Config)->None:
-        ''' Trains each ensemble member with the data
-        
-        Args:
-            x: torch.Tensor, architecture features [Nxfeatures]
-            y: torch.Tensor [Nx1] 
-            the first entry are architecture features, one per row
-            the second entry is the accuracy values per feature
-        '''
+        # Init ensemble models
+        self.ensemble = [
+            FFEnsembleMember(num_features, self.num_layers, self.width).to(self.device)
+            for _ in range(self.num_ensemble_members)
+        ]
 
-        lr = conf_train['lr']
-        num_steps = conf_train['num_steps']
+        Xt = torch.tensor(X, dtype=torch.float32).to(self.device)
+        yt = torch.tensor(y, dtype=torch.float32).to(self.device)
 
         # TODO: should we be splitting data into 
         # train and val?
-        for idx, member in enumerate(self.ensemble):
+        for idx, member in tqdm(enumerate(self.ensemble), desc='Training DNN Ensemble...'):
             logger.pushd(f'train ensemble_{idx}')
             criterion = torch.nn.MSELoss(reduction='sum')
-            optimizer = torch.optim.SGD(member.parameters(), lr=lr)
+            optimizer = torch.optim.Adam(member.parameters(), lr=self.lr)
             member.train()
-            for t in range(num_steps):
-                y_pred = member(x)
-                loss = criterion(y_pred, y)
+
+            for t in range(self.num_tr_steps):
+                y_pred = member(Xt)
+                loss = criterion(y_pred.squeeze(), yt.squeeze())
+
                 if t % 10 == 9:
                     logger.info(f'step {t}: training loss {loss.item()}')
                     print(f'step {t}: training loss {loss.item()}')
@@ -74,31 +80,26 @@ class PredictiveDNNEnsemble(PredictiveFunction):
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+
             logger.popd()
 
-        # set the flag that it has been fit
         self.is_fit = True
 
-
     @overrides
-    def predict(self, x:torch.Tensor)->MeanVar:
-        ''' x: torch.Tensor [1xfeat_dim] 
-            architecture feature for a single datapoint
-        '''
-        assert x.shape[0] == 1
-
+    def predict(self, X: np.ndarray) -> MeanVar:
+        assert len(X.shape) == 2
         assert self.is_fit, 'PredictiveDNNEnsemble: predict called before fit!' 
+
+        Xt = torch.tensor(X, dtype=torch.float32).to(self.device)
         
         preds = []
-        for member in self.ensemble:
-            member.eval()
-            preds.append(member(x).item())
+        with torch.no_grad():
+            for member in self.ensemble:
+                member.eval()
+                preds.append(member(Xt).to('cpu').numpy())
+        
         preds = np.array(preds)
-        mean = np.mean(preds)
-        std = np.std(preds)
-        var = std * std
-
-        return MeanVar(mean=mean, var=var)
+        return MeanVar(mean=np.mean(preds, axis=0), var=np.var(preds, axis=0))
 
 
 class FFEnsembleMember(nn.Module):
