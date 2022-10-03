796b473b3f20a63022b29c179f7419157940234b
piero2c <pierokauffmann@gmail.com>
2022-10-03 14:59:55 -0700

    feat(discrete_search): updates DnnEnsemble surrogate

--- archai/nas/surrogate/predictive_function.py
+++ archai/nas/surrogate/predictive_function.py
@@ -6,17 +6,27 @@ from typing import List
 from collections import namedtuple
 from overrides.enforce import EnforceOverrides
 
-import torch.nn as nn
-from archai.nas.arch_meta import ArchWithMetaData
+import numpy as np
 
 MeanVar = namedtuple("MeanVar", "mean var")
 
 
 class PredictiveFunction(EnforceOverrides, metaclass=ABCMeta):
+    @abstractmethod
+    def fit(self, encoded_archs: np.ndarray, y: np.ndarray) -> None:
+        pass
 
     @abstractmethod
-    def predict(self, arch:ArchWithMetaData)->MeanVar:
-        ''' Predictive function to be used with BO methods
-        like Bananas '''
+    def predict(self, encoded_archs: np.ndarray) -> List[MeanVar]:
+        """Predicts the performance of an array of architec encoded by 
+        by a subclass implementation of `BayesOptSearchSpaceBase.encode`
+
+        Args:
+            encoded_archs (np.ndarray): Array of encoded architectyres
+
+        Returns:
+            List[MeanVar]: List of tuples with mean and variance for each prediction
+        """        
+        ''' Array of architectures encoded by a subclass of `BayesOptSearchSpaceBase`'''
         pass
 
