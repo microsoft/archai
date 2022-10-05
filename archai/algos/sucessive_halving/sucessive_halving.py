385eb09b1fd59a89efd93e73dc28e470cc78ed35
piero2c <pierokauffmann@gmail.com>
2022-10-05 08:38:25 -0700

    fix(discrete_search): fix logger for `SucessiveHalvingAlgo`

--- archai/algos/sucessive_halving/sucessive_halving.py
+++ archai/algos/sucessive_halving/sucessive_halving.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import numpy as np
 
-from archai.common.common import logger
+from archai.common.utils import create_logger
 from archai.nas.arch_meta import ArchWithMetaData
 from archai.search_spaces.discrete.base import DiscreteSearchSpaceBase
 from archai.metrics.base import BaseMetric, BaseAsyncMetric
@@ -25,7 +25,7 @@ class SucessiveHalvingAlgo(Searcher):
                  init_budget: float = 1.0,
                  budget_multiplier: float = 2.0,
                  seed: int = 1):
-        
+
         assert isinstance(search_space, DiscreteSearchSpaceBase)
 
         # Search parameters
@@ -38,12 +38,15 @@ class SucessiveHalvingAlgo(Searcher):
         self.init_budget = init_budget
         self.budget_multiplier = budget_multiplier
 
+        self.output_dir.mkdir(exist_ok=True)
+
         # Utils
         self.iter_num = 0
         self.num_sampled_models = 0
         self.seed = seed
         self.search_state = SearchResults(search_space, objectives)
         self.rng = random.Random(seed)
+        self.logger = create_logger(str(self.output_dir / 'log.log'), enable_stdout=True)
 
         self.output_dir.mkdir(exist_ok=True, parents=True)
 
@@ -63,17 +66,17 @@ class SucessiveHalvingAlgo(Searcher):
 
         for i in range(self.num_iters):
             if len(selected_models) <= 1:
-                logger.info(f'Search ended. Architecture selected: {selected_models[0].metadata["archid"]}')
+                self.logger.info(f'Search ended. Architecture selected: {selected_models[0].metadata["archid"]}')
                 self.search_space.save_arch(selected_models[0], self.output_dir / 'final_model')
                 
                 break
 
-            logger.info(
+            self.logger.info(
                 f'Starting iteration {i} with {len(selected_models)} architectures '
                 f'and budget of {current_budget}.'
             )
 
-            logger.info(f'Evaluating {len(selected_models)} models with budget {current_budget}..')
+            self.logger.info(f'Evaluating {len(selected_models)} models with budget {current_budget}..')
             results = evaluate_models(selected_models, self.objectives, self.dataset_provider, budgets={
                 obj_name: current_budget
                 for obj_name in self.objectives
@@ -100,12 +103,12 @@ class SucessiveHalvingAlgo(Searcher):
             self.search_state.save_all_2d_pareto_evolution_plots(self.output_dir)
 
             # Keeps only the best `1/self.budget_multiplier` NDS frontiers
-            logger.info(f'Choosing models for the next iteration..')
+            self.logger.info(f'Choosing models for the next iteration..')
             nds_frontiers = get_non_dominated_sorting(selected_models, results, self.objectives)
             nds_frontiers = nds_frontiers[:int(len(nds_frontiers) * 1/self.budget_multiplier)]
 
             selected_models = [model for frontier in nds_frontiers for model in frontier['models']]
-            logger.info(f'Kept {len(selected_models)} models for next iteration.')
+            self.logger.info(f'Kept {len(selected_models)} models for next iteration.')
 
             # Update parameters for next iteration
             self.iter_num += 1
