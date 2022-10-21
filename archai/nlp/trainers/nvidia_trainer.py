20a0bc4fe2c0bbe224753bce06c00dfc46e75358
Gustavo Rosa <gth.rosa@uol.com.br>
2022-10-21 18:27:38 -0300

    chore(nlp): Adds initial boilerplate for trainers.

---
+++ archai/nlp/trainers/nvidia_trainer.py
@@ -0,0 +1,52 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+"""
+
+class NvidiaTrainer:
+    """
+    """
+
+    def __init__(
+        self,
+        model=None,
+        args=None,
+        data_collator=None,
+        tokenizer=None,
+        train_dataset=None,
+        eval_dataset=None,
+        compute_metrics=None,
+        callbacks=None,
+        optimizers=None,
+    ) -> None:
+        """"""
+        pass
+
+    def get_dataloader(self):
+        """"""
+        pass
+
+    def create_optimizer_and_scheduler(self):
+        """"""
+        pass
+
+    def training_step(self):
+        """"""
+        pass
+
+    def train(self):
+        """"""
+        pass
+
+    def prediction_step(self):
+        """"""
+        pass
+
+    def predict(self):
+        """"""
+        pass
+
+    def evaluate(self):
+        """"""
+        pass
