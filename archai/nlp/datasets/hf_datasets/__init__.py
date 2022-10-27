5ae08f4e2a2a1449cc8410a0aac1354718f30db2
Gustavo Rosa <gth.rosa@uol.com.br>
2022-10-27 16:49:32 -0300

    chore(nlp): Adds Bert and CodeGen tokenizers.

--- archai/nlp/datasets/hf_datasets/__init__.py
+++ archai/nlp/datasets/hf_datasets/__init__.py
@@ -3,3 +3,7 @@
 
 """Huggingface's-related datasets loaders, processors and tokenizers.
 """
+
+from archai.nlp.datasets.hf_datasets.tokenizer_utils import BertTokenizer, CodeGenTokenizer, GPT2Tokenizer, ArchaiPreTrainedTokenizerFast
+from archai.nlp.datasets.hf_datasets.loaders import load_dataset, encode_dataset
+from archai.nlp.datasets.hf_datasets.processors import merge_datasets
---
+++ archai/nlp/datasets/hf_datasets/tokenizer_utils/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Huggingface's-related tokenizers, pre-trained tokenizers and token's configuration.
+"""
+
+from archai.nlp.datasets.hf_datasets.tokenizer_utils.bert_tokenizer import BertTokenizer
+from archai.nlp.datasets.hf_datasets.tokenizer_utils.codegen_tokenizer import CodeGenTokenizer
+from archai.nlp.datasets.hf_datasets.tokenizer_utils.gpt2_tokenizer import GPT2Tokenizer
+from archai.nlp.datasets.hf_datasets.tokenizer_utils.pre_trained_tokenizer import ArchaiPreTrainedTokenizerFast
