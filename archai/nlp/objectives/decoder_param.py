bb00653c3d3b53421a6e47ab3ea529f1929fbf2f
piero2c <pierokauffmann@gmail.com>
2022-10-27 08:21:17 -0700

    feat(lts): non-embedding param proxy objective

---
+++ archai/nlp/objectives/decoder_param.py
@@ -0,0 +1,37 @@
+from overrides import overrides
+from typing import Optional, List, Type
+from archai.discrete_search import ArchaiModel, Objective, DatasetProvider
+
+from torch import nn
+
+class NonEmbeddingParamsProxy(Objective):
+    higher_is_better: bool = True
+
+    def __init__(self, exclude_cls: Optional[List[Type[nn.Module]]] = None, trainable_only: bool = True):
+        """Calculates the number of non-embedding parameters of a torch model.
+
+        Args:
+            exclude_cls (List[Type[nn.Module]], optional): List of nn.Module classes to be ignored during
+                parameter counting.
+            trainable_only (bool, optional): Only counts parameters with `param.requires_grad`.
+                Defaults to True.
+        """        
+        self.exclude_cls = [nn.Embedding] or exclude_cls
+        self.trainable_only = trainable_only
+    
+    @overrides
+    def evaluate(self, model: ArchaiModel, dataset: DatasetProvider,
+                 budget: Optional[float] = None) -> float:
+        total_params = sum(
+            param.numel() 
+            for param in model.arch.parameters()
+            if not self.trainable_only or param.requires_grad
+        )
+
+        embed_params = sum(
+            sum(param.numel() for param in module.parameters())
+            for module in model.arch.modules()
+            if isinstance(module, tuple(self.exclude_cls))
+        )
+
+        return total_params - embed_params
