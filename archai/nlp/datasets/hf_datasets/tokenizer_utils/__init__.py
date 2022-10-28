36fe5ec5bb7e4ac116b7f92e7342e07fb355eb94
Gustavo Rosa <gth.rosa@uol.com.br>
2022-10-28 10:15:07 -0300

    fix(nlp): Removes classes from datasets __init__.py to avoid hard-importing dependencies.

--- archai/nlp/datasets/hf_datasets/tokenizer_utils/__init__.py
+++ archai/nlp/datasets/hf_datasets/tokenizer_utils/__init__.py
@@ -3,8 +3,3 @@
 
 """Huggingface's-related tokenizers, pre-trained tokenizers and token's configuration.
 """
-
-from archai.nlp.datasets.hf_datasets.tokenizer_utils.bert_tokenizer import BertTokenizer
-from archai.nlp.datasets.hf_datasets.tokenizer_utils.codegen_tokenizer import CodeGenTokenizer
-from archai.nlp.datasets.hf_datasets.tokenizer_utils.gpt2_tokenizer import GPT2Tokenizer
-from archai.nlp.datasets.hf_datasets.tokenizer_utils.pre_trained_tokenizer import ArchaiPreTrainedTokenizerFast
