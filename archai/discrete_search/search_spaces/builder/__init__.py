4ebc6d34e069d516ac1ca7cb8a05bfffdef06c01
piero2c <pierokauffmann@gmail.com>
2022-10-28 18:46:57 -0700

    chore(ds): search space builder imports

--- archai/discrete_search/search_spaces/builder/__init__.py
+++ archai/discrete_search/search_spaces/builder/__init__.py
@@ -5,3 +5,7 @@ from archai.discrete_search.search_spaces.builder.discrete_choice import (
 from archai.discrete_search.search_spaces.builder.cell import (
     Cell, RepeatCell
 )
+
+from archai.discrete_search.search_spaces.builder.search_space import (
+    SearchSpaceBuilder
+)
