0ca9dd32efe12363289a4444034078a806d2f1e5
Gustavo Rosa <gth.rosa@uol.com.br>
2022-10-31 12:17:44 -0700

    chore(nlp): Deprecates utils module and moves files into thei related folders.

--- archai/nlp/trainers/nvidia/utils/__init__.py
+++ archai/nlp/trainers/nvidia/utils/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Utilities that are commonly re-used throughout the NLP package.
+"""NVIDIA-related trainer utilities.
 """
