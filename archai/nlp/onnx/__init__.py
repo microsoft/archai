b1754805717df051fa7694bc1d31f921c5c3ec49
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-03 07:58:02 -0700

    chore(nlp): Starts to implement an independent ONNX module.

---
+++ archai/nlp/onnx/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Integrates ONNX into high-level abstract classes and methods.
+"""
---
+++ archai/nlp/onnx/optimization_utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Provides post-exported ONNX optimization classes and methods.
+"""
