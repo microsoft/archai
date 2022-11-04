8f52ad5873fc1cadd5c90380b6f3d68f3b776795
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-04 07:41:04 -0700

    fix(nlp): Fixes the forward pass of position-wise ff with squared relu.

--- archai/nlp/search_spaces/transformer_flex/models/mem_transformer/utils/position_wise_ff.py
+++ archai/nlp/search_spaces/transformer_flex/models/mem_transformer/utils/position_wise_ff.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2018 Google AI, Google Brain, Carnegie Mellon University Authors and the HuggingFace Inc. team.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+#
 # Copyright (c) 2018, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0.
 
@@ -43,6 +45,38 @@ class PositionWiseFF(nn.Module):
             output += inputs
         else:
             output = self.ff(inputs)
+            output = self.layer_norm(inputs + output)
+
+        return output
+
+
+class PositionWiseFFPrimerEZ(nn.Module):
+    def __init__(self,
+                 d_model: int,
+                 d_inner: int,
+                 dropout: float,
+                 pre_lnorm: Optional[bool] = False) -> None:
+        super().__init__()
+
+        # Squared ReLU: https://arxiv.org/abs/2109.08668
+        self.d_model = d_model
+        self.d_inner = d_inner
+        self.dropout = dropout
+        self.pre_lnorm = pre_lnorm
+
+        self.ff1 = nn.Sequential(nn.Linear(d_model, d_inner), nn.ReLU(inplace=True))
+        self.ff2 = nn.Sequential(nn.Dropout(dropout),
+                                      nn.Linear(d_inner, d_model),
+                                      nn.Dropout(dropout))
+
+        self.layer_norm = nn.LayerNorm(d_model)
+        
+    def forward(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
+        if self.pre_lnorm:
+            output = self.ff2(self.ff1(self.layer_norm(inputs)) ** 2)
+            output += inputs
+        else:
+            output = self.ff2(self.ff1(inputs) ** 2)
             output = self.layer_norm(inputs + output)
 
         return output
