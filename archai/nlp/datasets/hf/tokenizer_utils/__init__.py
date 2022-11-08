75f1c53616be7a87d3e5fd7d29b2409de3b95b62
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-08 07:23:05 -0800

    chore(nlp): Adds tokenizer-based classes to their __init__.py to ease imports.

--- archai/nlp/datasets/hf/tokenizer_utils/__init__.py
+++ archai/nlp/datasets/hf/tokenizer_utils/__init__.py
@@ -8,5 +8,9 @@ from archai.nlp.datasets.hf.tokenizer_utils.bert_tokenizer import BertTokenizer
 from archai.nlp.datasets.hf.tokenizer_utils.char_tokenizer import CharTokenizer
 from archai.nlp.datasets.hf.tokenizer_utils.codegen_tokenizer import CodeGenTokenizer
 from archai.nlp.datasets.hf.tokenizer_utils.gpt2_tokenizer import GPT2Tokenizer
-from archai.nlp.datasets.hf.tokenizer_utils.pre_trained_tokenizer import ArchaiPreTrainedTokenizerFast
-from archai.nlp.datasets.hf.tokenizer_utils.transfo_xl_tokenizer import TransfoXLTokenizer
+from archai.nlp.datasets.hf.tokenizer_utils.pre_trained_tokenizer import (
+    ArchaiPreTrainedTokenizerFast,
+)
+from archai.nlp.datasets.hf.tokenizer_utils.transfo_xl_tokenizer import (
+    TransfoXLTokenizer,
+)
