03a57f8b8fdd06fb1962bab5ad8a85c3020f2f6a
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-08 07:15:38 -0800

    fix(nlp): Fixes TokenizerBase when training from iterator.

--- archai/nlp/datasets/hf/tokenizer_utils/__init__.py
+++ archai/nlp/datasets/hf/tokenizer_utils/__init__.py
@@ -3,3 +3,10 @@
 
 """Huggingface's-related tokenizers, pre-trained tokenizers and token's configuration.
 """
+
+from archai.nlp.datasets.hf.tokenizer_utils.bert_tokenizer import BertTokenizer
+from archai.nlp.datasets.hf.tokenizer_utils.char_tokenizer import CharTokenizer
+from archai.nlp.datasets.hf.tokenizer_utils.codegen_tokenizer import CodeGenTokenizer
+from archai.nlp.datasets.hf.tokenizer_utils.gpt2_tokenizer import GPT2Tokenizer
+from archai.nlp.datasets.hf.tokenizer_utils.pre_trained_tokenizer import ArchaiPreTrainedTokenizerFast
+from archai.nlp.datasets.hf.tokenizer_utils.transfo_xl_tokenizer import TransfoXLTokenizer
