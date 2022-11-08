43554986f8e0768c03baccdeddd4ef745466a1ae
piero2c <pierokauffmann@gmail.com>
2022-11-08 09:58:47 -0800

    feat(nlp/onnx): incomplete transformer flex latency obj

--- archai/nlp/objectives/transformer_flex_latency.py
+++ archai/nlp/objectives/transformer_flex_latency.py
@@ -1,30 +1,54 @@
+import copy
+import os
+import types
+from pathlib import Path
+import tempfile
+from typing import Optional, Sized
+
+from onnxruntime import GraphOptimizationLevel, InferenceSession, SessionOptions
+from onnxruntime.transformers import quantize_helper
+from archai.nlp.onnx.export_utils import prepare_model_for_onnx
 from overrides import overrides
-from typing import Optional
+import torch
 
-from archai.discrete_search import ArchaiModel, Objective, DatasetProvider
-from archai.nlp.compression.onnx_constraints import *
+from archai.discrete_search import ArchaiModel, DatasetProvider, Objective
+from archai.nlp.onnx.optimization import optimize_onnx
+from archai.nlp.onnx.onnx_forward import gpt2_onnx_forward
+from archai.nlp.onnx.export import export_to_onnx
 
+# ONNX-loading constants
+OMP_NUM_THREADS = 1
+OMP_WAIT_POLICY = 'ACTIVE'
 
-class TransformerFlexTorchLatency(Objective):
-    higher_is_better: bool = False
-
-    def __init__(self) -> None:
-        super().__init__()
-    
-    @overrides
-    def evaluate(self, arch: ArchaiModel, dataset: DatasetProvider,
-                 budget: Optional[float] = None) -> float:
-        return super().evaluate(arch, dataset, budget)
+# Constants available in onnxruntime
+# that enables performance optimization
+os.environ['OMP_NUM_THREADS'] = str(OMP_NUM_THREADS)
+os.environ['OMP_WAIT_POLICY'] = OMP_WAIT_POLICY
 
 
 class TransformerFlexOnnxLatency(Objective):
     higher_is_better: bool = False
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, search_space) -> None:
+        assert search_space.arch_type in ['gpt2', 'gpt2-flex']
+        self.search_space = search_space
     
+    def _load_and_prepare(self, config) -> torch.nn.Module:
+        config = copy.deepcopy(config)
+        config['use_cache'] = True
+
+        model = self.search_space._load_model_from_config(config)
+        
+        return prepare_model_for_onnx(model, self.search_space.arch_type)
+
     @overrides
     def evaluate(self, arch: ArchaiModel, dataset: DatasetProvider,
                  budget: Optional[float] = None) -> float:
-        return super().evaluate(arch, dataset, budget)
+        model = self._load_and_prepare(arch.metadata['config'])
+        
+        with tempfile.NamedTemporaryFile() as tmp:
+            export_to_onnx(model, Path(tmp.name), task='causal-lm',
+                           use_past=True, share_weights=True, opset=11)
+            optimize_onnx(Path(tmp.name), model.config)
+            
 
