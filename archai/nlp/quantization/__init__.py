9a3f56d309d3316bac7968906c0ae555a5ce6be7
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-09 06:10:40 -0800

    chore(nlp): Adds onnx and quantization public methods to init.

--- archai/nlp/quantization/__init__.py
+++ archai/nlp/quantization/__init__.py
@@ -3,3 +3,10 @@
 
 """Quantization-based classes, methods and definitions.
 """
+
+from archai.nlp.quantization.mixed_qat import MixedQAT
+from archai.nlp.quantization.ptq import (
+    dynamic_quantization_onnx,
+    dynamic_quantization_torch,
+)
+from archai.nlp.quantization.qat import prepare_with_qat
