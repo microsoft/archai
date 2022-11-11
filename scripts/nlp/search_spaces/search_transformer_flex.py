4757892ed0a05572d7d59d538d8d8291a4183bc3
Gustavo Rosa <gth.rosa@uol.com.br>
2022-11-11 06:09:21 -0800

    fix(scripts): Fixes imports on NLP-related scripts.

--- scripts/nlp/search_spaces/search_transformer_flex.py
+++ scripts/nlp/search_spaces/search_transformer_flex.py
@@ -4,14 +4,18 @@
 import argparse
 
 from archai.discrete_search.algos.evolution_pareto import EvolutionParetoSearch
-from archai.nlp.search_spaces.transformer_flex.search_space import TransformerFlexSearchSpace
 from archai.nlp.objectives.decoder_param import NonEmbeddingParamsProxy
+from archai.nlp.search_spaces.transformer_flex.search_space import (
+    TransformerFlexSearchSpace,
+)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Searches with TransformerFlex.")
 
-    parser.add_argument("-mt", "--model_type", type=str, choices=["gpt2", "gpt2-flex"], default="gpt2", help="Type of model.")
+    parser.add_argument(
+        "-mt", "--model_type", type=str, choices=["gpt2", "gpt2-flex"], default="gpt2", help="Type of model."
+    )
 
     parser.add_argument("-od", "--output_dir", type=str, default="", help="Output folder.")
 
@@ -22,12 +26,9 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args = parse_args()
-    
+
     space = TransformerFlexSearchSpace(args.model_type)
-    objectives = {
-        "non_embedding_params": NonEmbeddingParamsProxy()
-    }
+    objectives = {"non_embedding_params": NonEmbeddingParamsProxy()}
 
     algo = EvolutionParetoSearch(space, objectives, None, args.output_dir)
     algo.search()
-    
