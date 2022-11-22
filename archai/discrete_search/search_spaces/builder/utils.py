0bf9880a5d7f19e678ff0b63a9738b08ba3bea67
piero2c <pierokauffmann@gmail.com>
2022-11-22 13:59:06 -0800

    feat(builder): new util functions

--- archai/discrete_search/search_spaces/builder/utils.py
+++ archai/discrete_search/search_spaces/builder/utils.py
@@ -1,40 +1,78 @@
-from typing import Dict, Callable, Type
+from typing import Dict, Callable, Type, Iterable, Union, Any
 from copy import deepcopy
+from collections import OrderedDict
+
+from archai.discrete_search.search_spaces.builder.discrete_choice import DiscreteChoice
 
 
-def search_and_replace_node(param_tree: Dict, query: Type,
-                            repl_fn: Callable) -> Dict:    
-    def _search(d):
-        if isinstance(d, dict):
-            for k, v in d.items():
-                d[k] = _search(v)
-        
-        elif isinstance(d, list):
-            for i, v in enumerate(d):
-                d[i] = _search(v)
-        
-        elif isinstance(d, query):
-            return repl_fn(d)
-        
-        return d
-
-    return _search(deepcopy(param_tree))
-
-
-def replace_param_tree_pair(param_tree_1: dict, param_tree_2: dict, repl_fn: Callable) -> dict:
-    def _search(d1, d2):
-        if isinstance(d1, dict):
-            for k, v in d1.items():
-                d1[k] = _search(v, d2[k])
-        elif isinstance(d1, list):
-            for i, v in enumerate(d1):
-                d1[i] = _search(v, d2[i])
-        else:
-            return repl_fn(d1, d2)
-
-        return d1
+def flatten_dict(odict: Dict) -> OrderedDict:
+    fdict = OrderedDict()
     
-    return _search(
-        deepcopy(param_tree_1),
-        deepcopy(param_tree_2)
-    )
+    def _flatten(prefix, d):
+        prefix = prefix + '.' if prefix else prefix
+        
+        if isinstance(d, OrderedDict):
+            for k, v in d.items():
+                flat_v = _flatten(prefix + k, v)
+                
+                if flat_v is not None:
+                    fdict[prefix + k] = flat_v        
+        else:
+            return d
+    
+    _flatten('', odict)
+    return fdict
+
+
+def replace_ptree_choices(config_tree: Union[Dict, DiscreteChoice],
+                          repl_fn: Callable[[DiscreteChoice], Any]):
+    
+    def _replace_tree_nodes(node, repl_fn, ref_map):
+        if isinstance(node, dict):
+            output_tree = OrderedDict()
+
+            for param_name, param in node.items():
+                output_tree[param_name] = _replace_tree_nodes(param, repl_fn, ref_map)
+
+        elif isinstance(node, DiscreteChoice):
+            if id(node) not in ref_map:
+                ref_map[id(node)] = repl_fn(node)
+            return ref_map[id(node)]
+
+        else:
+            return node
+    
+        return output_tree
+
+    return _replace_tree_nodes(config_tree, repl_fn, {})
+
+
+def replace_ptree_pair_choices(query_tree: Union[Dict, DiscreteChoice],
+                                    aux_tree: Union[Dict, Any],
+                                    repl_fn: Callable[[DiscreteChoice, Any], Any]):
+    
+    def _replace_tree_nodes(query_node, aux_node, repl_fn, ref_map):
+        if isinstance(query_node, dict):
+            output_tree = OrderedDict()
+
+            for param_name, param in query_node.items():
+                assert param_name in aux_node,\
+                    '`aux_tree` must be identical to `query_tree` apart from terminal nodes'
+
+                output_tree[param_name] = _replace_tree_nodes(
+                    param, aux_node[param_name], repl_fn, ref_map
+                )
+
+        elif isinstance(query_node, DiscreteChoice):
+            if id(query_node) not in ref_map:
+                ref_map[id(query_node)] = repl_fn(query_node, aux_node)
+            
+            return ref_map[id(query_node)]
+
+        else:
+            return query_node
+    
+        return output_tree
+
+    return _replace_tree_nodes(query_tree, aux_tree, repl_fn, {})
+
