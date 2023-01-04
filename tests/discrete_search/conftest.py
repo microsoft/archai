22b7b4ce2986c1eafa6e7dc12894f63fa4f4da72
piero2c <pierokauffmann@gmail.com>
2023-01-04 15:17:47 -0800

    chore(tests): cleaner search algo tests using fixtures

---
+++ tests/discrete_search/conftest.py
@@ -0,0 +1,5 @@
+pytest_plugins = [
+    "algo_fixtures.search_space",
+    "algo_fixtures.objectives",
+    "algo_fixtures.surrogate_model",
+]
--- tests/discrete_search/test_evolution_pareto.py
+++ tests/discrete_search/test_evolution_pareto.py
@@ -1,75 +1,12 @@
 import pytest
-from overrides import overrides
-from typing import List
-from random import Random
-import json
 import os
 
-from archai.discrete_search import ArchaiModel, EvolutionarySearchSpace, SearchObjectives
 from archai.discrete_search.algos.evolution_pareto import EvolutionParetoSearch
-from archai.discrete_search.evaluators.functional import EvaluationFunction
-
-class DummySearchSpace(EvolutionarySearchSpace):
-        def __init__(self, seed: int = 10) -> None:
-            self.rng = Random(seed)
-        
-        @overrides
-        def random_sample(self) -> ArchaiModel:
-            return ArchaiModel(None, archid=str(self.rng.randint(0, 100_000)))
-
-        @overrides
-        def mutate(self, arch: ArchaiModel) -> ArchaiModel:
-            archid = arch.archid
-            return ArchaiModel(None, str(int(archid) + self.rng.randint(-10, 10)))
-
-        @overrides
-        def crossover(self, arch_list: List[ArchaiModel]) -> ArchaiModel:
-            m1, m2 = arch_list[:2]
-            new_archid = int((int(m1.archid) + int(m2.archid)) / 2)
-            return ArchaiModel(None, str(new_archid))
-
-        @overrides
-        def save_arch(self, model: ArchaiModel, path: str) -> None:
-            json.dump({'archid': model.archid}, open(path, 'w'))
-        
-        @overrides
-        def load_arch(self, path: str) -> ArchaiModel:
-            return ArchaiModel(None, json.load(path)['archid'])
-        
-        @overrides
-        def load_model_weights(self, model: ArchaiModel, path: str) -> None:
-            pass
-
-        @overrides
-        def save_model_weights(self, model: ArchaiModel, path: str) -> None:
-            pass
-
-@pytest.fixture
-def search_objectives():
-    rng1 = Random(1)
-    rng2 = Random(2)
-    rng3 = Random(3)
-
-    o1 = EvaluationFunction(lambda m, d, b: rng1.random())
-    o2 = EvaluationFunction(lambda m, d, b: rng2.random())
-    r = EvaluationFunction(lambda m, d, b: rng3.random())
-
-    so = SearchObjectives()
-    so.add_cheap_objective('Random1', o1, higher_is_better=False, constraint=(0.0, 0.4))
-    so.add_expensive_objective('Random2', o2, higher_is_better=True)
-
-    so.add_extra_constraint('Random3 constraint', r, constraint=(0.0, 0.6))
-
-    return so
-
-@pytest.fixture
-def search_space():
-    return DummySearchSpace()
 
 
 @pytest.fixture(scope="session")
 def output_dir(tmp_path_factory):
-    return tmp_path_factory.mktemp('out_evo')
+    return tmp_path_factory.mktemp('out')
 
 
 def test_pareto_evolution(output_dir, search_space, search_objectives):
@@ -77,7 +14,7 @@ def test_pareto_evolution(output_dir, search_space, search_objectives):
         search_space, search_objectives, None, output_dir, num_iters=30, 
         init_num_models=20
     )
-    
+
     search_results = algo.search()
     assert len(os.listdir(output_dir)) > 0
 
@@ -89,4 +26,3 @@ def test_pareto_evolution(output_dir, search_space, search_objectives):
     # Checks if all registered models satisfy constraints
     _, valid_models = search_objectives.eval_constraints(all_models, None)
     assert len(valid_models) == len(all_models)
-
