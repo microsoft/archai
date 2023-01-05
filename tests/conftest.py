d5ac39494376a8b31c47cfbbc41772f74071f40a
piero2c <pierokauffmann@gmail.com>
2023-01-05 12:39:56 -0800

    fix(tests): moves `conftest.py` to tests root directory

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+pytest_plugins = [
+    "discrete_search.algo_fixtures.search_space",
+    "discrete_search.algo_fixtures.objectives",
+    "discrete_search.algo_fixtures.surrogate_model",
+]
+ 
--- tests/discrete_search/conftest.py
+++
@@ -1,5 +0,0 @@
-pytest_plugins = [
-    "algo_fixtures.search_space",
-    "algo_fixtures.objectives",
-    "algo_fixtures.surrogate_model",
-]
