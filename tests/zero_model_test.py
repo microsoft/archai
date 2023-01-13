b0df5bd5a7a5e0ec82e7872b355e27dd37b5ad38
piero2c <pierokauffmann@gmail.com>
2023-01-13 18:53:59 -0300

    fix(tests): re-adds requires_gpus decorator

--- tests/zero_model_test.py
+++ tests/zero_model_test.py
@@ -2,10 +2,19 @@
 # Licensed under the MIT license.
 
 import torch
+from typing import Callable
 from archai.nas.model import Model
 from archai.nas.model_desc_builder import ModelDescBuilder
 from archai.common.common import common_init
 
+
+def requires_gpu(test_fn: Callable):
+    if torch.cuda.is_available() and torch.cuda.device_count() > 0:
+        return test_fn()
+    return
+
+
+@requires_gpu
 def test_darts_zero_model():
     conf = common_init(config_filepath='confs/algos/darts.yaml')
     conf_search = conf['nas']['search']
@@ -17,6 +26,7 @@ def test_darts_zero_model():
     y, aux = m(torch.rand((1, 3, 32, 32)))
     assert isinstance(y, torch.Tensor) and y.shape==(1,10) and aux is None
 
+@requires_gpu
 def test_petridish_zero_model():
     conf = common_init(config_filepath='confs/algos/petridish_toy.yaml')
     conf_search = conf['nas']['search']
