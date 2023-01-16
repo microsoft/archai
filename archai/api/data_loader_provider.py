659e933cf6fc7e08c63d792817c02004044899a3
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-16 10:12:55 -0300

    chore(archai): Adds data loader provider and its torch-based class.

--- archai/api/data_loader_provider.py
+++ archai/api/data_loader_provider.py
@@ -2,18 +2,35 @@
 # Licensed under the MIT license.
 
 from abc import abstractmethod
+from typing import Any
 
 from overrides import EnforceOverrides
 
 
 class DataLoaderProvider(EnforceOverrides):
-    """"""
+    """Abstract class for data loader provider."""
 
     def __init__(self) -> None:
-        """"""
+        """Initializes data loader provider."""
 
-        super().__init__()
+        pass
 
     @abstractmethod
-    def get_data_loader():
-        """"""
+    def get_data_loader(self, dataset: Any) -> Any:
+        """Get a data loader based on the input dataset.
+
+        This function needs to be overriden as any logic can be applied to
+        get the data loader.
+
+        Args:
+            dataset: Input dataset.
+
+        Returns:
+            Data loader based on the input dataset.
+
+        Examples:
+            >>> return torch.utils.data.DataLoader(dataset)
+
+        """
+
+        pass
