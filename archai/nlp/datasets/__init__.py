584d06ff56065547b45609d89b4f7004e9aab314
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-17 17:09:48 -0300

    chore(archai): Deprecates the nlp package as it files and packages have already been rearrenged.

--- archai/nlp/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-"""Dataset-related classes and methods."""
--- archai/nlp/eval/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-"""Evaluation-related classes and methods, such as few-shot and text prediction."""
