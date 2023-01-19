b84563df329147f78168ab2ce29b82530005fb02
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-19 14:42:41 -0300

    chore(archai): Adds docstrings to archai.common.logger.py.

--- archai/common/logger.py
+++ archai/common/logger.py
@@ -5,8 +5,11 @@ from __future__ import annotations
 
 import itertools
 import logging
+import os
+import pathlib
 import time
 from collections import OrderedDict
+from types import TracebackType
 from typing import Any, Dict, List, Optional, Union
 
 import yaml
@@ -15,16 +18,23 @@ from archai.common.logger_utils import get_logger
 
 
 class Logger:
-    """"""
+    """Logs and saves data in a hierarchical YAML structure."""
 
     def __init__(
-        self, source: Optional[str] = None, file_path: Optional[str] = None, delay: Optional[float] = 30.0
+        self, source: Optional[str] = None, file_path: Optional[str] = "archai.log.yaml", delay: Optional[float] = 30.0
     ) -> None:
-        """"""
+        """Initializes the logger.
+
+        Args:
+            source: Source of the logger.
+            file_path: File path of the log file.
+            delay: Delay between log saves.
+
+        """
 
         self.logger = get_logger(source or __name__)
 
-        self.file_path = file_path or "archai.log.yaml"
+        self.file_path = file_path
         self.delay = delay
 
         self.call_count = 0
@@ -33,27 +43,71 @@ class Logger:
         self.paths = [[""]]
         self.stack = [OrderedDict()]
 
+        if os.path.exists(self.file_path):
+            backup_file_path = pathlib.Path(self.file_path)
+            backup_file_path.rename(backup_file_path.with_suffix(f".{str(int(time.time()))}.yaml"))
+
     def __enter__(self) -> Logger:
+        """Context manager entry method.
+
+        Returns:
+            Instance of Logger.
+
+        """
+
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type: type[BaseException], exc_val: BaseException, exc_tb: TracebackType) -> None:
+        """Context manager exit method.
+
+        It pops the current node from the stack.
+
+        """
+
         self.popd()
 
     def __contains__(self, key: str) -> bool:
+        """Check if a key is in the current node.
+
+        Args:
+            key: key to check.
+
+        Returns:
+            `True` if key is in the current node, `False` otherwise.
+
+        """
+
         return key in self.current_node
 
     def __len__(self) -> int:
+        """Calculate the number of items in the current node.
+
+        Returns:
+            The number of items in the current node.
+
+        """
+
         return len(self.current_node)
 
     @property
     def root_node(self) -> OrderedDict:
-        """"""
+        """Root node of the current stack.
+
+        Returns:
+            Root node of the current stack.
+
+        """
 
         return self.stack[0]
 
     @property
     def current_node(self) -> OrderedDict:
-        """"""
+        """Current node of the current stack.
+
+        Returns:
+            Current node of the current stack.
+
+        """
 
         last_obj = None
 
@@ -76,16 +130,48 @@ class Logger:
 
     @property
     def current_path(self) -> str:
+        """Current path of the current stack.
+
+        Returns:
+            Current path of the current stack.
+
+        """
+
         return "/".join(itertools.chain.from_iterable(self.paths[1:]))
 
-    def save(self) -> None:
-        with open(self.file_path, "w") as f:
+    def save(self, file_path: Optional[str] = None) -> None:
+        """Save the current log data to an output file.
+
+        Args:
+            file_path: File path to save the log data to. If `None`,
+                defaults to the file path provided during initialization.
+
+        """
+
+        file_path = file_path or self.file_path
+
+        with open(file_path, "w") as f:
             yaml.dump(self.root_node, f)
 
     def load(self, file_path: str) -> None:
+        """Load log data from an input file.
+
+        Args:
+            file_path (str): File path to load data from.
+
+        """
+
         with open(file_path, "r") as f:
             obj = yaml.load(f, Loader=yaml.Loader)
             self.stack = [obj]
+
+    def close(self) -> None:
+        """Close the logger."""
+
+        self.save()
+
+        for handler in self.logger.handlers:
+            handler.flush()
 
     def update_key(
         self,
@@ -95,6 +181,20 @@ class Logger:
         path: Optional[List[str]] = None,
         override_key: Optional[bool] = False,
     ) -> None:
+        """Update a key in a node in the current stack.
+
+        Args:
+            key: Key to update.
+            value: Calue to update the key with.
+            node: Node to update the key in.
+            path: Path to the node in the current stack.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        Raises:
+            KeyError: If the key is already being used in the current node and `override_key` is `False`.
+
+        """
+
         if not override_key and key in self.current_node:
             raise KeyError(f"`{key}` is already being used. Cannot use it again, unless popd() is called.")
 
@@ -108,7 +208,13 @@ class Logger:
         current_node[str(key)] = value
 
     def update(self, obj: Dict[str, Any], override_key: Optional[bool] = False) -> None:
-        """"""
+        """Update the current node with the key-value pairs in the provided object.
+
+        Args:
+            obj: Dictionary to update the current node with.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         for k, v in obj.items():
             self.update_key(k, v, override_key=override_key)
@@ -116,7 +222,14 @@ class Logger:
     def log(
         self, obj: Union[Dict[str, Any], str], level: Optional[int] = None, override_key: Optional[bool] = False
     ) -> None:
-        """"""
+        """Log the provided dictionary/string at the specified level.
+
+        Args:
+            obj: Object to log.
+            level: Logging level.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         self.call_count += 1
 
@@ -139,28 +252,57 @@ class Logger:
             self.save()
             self.timestamp = time.time()
 
-    def info(self, obj: Dict[str, Any], override_key: Optional[bool] = False) -> None:
-        """"""
+    def info(self, obj: Union[Dict[str, Any], str], override_key: Optional[bool] = False) -> None:
+        """Log the provided dictionary/string at the `info` level.
+
+        Args:
+            obj: Object to log.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         self.log(obj, level=logging.INFO, override_key=override_key)
 
-    def debug(self, obj: Dict[str, Any], override_key: Optional[bool] = False) -> None:
-        """"""
+    def debug(self, obj: Union[Dict[str, Any], str], override_key: Optional[bool] = False) -> None:
+        """Log the provided dictionary/string at the `debug` level.
+
+        Args:
+            obj: Object to log.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         self.log(obj, level=logging.DEBUG, override_key=override_key)
 
-    def warn(self, obj: Dict[str, Any], override_key: Optional[bool] = False) -> None:
-        """"""
+    def warn(self, obj: Union[Dict[str, Any], str], override_key: Optional[bool] = False) -> None:
+        """Log the provided dictionary/string at the `warning` level.
+
+        Args:
+            obj: Object to log.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         self.log(obj, level=logging.WARNING, override_key=override_key)
 
-    def error(self, obj: Dict[str, Any], override_key: Optional[bool] = False) -> None:
-        """"""
+    def error(self, obj: Union[Dict[str, Any], str], override_key: Optional[bool] = False) -> None:
+        """Log the provided dictionary/string at the `error` level.
+
+        Args:
+            obj: Object to log.
+            override_key: Whether key can be overridden if it's already in current node.
+
+        """
 
         self.log(obj, level=logging.ERROR, override_key=override_key)
 
     def pushd(self, *keys: Any) -> Logger:
-        """"""
+        """Push the provided keys onto the current path stack.
+
+        Returns:
+            Instance of current logger.
+
+        """
 
         self.paths.append([str(k) for k in keys])
         self.stack.append(None)  # Delays creation of node until it is needed
@@ -168,10 +310,10 @@ class Logger:
         return self  # Allows to call __enter__
 
     def popd(self) -> None:
-        """"""
+        """Pop the last path and node off the stack."""
 
         if len(self.stack) == 1:
-            self.warn("Invalid call due to no child logger.")
+            self.warn("Invalid call. No available child in the stack.")
             return
 
         self.stack.pop()
