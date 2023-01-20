4b5b12136b1e3c7b97392b3ef322489ae91e2a50
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-20 12:45:52 -0300

    fix(archai): Fixes wrong import for get_full_path method.

--- archai/datasets/nlp/nvidia_dataset_provider_utils.py
+++ archai/datasets/nlp/nvidia_dataset_provider_utils.py
@@ -8,9 +8,8 @@ from typing import Optional, Tuple
 import numpy as np
 import torch
 
-from archai.common.common import default_dataroot, pt_dirs
+from archai.common.file_utils import get_full_path
 from archai.common.logger import Logger
-from archai.common.utils import delete_file, full_path
 from archai.datasets.nlp.tokenizer_utils.bbpe_tokenizer import BbpeTokenizer
 from archai.datasets.nlp.tokenizer_utils.gpt2_tokenizer import Gpt2Tokenizer
 from archai.datasets.nlp.tokenizer_utils.tokenizer_base import TokenizerBase
@@ -19,73 +18,22 @@ from archai.datasets.nlp.tokenizer_utils.word_tokenizer import WordTokenizer
 logger = Logger(source=__name__)
 
 
-def get_dataset_dir_name(dataset_name: str) -> str:
-    """Get the name of the datasets' directory.
+def delete_file(file_path: str) -> bool:
+    """Delete a file.
 
     Args:
-        dataset: Name of dataset.
+        file_path: Path to the file.
 
     Returns:
-        Name of the datasets directory.
-
-    Raises:
-        RuntimeError: If the dataset is not supported.
+        `True` if the file was deleted, `False` otherwise.
 
     """
 
-    if dataset_name == "wt2":
-        return "wikitext-2"
-    if dataset_name == "wt103":
-        return "wikitext-103"
-    if dataset_name == "lm1b":
-        return "one-billion-words"
-    if dataset_name.startswith("olx_"):
-        return dataset_name
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        return True
 
-    raise RuntimeError(f"Dataset: {dataset_name} is not supported yet.")
-
-
-def create_dirs(
-    dataroot: str,
-    dataset_name: str,
-    experiment_name: Optional[str] = "tmp",
-    output_dir: Optional[str] = "~/logdir",
-    pretrained_path: Optional[str] = "",
-    cache_dir: Optional[str] = "",
-) -> Tuple[str, str, str, str]:
-    """Create dataset-related folders with proper paths.
-
-    Args:
-        dataroot: Dataset folder.
-        dataset_name: Name of the dataset.
-        experiment_name: Experiment name.
-        output_dir: Output folder.
-        pretrained_path: Path to the pre-trained checkpoint file.
-        cache_dir: Dataset cache folder.
-
-    Returns:
-        Tuple containing dataset, output, pre-trained checkpoint and cache folders.
-
-    """
-
-    pt_data_dir, pt_output_dir = pt_dirs()
-    if pt_output_dir:
-        pt_output_dir = os.path.join(pt_output_dir, experiment_name)
-
-    dataroot = dataroot or pt_data_dir or default_dataroot()
-    dataroot = full_path(dataroot)
-
-    dataset_dir = full_path(os.path.join(dataroot, "textpred", get_dataset_dir_name(dataset_name)))
-    output_dir = full_path(pt_output_dir or os.path.join(output_dir, experiment_name), create=True)
-
-    if not os.path.isabs(cache_dir):
-        cache_dir = os.path.join(dataset_dir, cache_dir)
-    cache_dir = full_path(cache_dir, create=True)
-
-    if not os.path.isabs(pretrained_path) and pretrained_path:
-        pretrained_path = os.path.join(os.path.dirname(output_dir), pretrained_path)
-
-    return dataset_dir, output_dir, pretrained_path, cache_dir
+    return False
 
 
 class Corpus:
@@ -120,7 +68,7 @@ class Corpus:
         self.vocab_size = vocab_size
 
         # Corpus cache is created using dataset/vocab_type/vocab_size path
-        self.corpus_cache_dir = full_path(
+        self.corpus_cache_dir = get_full_path(
             os.path.join(cache_dir, str(dataset_name), str(vocab_type), str(vocab_size)), create=True
         )
 
--- archai/trainers/nlp/nvidia_training_args.py
+++ archai/trainers/nlp/nvidia_training_args.py
@@ -3,17 +3,73 @@
 
 import os
 from dataclasses import dataclass, field
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Tuple
 
 import numpy as np
 import torch
 
+from archai.common.common import default_dataroot, pt_dirs
 from archai.common.distributed_utils import (
     get_world_size,
     init_distributed,
     sync_workers,
 )
-from archai.datasets.nlp.nvidia_dataset_provider_utils import create_dirs
+from archai.common.file_utils import get_full_path
+
+
+def create_dirs(
+    dataroot: str,
+    dataset_name: str,
+    experiment_name: Optional[str] = "tmp",
+    output_dir: Optional[str] = "~/logdir",
+    pretrained_path: Optional[str] = "",
+    cache_dir: Optional[str] = "",
+) -> Tuple[str, str, str, str]:
+    """Create dataset-related folders with proper paths.
+
+    Args:
+        dataroot: Dataset folder.
+        dataset_name: Name of the dataset.
+        experiment_name: Experiment name.
+        output_dir: Output folder.
+        pretrained_path: Path to the pre-trained checkpoint file.
+        cache_dir: Dataset cache folder.
+
+    Returns:
+        Tuple containing dataset, output, pre-trained checkpoint and cache folders.
+
+    """
+
+    def _get_dataset_dir_name(dataset_name: str) -> str:
+        if dataset_name == "wt2":
+            return "wikitext-2"
+        if dataset_name == "wt103":
+            return "wikitext-103"
+        if dataset_name == "lm1b":
+            return "one-billion-words"
+        if dataset_name.startswith("olx_"):
+            return dataset_name
+
+        raise RuntimeError(f"Dataset: {dataset_name} is not supported yet.")
+
+    pt_data_dir, pt_output_dir = pt_dirs()
+    if pt_output_dir:
+        pt_output_dir = os.path.join(pt_output_dir, experiment_name)
+
+    dataroot = dataroot or pt_data_dir or default_dataroot()
+    dataroot = get_full_path(dataroot)
+
+    dataset_dir = get_full_path(os.path.join(dataroot, "textpred", _get_dataset_dir_name(dataset_name)))
+    output_dir = get_full_path(pt_output_dir or os.path.join(output_dir, experiment_name), create_folder=True)
+
+    if not os.path.isabs(cache_dir):
+        cache_dir = os.path.join(dataset_dir, cache_dir)
+    cache_dir = get_full_path(cache_dir, create_folder=True)
+
+    if not os.path.isabs(pretrained_path) and pretrained_path:
+        pretrained_path = os.path.join(os.path.dirname(output_dir), pretrained_path)
+
+    return dataset_dir, output_dir, pretrained_path, cache_dir
 
 
 @dataclass
