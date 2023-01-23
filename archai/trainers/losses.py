6bec012e46d39443c49a8fa700a89bad28d942d2
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-23 11:25:48 -0300

    chore(archai): Adds proper docstrings to some archai.trainers modules.

--- archai/trainers/losses.py
+++ archai/trainers/losses.py
@@ -1,20 +1,48 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from typing import Optional
+
 import torch
-import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
-from torch import nn
-from torch.nn.modules.loss import _Loss, _WeightedLoss
-from torch.optim import SGD, Adam, lr_scheduler
+from torch.nn.modules.loss import _WeightedLoss
 
 
 class SmoothCrossEntropyLoss(_WeightedLoss):
-    def __init__(self, weight=None, reduction="mean", smoothing=0.0):
+    """Cross entropy loss with label smoothing support."""
+
+    def __init__(
+        self, weight: Optional[torch.Tensor] = None, reduction: Optional[str] = "mean", smoothing: Optional[float] = 0.0
+    ) -> None:
+        """Initializes the loss function.
+
+        Args:
+            weight: Weight for each class.
+            reduction: Reduction method.
+            smoothing: Label smoothing factor.
+
+        """
+
         super().__init__(weight=weight, reduction=reduction)
+
         self.smoothing = smoothing
         self.weight = weight
         self.reduction = reduction
 
     @staticmethod
-    def _smooth_one_hot(targets: torch.Tensor, n_classes: int, smoothing=0.0):
+    def _smooth_one_hot(targets: torch.Tensor, n_classes: int, smoothing: Optional[float] = 0.0) -> torch.Tensor:
+        """Applies label smoothing to targets.
+
+        Args:
+            targets: Target tensor.
+            n_classes: Number of classes.
+            smoothing: Label smoothing factor.
+
+        Returns:
+            Smoothed targets.
+
+        """
+
         assert 0 <= smoothing < 1
         with torch.no_grad():
             # For label smoothing, we replace 1-hot vector with 0.9-hot vector instead.
@@ -27,11 +55,22 @@ class SmoothCrossEntropyLoss(_WeightedLoss):
             )
         return targets
 
-    def forward(self, inputs, targets):
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Computes the loss.
+
+        Args:
+            inputs: Input tensor.
+            targets: Target tensor.
+
+        Returns:
+            Loss value.
+
+        """
+
         targets = SmoothCrossEntropyLoss._smooth_one_hot(targets, inputs.size(-1), self.smoothing)
         lsm = F.log_softmax(inputs, -1)
 
-        if self.weight is not None:  # to support weighted targets
+        if self.weight is not None:  # To support weighted targets
             lsm = lsm * self.weight.unsqueeze(0)
 
         loss = -(targets * lsm).sum(-1)
@@ -42,55 +81,3 @@ class SmoothCrossEntropyLoss(_WeightedLoss):
             loss = loss.mean()
 
         return loss
-
-
-# Credits: https://github.com/NVIDIA/DeepLearningExamples/blob/342d2e7649b9a478f35ea45a069a4c7e6b1497b8/PyTorch/Classification/ConvNets/main.py#L350
-class NLLMultiLabelSmooth(nn.Module):
-    """According to NVidia code, this should be used with mixup?"""
-
-    def __init__(self, smoothing=0.0):
-        super(NLLMultiLabelSmooth, self).__init__()
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
-
-    def forward(self, x, target):
-        if self.training:
-            x = x.float()
-            target = target.float()
-            logprobs = torch.nn.functional.log_softmax(x, dim=-1)
-
-            nll_loss = -logprobs * target
-            nll_loss = nll_loss.sum(-1)
-
-            smooth_loss = -logprobs.mean(dim=-1)
-
-            loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-
-            return loss.mean()
-        else:
-            return torch.nn.functional.cross_entropy(x, target)
-
-
-class LabelSmoothing(nn.Module):
-    """
-    NLL loss with label smoothing.
-    """
-
-    def __init__(self, smoothing=0.0):
-        """
-        Constructor for the LabelSmoothing module.
-
-        :param smoothing: label smoothing factor
-        """
-        super(LabelSmoothing, self).__init__()
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
-
-    def forward(self, x, target):
-        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
-
-        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
-        nll_loss = nll_loss.squeeze(1)
-        smooth_loss = -logprobs.mean(dim=-1)
-        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-        return loss.mean()
