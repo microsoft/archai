513d09c7a15db8c3bb76918e158ed15553603a25
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-23 11:48:04 -0300

    chore(archai): Removes unnecessary imports from archai.datasets.distributed_stratified_sampler.

--- archai/datasets/distributed_stratified_sampler.py
+++ archai/datasets/distributed_stratified_sampler.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT license.
 
 import math
-from typing import List, Optional, Tuple
+from typing import Iterable, Optional, Tuple
 
 import numpy as np
 import torch
@@ -13,37 +13,59 @@ from torch.utils.data.dataset import Dataset
 
 
 class DistributedStratifiedSampler(Sampler):
+    """Distributed stratified sampling of dataset.
+
+    This sampler works in distributed as well as non-distributed setting with no penalty in
+    either mode and is a replacement for built-in `torch.util.data.DistributedSampler`.
+
+    In distributed setting, many instances of the same code runs as process
+    known as replicas. Each replica has sequential number assigned by the launcher,
+    starting from 0 to uniquely identify it. This is known as global rank or simply rank.
+    The number of replicas is known as the world size. For non-distributed setting,
+    `world_size=1` and `rank=0`.
+
+    This sampler assumes that labels for each datapoint is available in dataset.targets
+    property which should be array like containing as many values as length of the dataset.
+    This is availalble already for many popular datasets such as cifar and, with newer
+    PyTorch versions, `ImageFolder` as well as `DatasetFolder`. If you are using custom dataset,
+    you can usually create this property with one line of code such as
+    `dataset.targets = [yi for _, yi in dataset]`.
+
+    To do distributed sampling, each replica must shuffle with same seed as all other
+    replicas with every epoch and then chose some subset of dataset for itself.
+    Traditionally, we use epoch number as seed for shuffling for each replica.
+    However, this then requires that training code calls `sampler.set_epoch(epoch)`
+    to set seed at every epoch.
+
+    """
+
     def __init__(
         self,
         dataset: Dataset,
         world_size: Optional[int] = None,
         rank: Optional[int] = None,
-        shuffle=True,
+        shuffle: Optional[bool] = True,
         val_ratio: Optional[float] = 0.0,
-        is_val=False,
-        max_items: Optional[int] = None,
-    ):
-        """Performs stratified sampling of dataset for each replica in the distributed as well as non-distributed setting. If validation split is needed then yet another stratified sampling within replica's split is performed to further obtain the train/validation splits.
+        is_val_split: Optional[bool] = False,
+        max_samples: Optional[int] = None,
+    ) -> None:
+        """Initializes the sampler.
 
-        This sampler works in distributed as well as non-distributed setting with no panelty in either mode and is replacement for built-in torch.util.data.DistributedSampler. In distributed setting, many instances of the same code runs as process known as replicas. Each replica has sequential number assigned by the launcher, starting from 0 to uniquely identify it. This is known as global rank or simply rank. The number of replicas is known as the world size. For non-distributed setting, world_size=1 and rank=0.
+        Args:
+            dataset: Input dataset.
+            world_size: Total number of replicas. If `None` then auto-detect,
+                while 1 for non-distributed setting.
+            rank: Global rank of this replica. If `None` then auto-detect,
+                while 0 for non-distributed setting.
+            shuffle: Whether shuffling should be applied for every epoch.
+            val_ratio: Creates a validation split when set to > 0.
+            is_val_split: Whether the validation split should be returned.
+            max_samples: Maximum number of samples for each replica.
 
-        To perform stratified sampling we need labels. This sampler assumes that labels for each datapoint is available in dataset.targets property which should be array like containing as many values as length of the dataset. This is availalble already for many popular datasets such as cifar and, with newer PyTorch versions, ImageFolder as well as DatasetFolder. If you are using custom dataset, you can usually create this property with one line of code such as `dataset.targets = [yi for _, yi in dataset]`.
-
-        Generally, to do distributed sampling, each replica must shuffle with same seed as all other replicas with every epoch and then chose some subset of dataset for itself. Traditionally, we use epoch number as seed for shuffling for each replica. However, this then requires that training code calls sampler.set_epoch(epoch) to set seed at every epoch.
-
-        Arguments:
-            dataset -- PyTorch dataset like object
-
-        Keyword Arguments:
-            world_size -- Total number of replicas running in distributed setting, if None then auto-detect, 1 for non distributed setting (default: {None})
-            rank -- Global rank of this replica, if None then auto-detect, 0 for non distributed setting (default: {None})
-            shuffle {bool} -- If True then suffle at every epoch (default: {True})
-            val_ratio {float} -- If you want to create validation split then set to > 0 (default: {0.0})
-            is_val {bool} -- If True then validation split is returned set to val_ratio otherwise main split is returned (default: {False})
-            max_items -- if >= 0 then dataset will be trimmed to these many items for each replica (useful to test on smaller dataset)
         """
 
-        # cifar10 amd DatasetFolder has this attribute, for others it may be easy to add from outside
+        # CIFAR-10 amd DatasetFolder has this attribute
+        # For others it may be easy to add from outside
         assert (
             hasattr(dataset, "targets") and dataset.targets is not None
         ), "dataset needs to have targets attribute to work with this sampler"
@@ -53,11 +75,13 @@ class DistributedStratifiedSampler(Sampler):
                 world_size = dist.get_world_size()
             else:
                 world_size = 1
+
         if rank is None:
             if dist.is_available() and dist.is_initialized():
                 rank = dist.get_rank()
             else:
                 rank = 0
+
         if val_ratio is None:
             val_ratio = 0.0
 
@@ -68,69 +92,84 @@ class DistributedStratifiedSampler(Sampler):
         self.dataset = dataset
         self.world_size = world_size
         self.rank = rank
-        self.epoch = 0  # this will be used as seed so cannot be < 0
+        self.epoch = 0  # Used as a seed
 
         self.shuffle = shuffle
         self.data_len = len(self.dataset)
-        self.max_items = max_items if max_items is not None and max_items >= 0 else None
+        self.max_samples = max_samples if max_samples is not None and max_samples >= 0 else None
         assert self.data_len == len(dataset.targets)
-        self.val_ratio = val_ratio
-        self.is_val = is_val
 
-        # computing duplications we needs
+        self.val_ratio = val_ratio
+        self.is_val_split = is_val_split
+
+        # Computing duplications of dataset to make it divisible by world_size
         self.replica_len = self.replica_len_full = int(math.ceil(float(self.data_len) / self.world_size))
         self.total_size = self.replica_len_full * self.world_size
         assert self.total_size >= self.data_len
 
-        if self.max_items is not None:
-            self.replica_len = min(self.replica_len_full, self.max_items)
+        if self.max_samples is not None:
+            self.replica_len = min(self.replica_len_full, self.max_samples)
 
         self.main_split_len = int(math.floor(self.replica_len * (1 - val_ratio)))
         self.val_split_len = self.replica_len - self.main_split_len
-        self._len = self.val_split_len if self.is_val else self.main_split_len
+        self._len = self.val_split_len if self.is_val_split else self.main_split_len
 
-    def __iter__(self):
-        # get shuffled indices, dataset is extended if needed to divide equally
-        # between replicas
-        indices, targets = self._indices()
+    def __len__(self) -> int:
+        """Return the length of the current replica's split."""
 
-        # get the fold which we will assign to current replica
-        indices, targets = self._replica_fold(indices, targets)
+        return self._len
 
-        indices, targets = self._limit(indices, targets, self.max_items)
+    def __iter__(self) -> Iterable:
+        """Return an iterator over the current replica's split."""
 
-        # split current replica's fold between train and val
-        # return indices depending on if we are val or train split
-        indices, _ = self._split(indices, targets, self.val_split_len, self.is_val)
+        indices, targets = self._get_indices()
+        indices, targets = self._split_rank(indices, targets)
+        indices, targets = self._limit_indices(indices, targets, self.max_samples)
+
+        indices, _ = self._split_indices(indices, targets, self.val_split_len, self.is_val_split)
         assert len(indices) == self._len
 
-        # when val fold is needed and shuffle is on, for epoch > 0 we can
-        # shuffle only val fold. The seed for other epochs is 0 so that we don't
-        # mix val with other folds
         if self.shuffle and self.val_ratio > 0.0 and self.epoch > 0:
             np.random.shuffle(indices)
 
         return iter(indices)
 
-    def _replica_fold(self, indices: np.ndarray, targets: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    def _split_rank(self, indices: np.ndarray, targets: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """Split the indices into `world_size` folds and return the one for current rank.
+
+        Args:
+            indices: Indices to split.
+            targets: Targets to split.
+
+        Returns:
+            Indices and targets for current rank.
+
+        """
 
         if self.world_size > 1:
             replica_fold_idxs = None
-            # we don't need shuffling here as it has already been done in _indices()
+
             rfolder = StratifiedKFold(n_splits=self.world_size, shuffle=False)
             folds = rfolder.split(indices, targets)
-            # walk to the split for our rank
+
             for _ in range(self.rank + 1):
-                other_fold_idxs, replica_fold_idxs = next(folds)
+                _, replica_fold_idxs = next(folds)
 
             assert replica_fold_idxs is not None and len(replica_fold_idxs) == self.replica_len_full
 
             return indices[replica_fold_idxs], targets[replica_fold_idxs]
-        else:
-            assert self.world_size == 1
-            return indices, targets
 
-    def _indices(self) -> Tuple[np.ndarray, np.ndarray]:
+        assert self.world_size == 1
+        return indices, targets
+
+    def _get_indices(self) -> Tuple[np.ndarray, np.ndarray]:
+        """Get indices for the current epoch.
+
+        Returns:
+            Indices and targets for the current epoch.
+
+        """
+
         if self.shuffle:
             g = torch.Generator()
             g.manual_seed(self._get_seed())
@@ -138,48 +177,80 @@ class DistributedStratifiedSampler(Sampler):
         else:
             indices = np.arange(self.data_len)
 
-        # add extra samples to make it evenly divisible
-        # this is neccesory because we have __len__ which must return same
-        # number consistently
         if self.total_size > self.data_len:
             indices = np.append(indices, indices[: (self.total_size - self.data_len)])
         else:
-            assert self.total_size == self.data_len, "total_size cannot be less than dataset size!"
+            assert self.total_size == self.data_len, "`total_size` cannot be less than dataset size."
 
         targets = np.array(list(self.dataset.targets[i] for i in indices))
         assert len(indices) == self.total_size
 
         return indices, targets
 
-    def _limit(
-        self, indices: np.ndarray, targets: np.ndarray, max_items: Optional[int]
+    def _limit_indices(
+        self, indices: np.ndarray, targets: np.ndarray, max_samples: Optional[int]
     ) -> Tuple[np.ndarray, np.ndarray]:
-        # this will limit the items to specified max value
-        if max_items is not None:
-            return self._split(indices, targets, len(indices) - max_items, False)
+        """Limit the number of indices to `max_samples`.
+
+        Args:
+            indices: Indices to limit.
+            targets: Targets to limit.
+
+        Returns:
+            Limited indices and targets.
+
+        """
+
+        if max_samples is not None:
+            return self._split_indices(indices, targets, len(indices) - max_samples, False)
+
         return indices, targets
 
     def _get_seed(self) -> int:
-        # if val fold is needed then only do the first shuffle
-        # otherwise deterministically shuffle on every epoch
+        """Get the seed for the current epoch.
+
+        Returns:
+            Seed for the current epoch.
+
+        """
+
         return self.epoch if self.val_ratio == 0.0 else 0
 
-    def _split(
-        self, indices: np.ndarray, targets: np.ndarray, test_size: int, return_test_split: bool
+    def _split_indices(
+        self, indices: np.ndarray, targets: np.ndarray, val_size: int, return_val_split: bool
     ) -> Tuple[np.ndarray, np.ndarray]:
-        if test_size:
-            assert isinstance(test_size, int)  # othewise next call assumes ratio instead of count
-            vfolder = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=self._get_seed())
+        """Split the indices into train and validation sets.
+
+        Args:
+            indices: Indices to split.
+            targets: Targets to split.
+            val_size: Number of samples to use for validation.
+            return_val_split: Whether to return the validation split.
+
+        Returns:
+            Train/validation indices and targets.
+
+        """
+
+        if val_size:
+            assert isinstance(val_size, int)
+
+            vfolder = StratifiedShuffleSplit(n_splits=1, test_size=val_size, random_state=self._get_seed())
             vfolder = vfolder.split(indices, targets)
+
             train_idx, valid_idx = next(vfolder)
 
-            idxs = valid_idx if return_test_split else train_idx
+            idxs = valid_idx if return_val_split else train_idx
             return indices[idxs], targets[idxs]
-        else:
-            return indices, targets
 
-    def __len__(self):
-        return self._len
+        return indices, targets
 
-    def set_epoch(self, epoch):
+    def set_epoch(self, epoch: int) -> int:
+        """Set the epoch for the current replica, which is used to seed the shuffling.
+
+        Args:
+            epoch (int): Epoch number.
+
+        """
+
         self.epoch = epoch
