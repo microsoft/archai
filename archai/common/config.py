fa6efce8a6fc6db30b5fabe5e87b773333d8e016
Gustavo Rosa <gth.rosa@uol.com.br>
2023-01-23 15:54:08 -0300

    fix(archai): Fixes config system to allow additional args that are not present in yaml files.

--- archai/common/config.py
+++ archai/common/config.py
@@ -16,7 +16,17 @@ from archai.common.config_utils import resolve_all
 
 
 def deep_update(d: MutableMapping, u: Mapping, mapping_fn: Callable[[], MutableMapping]) -> MutableMapping:
-    """"""
+    """Recursively update a dictionary with another dictionary.
+
+    Args:
+        d: Dictionary to update.
+        u: Dictionary to update from.
+        mapping_fn: Dunction to create a new dictionary.
+
+    Returns:
+        Updated dictionary.
+
+    """
 
     for k, v in u.items():
         if isinstance(v, Mapping):
@@ -28,37 +38,49 @@ def deep_update(d: MutableMapping, u: Mapping, mapping_fn: Callable[[], MutableM
 
 
 class Config(UserDict):
-    """"""
+    """Configuration class that supports YAML-based configuration files and
+    command line arguments.
+
+    """
 
     def __init__(
         self,
         file_path: Optional[str] = None,
         args: Optional[List[Any]] = None,
-        force_cl_args: Optional[bool] = False,
+        parse_cl_args: Optional[bool] = False,
         cl_description: Optional[str] = None,
         resolve_redirect: Optional[bool] = True,
     ) -> None:
-        """"""
+        """Initializes a configuration object.
+
+        Args:
+            file_path: Path to YAML-based configuration file.
+            args: List of extra arguments, e.g., ["--arg", "value", "--arg2", "value2"].
+            parse_cl_args: Whether to parse command line arguments.
+            cl_description: Description to use for command line arguments.
+            resolve_redirect: Whether to resolve the YAML-based redirects.
+
+        """
 
         super().__init__()
 
         args = args or []
         extra_args = []
 
-        if force_cl_args:
+        if parse_cl_args:
             parser = argparse.ArgumentParser(description=cl_description)
             parser.add_argument(
                 "-c" "--config",
                 type=str,
                 default=None,
-                help="YAML-based configuration file-paths (separated by `;` if multiple).",
+                help="YAML-based configuration file-paths (separated by `,` if multiple).",
             )
 
             args, extra_args = parser.parse_known_args()
             file_path = args.config or file_path
 
         if file_path:
-            for fp in file_path.strip().split(";"):
+            for fp in file_path.strip().split(","):
                 self._load(fp.strip())
 
         # Create a copy and resolve it, which can be used to search for overrides
@@ -77,7 +99,12 @@ class Config(UserDict):
         self.file_path = file_path
 
     def _load(self, file_path: str) -> None:
-        """"""
+        """Load a YAML-based configuration file.
+
+        Args:
+            file_path: Path to YAML-based configuration file.
+
+        """
 
         if file_path:
             file_path = os.path.abspath(os.path.expanduser(os.path.expandvars(file_path)))
@@ -88,7 +115,13 @@ class Config(UserDict):
             deep_update(self, config_yaml, lambda: Config(resolve_redirect=False))
 
     def _update_from_include(self, config_yaml: Dict[str, Any], file_path: str) -> None:
-        """"""
+        """Update the configuration from the __include__ directive.
+
+        Args:
+            config_yaml: YAML-based configuration.
+            file_path: Path to YAML-based configuration file.
+
+        """
 
         if "__include__" in config_yaml:
             includes = config_yaml["__include__"]
@@ -100,58 +133,89 @@ class Config(UserDict):
                 self._load_from_file(include_file_path)
 
     def _update_from_args(self, args: List[Any], resolved_section: Config) -> None:
-        """"""
+        """Update the configuration from extra/command line arguments.
+
+        Args:
+            args: List of extra arguments, e.g., ["--arg", "value", "--arg2", "value2"].
+            resolved_section: Resolved configuration.
+
+        """
 
         i = 0
         while i < len(args) - 1:
             arg = args[i]
-            if arg.startswith(("--")):
-                path = arg[len("--") :].split(".")
+            if arg.startswith("--"):
+                path = arg[2:].split(".")
                 i += self._update_section(path, args[i + 1], resolved_section)
             else:
                 i += 1
 
     def _update_section(self, path: List[str], value: Any, resolved_section: Config) -> int:
-        """"""
+        """Update the section of a configuration object.
+
+        Args:
+            path: Path to the configuration section.
+            value: Value to set.
+            resolved_section: Resolved configuration.
+
+        Returns:
+            `2` if arguments have been consumed and `1` if path has not been found.
+
+        """
 
         for p in range(len(path) - 1):
             sub_path = path[p]
             if sub_path in resolved_section:
                 resolved_section = resolved_section[sub_path]
-                if sub_path not in self:
-                    self[sub_path] = Config(resolve_redirects=False)
-                self = self[sub_path]
-            else:
-                return 1  # path not found, ignore this
-        key = path[-1]  # final leaf node value
 
+            if sub_path not in self:
+                self[sub_path] = Config(resolve_redirect=False)
+            self = self[sub_path]
+
+        key = path[-1]  # Final leaf node value
         if key in resolved_section:
             original_val, original_type = None, None
             try:
                 original_val = resolved_section[key]
                 original_type = type(original_val)
+
                 if original_type == bool:  # bool('False') is True :(
 
                     def original_type(x):
                         return strtobool(x) == 1
 
                 self[key] = original_type(value)
-            except Exception as e:
+
+            except Exception:
                 raise KeyError(
-                    f'The yaml key or command line argument "{key}" is likely not named correctly or value is of wrong data type. Error was occured when setting it to value "{value}".'
-                    f"Originally it is set to {original_val} which is of type {original_type}."
-                    f"Original exception: {e}"
+                    f"Key: {key} could not been set to value {value}."
+                    f"Original value was {original_val} of type {original_type}."
                 )
-            return 2  # path was found, increment arg pointer by 2 as we use up val
         else:
-            return 1  # path not found, ignore this
+            self[key] = value
+
+        return 2  # Path was found or created, increment arg pointer by 2 as we use up val
 
     def to_dict(self) -> Dict[str, Any]:
-        """"""
+        """Converts the `Config` object to a dictionary.
+
+        Returns:
+            `Config` represented as a dictionary.
+
+        """
 
         return deep_update({}, self, lambda: dict())
 
     def get(self, key: str, default_value: Optional[Any] = None) -> Any:
-        """"""
+        """Get a value from the `Config` object.
+
+        Args:
+            key: Key to search for.
+            default_value: Default value to return if `key` is not found.
+
+        Returns:
+            Value corresponding to the key.
+
+        """
 
         return super().get(key, default_value)
--- archai/common/config_utils.py
+++ archai/common/config_utils.py
@@ -1,22 +1,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from typing import Any, Mapping, MutableMapping, Optional
 
-from typing import Mapping, MutableMapping, Any, Optional
-
-
-_PREFIX_NODE = '_copy' # for copy node content command (must be dict)
-_PREFIX_PATH = '_copy:' # for copy node value command (must be scaler)
+_PREFIX_NODE = "_copy"  # For copy node content command (must be dict)
+_PREFIX_PATH = "_copy:"  # For copy node value command (must be scalar)
 
 
-def resolve_all(root_d:MutableMapping):
-    _resolve_all(root_d, root_d, '/', set())
+def resolve_all(root_d: MutableMapping):
+    _resolve_all(root_d, root_d, "/", set())
 
-def _resolve_all(root_d:MutableMapping, cur:MutableMapping, cur_path:str, prev_paths:set):
+
+def _resolve_all(root_d: MutableMapping, cur: MutableMapping, cur_path: str, prev_paths: set):
     assert is_proper_path(cur_path)
 
     if cur_path in prev_paths:
-        return # else we get in to infinite recursion
+        return  # else we get in to infinite recursion
     prev_paths.add(cur_path)
 
     # if cur dict has '_copy' node with path in it
@@ -36,13 +35,13 @@ def _resolve_all(root_d:MutableMapping, cur:MutableMapping, cur_path:str, prev_p
         # if this key needs path resolution, get target and replace the value
         rpath = _req_resolve(cur[k])
         if rpath:
-            cur[k] = _resolve_path(root_d,
-                        _rel2full_path(_join_path(cur_path, k), rpath), prev_paths)
+            cur[k] = _resolve_path(root_d, _rel2full_path(_join_path(cur_path, k), rpath), prev_paths)
         # if replaced value is again dictionary, recurse on it
         if isinstance(cur[k], MutableMapping):
             _resolve_all(root_d, cur[k], _join_path(cur_path, k), prev_paths)
 
-def _merge_source(source:Mapping, dest:MutableMapping)->None:
+
+def _merge_source(source: Mapping, dest: MutableMapping) -> None:
     # for anything that source has but dest doesn't, just do copy
     for sk in source:
         if sk not in dest:
@@ -56,78 +55,83 @@ def _merge_source(source:Mapping, dest:MutableMapping)->None:
                 _merge_source(source[sk], dest[sk])
             # else at least dest value is not dict and should not be overriden
 
-def _req_resolve(v:Any)->Optional[str]:
+
+def _req_resolve(v: Any) -> Optional[str]:
     """If the value is actually a path we need resolve then return that path or return None"""
     if isinstance(v, str) and v.startswith(_PREFIX_PATH):
         # we will almost always have space after _copy command
-        return v[len(_PREFIX_PATH):].strip()
+        return v[len(_PREFIX_PATH) :].strip()
     return None
 
-def _join_path(path1:str, path2:str):
-    mid = 1 if path1.endswith('/') else 0
-    mid += 1 if path2.startswith('/') else 0
+
+def _join_path(path1: str, path2: str):
+    mid = 1 if path1.endswith("/") else 0
+    mid += 1 if path2.startswith("/") else 0
 
     # only 3 possibilities
-    if mid==0:
-        res = path1 + '/' + path2
-    elif mid==1:
+    if mid == 0:
+        res = path1 + "/" + path2
+    elif mid == 1:
         res = path1 + path2
     else:
         res = path1[:-1] + path2
 
     return _norm_ended(res)
 
-def _norm_ended(path:str)->str:
-    if len(path) > 1 and path.endswith('/'):
+
+def _norm_ended(path: str) -> str:
+    if len(path) > 1 and path.endswith("/"):
         path = path[:-1]
     return path
 
-def is_proper_path(path:str)->bool:
-    return path.startswith('/') and (len(path)==1 or not path.endswith('/'))
 
-def _rel2full_path(cwd:str, rel_path:str)->str:
+def is_proper_path(path: str) -> bool:
+    return path.startswith("/") and (len(path) == 1 or not path.endswith("/"))
+
+
+def _rel2full_path(cwd: str, rel_path: str) -> str:
     """Given current directory and path, we return abolute path. For example,
     cwd='/a/b/c' and rel_path='../d/e' should return '/a/b/d/e'. Note that rel_path
     can hold absolute path in which case it will start with '/'
     """
-    assert len(cwd) > 0 and cwd.startswith('/'), 'cwd must be absolute path'
+    assert len(cwd) > 0 and cwd.startswith("/"), "cwd must be absolute path"
 
-    rel_parts = rel_path.split('/')
-    if rel_path.startswith('/'):
-        cwd_parts = [] # rel_path is absolute path so ignore cwd
+    rel_parts = rel_path.split("/")
+    if rel_path.startswith("/"):
+        cwd_parts = []  # rel_path is absolute path so ignore cwd
     else:
-        cwd_parts = cwd.split('/')
+        cwd_parts = cwd.split("/")
     full_parts = cwd_parts + rel_parts
 
     final = []
     for i in range(len(full_parts)):
         part = full_parts[i].strip()
-        if not part or part == '.': # remove blank strings and single dots
+        if not part or part == ".":  # remove blank strings and single dots
             continue
-        if part == '..':
+        if part == "..":
             if len(final):
                 final.pop()
             else:
-                raise RuntimeError(f'cannot create abs path for cwd={cwd} and rel_path={rel_path}')
+                raise RuntimeError(f"cannot create abs path for cwd={cwd} and rel_path={rel_path}")
         else:
             final.append(part)
 
-    final = '/' + '/'.join(final)  # should work even when final is empty
-    assert not '..' in final and is_proper_path(final) # make algo indeed worked
+    final = "/" + "/".join(final)  # should work even when final is empty
+    assert ".." not in final and is_proper_path(final)  # make algo indeed worked
     return final
 
 
-def _resolve_path(root_d:MutableMapping, path:str, prev_paths:set)->Any:
+def _resolve_path(root_d: MutableMapping, path: str, prev_paths: set) -> Any:
     """For given path returns value or node from root_d"""
 
     assert is_proper_path(path)
 
     # traverse path in root dict hierarchy
-    cur_path = '/' # path at each iteration of for loop
+    cur_path = "/"  # path at each iteration of for loop
     d = root_d
-    for part in path.split('/'):
+    for part in path.split("/"):
         if not part:
-            continue # there will be blank vals at start
+            continue  # there will be blank vals at start
 
         # For each part, we need to be able find key in dict but some dics may not
         # be fully resolved yet. For last key, d will be either dict or other value.
@@ -143,7 +147,9 @@ def _resolve_path(root_d:MutableMapping, path:str, prev_paths:set)->Any:
             else:
                 raise RuntimeError(f'Path {path} could not be found in specified dictionary at "{part}"')
         else:
-            raise KeyError(f'Path "{path}" cannot be resolved because "{cur_path}" is not a dictionary so "{part}" cannot exist in it')
+            raise KeyError(
+                f'Path "{path}" cannot be resolved because "{cur_path}" is not a dictionary so "{part}" cannot exist in it'
+            )
 
     # last child is our answer
     rpath = _req_resolve(d)
