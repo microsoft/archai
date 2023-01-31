f420306efd12f38a058f62ae777c8cbce97792b4
piero2c <pierokauffmann@gmail.com>
2023-01-30 17:17:10 -0800

    feat(ds): makes `repeat_config` accept constant number of repetitions

--- archai/discrete_search/search_spaces/config/helpers.py
+++ archai/discrete_search/search_spaces/config/helpers.py
@@ -2,25 +2,29 @@
 # Licensed under the MIT license.
 
 from copy import deepcopy
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from archai.discrete_search.search_spaces.config.discrete_choice import DiscreteChoice
 
 
 def repeat_config(
-    config_dict: Dict[str, Any], repeat_times: List[int], share_arch: Optional[bool] = False
+    config_dict: Dict[str, Any], repeat_times: Union[int, List[int]], share_arch: Optional[bool] = False
 ) -> Dict[str, Any]:
-    """Repeat a configuration multiple times.
+    """Repeats an architecture config a variable number of times.
 
     Args:
-        config_dict: Configuration dictionary.
-        repeat_times: Number of times to repeat the configuration.
-        share_arch: Whether to share the architecture between the repeated configurations.
+        config_dict (Dict[str, Any]): Config dictionary to repeat.
+        
+        repeat_times (Union[int, List[int]]): If an integer, the number of times to repeat the config 
+            will be treated as constant. If a list of integers, the number of times to repeat the config will be
+            also considered an architecture parameter and will be sampled from the list.
+        
+        share_arch (bool, optional): Whether to share the architecture parameters across the
+            repeated configs. Defaults to False.
 
     Returns:
-        Repeated configuration dictionary.
-
-    """
+        Dict[str, Any]: Config dictionary with the repeated config.
+    """    
 
     return {
         "_config_type": "config_list",
