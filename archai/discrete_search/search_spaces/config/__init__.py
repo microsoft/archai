baa7b7b2799f98f1b29ee51e583094a20db38353
piero2c <pierokauffmann@gmail.com>
2023-01-31 05:12:54 -0800

    chore(ds): shorter imports for config ss

--- archai/discrete_search/search_spaces/config/__init__.py
+++ archai/discrete_search/search_spaces/config/__init__.py
@@ -1,2 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+
+from archai.discrete_search.search_spaces.config.arch_config import ArchConfig
+from archai.discrete_search.search_spaces.config.arch_param_tree import ArchParamTree
+from archai.discrete_search.search_spaces.config.discrete_choice import DiscreteChoice
+from archai.discrete_search.search_spaces.config.helpers import repeat_config
+from archai.discrete_search.search_spaces.config.search_space import ConfigSearchSpace
