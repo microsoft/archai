c081c71cf1ccf7908ae658a7b40465a687fa611f
piero2c <pierokauffmann@gmail.com>
2023-01-31 05:40:36 -0800

    chore(ds): shorter algo imports

--- archai/discrete_search/algos/__init__.py
+++ archai/discrete_search/algos/__init__.py
@@ -1,2 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+
+from archai.discrete_search.algos.bananas import MoBananasSearch
+from archai.discrete_search.algos.evolution_pareto import EvolutionParetoSearch
+from archai.discrete_search.algos.local_search import LocalSearch
+from archai.discrete_search.algos.random_search import RandomSearch
+from archai.discrete_search.algos.regularized_evolution import RegularizedEvolutionSearch
