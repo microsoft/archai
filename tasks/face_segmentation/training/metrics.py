4beb6a0f7400d73500b3be99d945204f0a1175f8
piero2c <pierokauffmann@gmail.com>
2023-02-02 06:27:13 -0800

    fix(task/seg): metrics ignore labels

--- tasks/face_segmentation/training/metrics.py
+++ tasks/face_segmentation/training/metrics.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Dict
 import torch
 
 @torch.no_grad()
@@ -16,34 +16,40 @@ def get_confusion_matrix(pred_labels: torch.LongTensor,
 
 @torch.no_grad()
 def get_iou(confusion_matrix: torch.LongTensor,
-            ignore_labels: Optional[List[int]] = None) -> torch.Tensor:
+            ignore_labels: Optional[List[int]] = None) -> Dict[str, torch.Tensor]:
     ignore_labels = ignore_labels or []
+    ignore_labels = torch.isin(
+        torch.arange(len(confusion_matrix)), torch.tensor(ignore_labels)
+    )
     
     diag = confusion_matrix.diag()
     row_sum = confusion_matrix.sum(dim=1)
     col_sum = confusion_matrix.sum(dim=0)
     
     class_iou = (diag + 1e-7) / (row_sum + col_sum - diag + 1e-7)
-    class_iou[ignore_labels] = torch.nan
-    
-    return class_iou
+
+    return {
+        'class_iou': class_iou,
+        'mIOU': class_iou[~ignore_labels].mean(),
+    }
+
 
 @torch.no_grad()
 def get_f1_scores(confusion_matrix: torch.LongTensor,
-                  ignore_labels: Optional[List[int]] = None) -> torch.Tensor:
+                  ignore_labels: Optional[List[int]] = None) -> Dict[str, torch.Tensor]:
     ignore_labels = ignore_labels or []
+    ignore_labels = torch.isin(
+        torch.arange(len(confusion_matrix)), torch.tensor(ignore_labels)
+    )
 
     recall = confusion_matrix.diag() / (confusion_matrix.sum(dim=1) + 1e-7)
     prec = confusion_matrix.diag() / (confusion_matrix.sum(dim=0) + 1e-7)
 
     class_f1 = 2 * prec * recall / (prec + recall + 1e-7)
-    class_f1 = class_f1[~torch.tensor(ignore_labels)]
-
-    support = confusion_matrix.sum(dim=1)
-    support = support[~torch.tensor(ignore_labels)]
+    support = confusion_matrix.sum(dim=1)[~ignore_labels]
 
     return {
         'class_f1': class_f1,
-        'macro_f1': class_f1[~torch.isnan(class_f1)].mean(),
-        'weighted_f1': (class_f1 * support) / support.sum(),
+        'macro_f1': class_f1[~ignore_labels].mean(),
+        'weighted_f1': (class_f1[~ignore_labels] * support).sum() / support.sum()
     }
