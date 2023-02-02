4ff829ae9b2e6d5db5e886eea418547bbfe81b79
piero2c <pierokauffmann@gmail.com>
2023-02-02 12:56:32 -0800

    fix(task/seg): fix `PartialTrainingValIOU` metric

--- tasks/face_segmentation/training/partial_training_evaluator.py
+++ tasks/face_segmentation/training/partial_training_evaluator.py
@@ -10,7 +10,7 @@ from .pl_trainer import SegmentationTrainingLoop
 
 
 class PartialTrainingValIOU(ModelEvaluator):
-    def __init__(self, output_dir: str, tr_epochs: float = 0.2,
+    def __init__(self, output_dir: str, tr_epochs: float = 1.0,
                  batch_size: int = 16, lr: float = 2e-4,
                  tr_dl_workers: int = 8, val_dl_workers: int = 8,
                  val_check_interval: float = 0.1):
@@ -26,12 +26,6 @@ class PartialTrainingValIOU(ModelEvaluator):
     def evaluate(self, model: ArchaiModel, dataset_provider: DatasetProvider, 
                  budget: Optional[float] = None) -> float:
         
-        trainer = Trainer(
-            default_root_dir=str(self.output_dir), gpus=1, 
-            val_check_interval=self.val_check_interval,
-            max_epochs=self.tr_epochs
-        )
-
         tr_dataset = dataset_provider.get_train_dataset()
         val_dataset = dataset_provider.get_val_dataset()
 
@@ -44,9 +38,15 @@ class PartialTrainingValIOU(ModelEvaluator):
             num_workers=self.val_dl_workers
         )
 
+        trainer = Trainer(
+            default_root_dir=str(self.output_dir), gpus=1, 
+            val_check_interval=int(self.val_check_interval * len(tr_dataloader)),
+            max_steps=int(self.tr_epochs * len(tr_dataloader)),
+        )
+
         trainer.fit(
             SegmentationTrainingLoop(model.arch, lr=self.lr),
             tr_dataloader, val_dataloader,
         )
 
-        return trainer.validate(dataloaders=val_dataloader)['validation_mIOU']
+        return trainer.validate(trainer.model, val_dataloader)[0]['validation_mIOU']
