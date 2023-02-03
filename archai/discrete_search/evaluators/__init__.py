aa13d7113985a662d1cdc9d0ad5c3fb3f0c153fe
piero2c <pierokauffmann@gmail.com>
2023-02-03 10:58:13 -0800

    chore(ds): shorter imports for archai.ds.evaluators

--- archai/discrete_search/evaluators/__init__.py
+++ archai/discrete_search/evaluators/__init__.py
@@ -1,2 +1,18 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
+
+from archai.discrete_search.evaluators.functional import EvaluationFunction
+from archai.discrete_search.evaluators.onnx_model import AvgOnnxLatency
+from archai.discrete_search.evaluators.progressive_training import (
+    ProgressiveTraining, RayProgressiveTraining
+)
+from archai.discrete_search.evaluators.pt_profiler import (
+    TorchFlops, TorchLatency, TorchCudaPeakMemory, TorchNumParameters
+)
+from archai.discrete_search.evaluators.ray import RayParallelEvaluator
+
+__all__ = [
+    'EvaluationFunction', 'AvgOnnxLatency', 'ProgressiveTraining', 
+    'RayProgressiveTraining', 'TorchFlops', 'TorchLatency', 'TorchCudaPeakMemory',
+    'TorchNumParameters', 'RayParallelEvaluator'
+]
