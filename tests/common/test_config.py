e6cc4e8acd7d69499cfb5cb7197e399186d9229c
Shital Shah <shitals@microsoft.com>
2023-02-04 17:08:33 -0800

    renamearg:  file_path -> config_filepath

--- tests/common/test_config.py
+++ tests/common/test_config.py
@@ -8,14 +8,14 @@ from archai.common.config import Config
 
 def test_config():
     # Asserts that it can load keys from a YAML file
-    config_file_path = "config.yaml"
-    with open(config_file_path, "w") as f:
+    config_filepath = "config.yaml"
+    with open(config_filepath, "w") as f:
         f.write("test_key: test_value")
 
-    config = Config(file_path=config_file_path)
+    config = Config(config_filepath=config_filepath)
     assert config["test_key"] == "test_value"
 
-    os.remove(config_file_path)
+    os.remove(config_filepath)
 
     # Assert that additional arguments can be loaded as keys
     config = Config(
