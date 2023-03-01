a7bc2d68c485854eac6a3d46b695684a98a7f0b0
Gustavo Rosa <gth.rosa@uol.com.br>
2023-03-01 06:08:11 -0800

    chore(trainers): Adds support for evaluation and checkpoint in DsTrainer.

--- archai/trainers/ds_trainer.py
+++ archai/trainers/ds_trainer.py
@@ -1,15 +1,27 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from typing import Optional, Union
+from typing import Any, Dict, Optional, Union
 
 import deepspeed
 import torch
 from deepspeed.pipe import PipelineModule
+from deepspeed.utils import RepeatingLoader
 from overrides import overrides
+from torch.utils.data import DataLoader
 
 from archai.api.trainer_base import TrainerBase
 from archai.trainers.ds_training_args import DsTrainingArguments
+
+
+def _create_deepspeed_config() -> Dict[str, Any]:
+    return {
+        "train_micro_batch_size_per_gpu": 256,
+        "gradient_accumulation_steps": 16,
+        "fp16": {"enabled": True, "initial_scale_power": 12},
+        "zero_optimization": {"stage": 1, "reduce_bucket_size": 5e8},
+        "optimizer": {"type": "Adam", "params": {"lr": 5e-5, "betas": [0.9, 0.999], "eps": 1e-8}},
+    }
 
 
 class DsTrainer(TrainerBase):
@@ -25,14 +37,15 @@ class DsTrainer(TrainerBase):
         lr_scheduler=None,
         mpu=None,
         dist_init_required=None,
-        collate_fn=None,
+        train_dataset=None,
+        eval_dataset=None,
     ) -> None:
         """"""
 
         deepspeed.init_distributed()
 
         if args is None:
-            args = DsTrainingArguments({})
+            args = DsTrainingArguments(_create_deepspeed_config())
         assert isinstance(args, DsTrainingArguments), "`args` should be an instance of `DsTrainingArguments`."
         self.args = args
 
@@ -52,26 +65,64 @@ class DsTrainer(TrainerBase):
             model=model,
             optimizer=optimizer,
             model_parameters=model_parameters or [p for p in model.parameters() if p.requires_grad],
-            training_data=training_data,
             lr_scheduler=lr_scheduler,
             mpu=mpu,
             dist_init_required=dist_init_required,
-            collate_fn=collate_fn,
-            config=self.args.deepspeed_config,
+            config=self.args.config,
         )
+
+        self.train_dataset = train_dataset
+        self.eval_dataset = eval_dataset
+
+    def _get_dataloader(self, dataset, sampler=None, shuffle=False):
+        if sampler is None:
+            sampler = torch.utils.data.distributed.DistributedSampler(
+                dataset,
+                num_replicas=self.engine.dp_world_size,
+                rank=self.engine.mpu.get_data_parallel_rank(),
+                shuffle=shuffle,
+            )
+
+        dataloader = DataLoader(dataset, sampler=sampler, drop_last=True, batch_size=self.engine.micro_batch_size)
+
+        return iter(RepeatingLoader(dataloader))
 
     @overrides
     def train(
         self,
     ) -> None:
-        for _ in range(self.args.max_steps):
-            self.engine.train_batch()
+        train_dataloader = self._get_dataloader(self.train_dataset, shuffle=True)
+        for step in range(self.args.max_steps):
+            _ = self.engine.train_batch(train_dataloader)
+
+            if step % self.args.eval_interval and self.args.do_eval:
+                eval_dataloader = self._get_dataloader(self.eval_dataset, shuffle=False)
+                self._evaluation_step(eval_dataloader)
+
+            if step % self.args.save_steps:
+                self.engine.save_checkpoint(self.args.output_dir, step)
+
+    def _evaluation_step(self, eval_dataloader):
+        eval_loss = 0.0
+
+        for _ in range(len(eval_dataloader)):
+            loss = self.engine.eval_batch(eval_dataloader)
+            eval_loss += loss.mean().item()
+
+        eval_loss / len(eval_dataloader)
+
+        return eval_loss
 
     @overrides
     def evaluate(
         self,
+        eval_dataset=None,
     ) -> None:
-        pass
+        eval_dataset = eval_dataset or self.eval_dataset
+        assert eval_dataset, "`eval_dataset` must be supplied in constructor or evaluate()."
+
+        eval_dataloader = self._get_dataloader(eval_dataset, shuffle=False)
+        _ = self._evaluation_step(eval_dataloader)
 
     @overrides
     def predict(
--- archai/trainers/ds_training_args.py
+++ archai/trainers/ds_training_args.py
@@ -1,9 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import json
 import os
 from dataclasses import asdict, dataclass, field
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 import deepspeed
 import torch
@@ -18,7 +19,9 @@ class DsTrainingArguments:
 
     """
 
-    deepspeed_config: str = field(metadata={"help": "Path to the DeepSpeed configuration."})
+    config: Union[dict, str] = field(metadata={"help": "DeepSpeed configuration (dictionary or path to JSON file)."})
+
+    output_dir: str = field(default="~/logdir", metadata={"help": "Output folder."})
 
     seed: int = field(default=42, metadata={"help": "Random seed."})
 
@@ -27,6 +30,12 @@ class DsTrainingArguments:
     backend: int = field(default="nccl", metadata={"help": "Distributed training backend."})
 
     max_steps: int = field(default=100, metadata={"help": "Maximum number of training steps."})
+
+    do_eval: bool = field(default=True, metadata={"help": "Whether to enable evaluation."})
+
+    eval_steps: int = field(default=100, metadata={"help": "Number of steps between evaluations."})
+
+    save_steps: int = field(default=500, metadata={"help": "Number of steps between checkpoints."})
 
     pipeline_parallalelism: bool = field(default=True, metadata={"help": "Whether to use pipeline parallelism."})
 
@@ -41,11 +50,27 @@ class DsTrainingArguments:
     def __post_init__(self) -> None:
         """Override post-initialization with custom instructions."""
 
+        if isinstance(self.config, str):
+            with open(self.config, "r") as f:
+                self.config = json.load(f)
+
         torch.manual_seed(self.seed)
         deepspeed.runtime.utils.set_random_seed(self.seed)
 
         self.local_rank = int(self.local_rank)
         torch.cuda.set_device(self.local_rank)
+
+        # self.batch_size
+
+    @property
+    def batch_size(self) -> int:
+        if "train_micro_batch_size_per_gpu" in self.config:
+            return self.config.train_micro_batch_size_per_gpu
+
+        # if "train_batch_size" in self.config:
+
+        # print(self.config)
+        # raise
 
     def to_dict(self) -> Dict[str, Any]:
         """Convert attributes into a dictionary representation.
