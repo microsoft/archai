d9c756cca2b78df8b8f0b03cfa1eb18b7fcdc6dc
Luis França <luis.franca@microsoft.com>
2023-03-06 12:07:25 -0300

    Adding Optional arguments and docstrings to azureml and notebook helper modules.

--- archai/common/notebook_helper.py
+++ archai/common/notebook_helper.py
@@ -1,8 +1,8 @@
-from typing import Union
+from typing import Union, Optional
 from pathlib import Path
 import pandas as pd
 
-def get_search_csv(output_path: Union[str, Path], iteration_num: int = -1) -> pd.DataFrame:
+def get_search_csv(output_path: Union[str, Path], iteration_num: Optional[int] = -1) -> pd.DataFrame:
     """Reads the search csv file from the output path and returns a pandas dataframe
 
     Args:
@@ -24,6 +24,14 @@ def get_search_csv(output_path: Union[str, Path], iteration_num: int = -1) -> pd
     return df
 
 def get_csv_as_stylized_html(df: pd.DataFrame) -> str:
+    """Returns a stylized html table from a pandas dataframe with a scrollbar
+
+    Args:
+        df (pd.DataFrame): Pandas dataframe to convert to html table
+
+    Returns:
+        str: Stylized html table
+    """
     styled_table = df.style.set_properties(**{'background-color': 'lightblue',
                                             'color': 'black',
                                             'border-color': 'white',
@@ -35,7 +43,17 @@ def get_csv_as_stylized_html(df: pd.DataFrame) -> str:
 
     return html_with_scrollbar
 
-def get_arch_abs_path(archid: str, downloaded_folder: Union[str, Path], iteration_num: int = -1) -> Path:
+def get_arch_abs_path(archid: str, downloaded_folder: Union[str, Path], iteration_num: Optional[int] = -1) -> Path:
+    """Returns the absolute path to the architecture file
+
+    Args:
+        archid (str): Architecture id
+        downloaded_folder (Union[str, Path]): Path to the downloaded folder
+        iteration_num (int, optional): Search iteration to read from. Defaults to -1, which will point to the last iteration
+
+    Returns:
+        Path: Absolute path to the architecture file
+    """
     if iteration_num == -1:
         dir_path = max(Path(downloaded_folder).glob("pareto_models_iter_*"), key=lambda x: int(x.stem.split("_")[-1]))
     else:
