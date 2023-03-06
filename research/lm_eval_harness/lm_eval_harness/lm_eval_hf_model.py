bb17a23c9d4c8169c1e3c8d53e4a759024b1b166
mojanjp <mojavahe@ucsd.edu>
2023-03-06 10:40:40 -0800

    mapping inputs to correct devcie for generation tasks in lm_eval_hf_model.py

--- research/lm_eval_harness/lm_eval_harness/lm_eval_hf_model.py
+++ research/lm_eval_harness/lm_eval_harness/lm_eval_hf_model.py
@@ -82,7 +82,7 @@ class HFEvalModel(BaseLM):
 
             # Encodes the context and defines number of tokens to be
             # removed when generation ends (default = 1)
-            input_ids = self.tokenizer(context, return_tensors="pt")["input_ids"]
+            input_ids = self.tokenizer(context, return_tensors="pt")["input_ids"].to(self.device)
             n_removal_tokens = 1
 
             if stop_tokens:
