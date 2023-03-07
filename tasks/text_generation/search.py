0cb708018e512ce792f8ea73a63d1fc39d83d474
piero2c <pierokauffmann@gmail.com>
2023-03-07 12:09:47 -0800

    fix(text_gen): sets save_pareto_weights to false

--- tasks/text_generation/search.py
+++ tasks/text_generation/search.py
@@ -135,6 +135,7 @@ if __name__ == "__main__":
         mutations_per_parent=args.mutations_per_parent,
         num_crossovers=args.num_crossovers,
         clear_evaluated_models=True,
+        save_pareto_model_weights=False,
         seed=args.seed,
     )
     algo.search()
