e99a21e056336d900dd5c37a22dcb954a6b1b8b5
lovettchris <lovettchris@users.noreply.github.com>
2023-03-07 20:56:00 +0000

    deploy: 8fc105846dcc30a1cf3d53efceb80539d6a09b49

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -7,6 +7,6 @@ var DOCUMENTATION_OPTIONS = {
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: '',
     NAVIGATION_WITH_KEYS: true
 };
