438c6abda8dc98d3df4d956995112ef0da3c18fe
piero2c <pierokauffmann@gmail.com>
2023-03-30 18:35:37 -0300

    refactor(prog_tr): changes progressive training implementation to use ProgressiveModelEvaluator interface

--- archai/discrete_search/evaluators/progressive_training.py
+++ archai/discrete_search/evaluators/progressive_training.py
@@ -2,16 +2,17 @@
 # Licensed under the MIT license.
 
 import tempfile
+from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import ray
+import torch
 from overrides import overrides
 
 from archai.api.dataset_provider import DatasetProvider
 from archai.discrete_search.api.archai_model import ArchaiModel
 from archai.discrete_search.api.model_evaluator import (
-    AsyncModelEvaluator,
-    ModelEvaluator,
+    AsyncModelEvaluator, ModelEvaluator, ProgressiveModelEvaluator
 )
 from archai.discrete_search.api.search_space import DiscreteSearchSpace
 
@@ -26,23 +27,42 @@ def _ray_wrap_training_fn(training_fn) -> Callable:
     return _stateful_training_fn
 
 
-class ProgressiveTraining(ModelEvaluator):
+class ProgressiveTraining(ModelEvaluator, ProgressiveModelEvaluator):
     """Progressive training evaluator."""
 
-    def __init__(self, search_space: DiscreteSearchSpace, training_fn: Callable) -> None:
+    def __init__(self, training_fn: Callable, output_dir: str) -> None:
         """Initialize the evaluator.
 
         Args:
-            search_space: Search space.
             training_fn: Training function.
 
         """
 
-        self.search_space = search_space
         self.training_fn = training_fn
+        self.output_dir = Path(output_dir)
+
+        assert not self.output_dir.is_file(), f'Output directory {self.output_dir} is a file'
+        self.output_dir.mkdir(parents=True, exist_ok=True)
 
         # Training state buffer (e.g optimizer state) for each architecture id
         self.training_states = {}
+
+    @overrides
+    def save_checkpoint(self) -> str:
+        for archid, tr_state in self.training_states.items():
+            torch.save(tr_state, self.output_dir / f'{archid}.pt')
+        
+        return str(self.output_dir)
+
+    @overrides
+    def load_checkpoint(self, checkpoint: str) -> None:
+        checkpoint_dir = Path(checkpoint)
+        assert checkpoint_dir.is_dir(), f'Checkpoint {checkpoint} is not a directory'
+
+        for checkpoint_file in checkpoint_dir.glob('*.pt'):
+            archid = checkpoint_file.stem
+            tr_state = torch.load(checkpoint_file)
+            self.training_states[archid] = tr_state        
 
     @overrides
     def evaluate(self, arch: ArchaiModel, dataset: DatasetProvider, budget: Optional[float] = None) -> float:
@@ -56,12 +76,11 @@ class ProgressiveTraining(ModelEvaluator):
         return metric_result
 
 
-class RayProgressiveTraining(AsyncModelEvaluator):
+class RayProgressiveTraining(AsyncModelEvaluator, ProgressiveModelEvaluator):
     """Progressive training evaluator using Ray."""
 
     def __init__(
         self,
-        search_space: DiscreteSearchSpace,
         training_fn: Callable,
         timeout: Optional[float] = None,
         force_stop: Optional[bool] = False,
@@ -70,14 +89,11 @@ class RayProgressiveTraining(AsyncModelEvaluator):
         """Initialize the evaluator.
 
         Args:
-            search_space: Search space.
             training_fn: Training function.
             timeout: Timeout (seconds) for fetching results.
             force_stop: If True, forces to stop all training jobs when fetching results.
 
         """
-
-        self.search_space = search_space
 
         if ray_kwargs:
             self.compute_fn = ray.remote(**ray_kwargs)(_ray_wrap_training_fn(training_fn))
@@ -96,6 +112,23 @@ class RayProgressiveTraining(AsyncModelEvaluator):
 
         # Training state buffer (e.g optimizer state) for each architecture id
         self.training_states = {}
+
+    @overrides
+    def save_checkpoint(self) -> str:
+        for archid, tr_state in self.training_states.items():
+            torch.save(tr_state, self.output_dir / f'{archid}.pt')
+        
+        return str(self.output_dir)
+    
+    @overrides
+    def load_checkpoint(self, checkpoint: str) -> None:
+        checkpoint_dir = Path(checkpoint)
+        assert checkpoint_dir.is_dir(), f'Checkpoint {checkpoint} is not a directory'
+
+        for checkpoint_file in checkpoint_dir.glob('*.pt'):
+            archid = checkpoint_file.stem
+            tr_state = torch.load(checkpoint_file)
+            self.training_states[archid] = tr_state
 
     @overrides
     def send(self, arch: ArchaiModel, dataset: DatasetProvider, budget: Optional[float] = None) -> None:
@@ -135,14 +168,8 @@ class RayProgressiveTraining(AsyncModelEvaluator):
             if job_results:
                 trained_model, job_metric, training_state = job_results
 
-                # Syncs model weights
-                with tempfile.NamedTemporaryFile() as tmp:
-                    self.search_space.save_model_weights(trained_model, tmp.name)
-                    self.search_space.load_model_weights(self.models[job_id], tmp.name)
-
                 # Syncs training state
                 self.training_states[trained_model.archid] = training_state
-
                 metric_results.append(job_metric)
 
         # Resets model and job buffers
